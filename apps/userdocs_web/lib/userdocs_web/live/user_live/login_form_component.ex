b2f7164bd97616e9dbee3c2ec949f21b838830c1
John Davenport <johns10@gmail.com>
2020-11-16 18:15:22 -0500

    Refactored login stuff to seperate modules

---
+++ apps/userdocs_web/lib/userdocs_web/live/user_live/login_form_component.ex
@@ -0,0 +1,28 @@
+defmodule UserDocsWeb.UserLive.LoginFormComponent do
+  use UserDocsWeb, :live_component
+
+  @impl true
+  def render(assigns) do
+    ~L"""
+      <%= form_for @changeset, Routes.login_path(@socket, :new), [as: :user], fn f -> %>
+        <%= if @changeset.action do %>
+          <div class="alert alert-danger">
+            <p>Oops, something went wrong! Please check the errors below.</p>
+          </div>
+        <% end %>
+
+        <%= label f, Pow.Ecto.Schema.user_id_field(@changeset) %>
+        <%= text_input f, Pow.Ecto.Schema.user_id_field(@changeset) %>
+        <%= error_tag f, Pow.Ecto.Schema.user_id_field(@changeset) %>
+
+        <%= label f, :password %>
+        <%= password_input f, :password %>
+        <%= error_tag f, :password %>
+
+        <div>
+          <%= submit "Sign in" %>
+        </div>
+      <% end %>
+    """
+  end
+end
