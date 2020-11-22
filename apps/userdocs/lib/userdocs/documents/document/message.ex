69c395ba209d632588a8be110ae6f9ab7d6e5d45
John Davenport <johns10@gmail.com>
2020-11-22 11:25:20 -0500

    Added Document Message (uses params)

---
+++ apps/userdocs/lib/userdocs/documents/document/message.ex
@@ -0,0 +1,32 @@
+defmodule UserDocs.Document.Messages do
+
+  alias UserDocs.Users
+  alias UserDocs.Documents.Document
+  alias UserDocs.Helpers
+
+  def new_modal_menu(socket, parent, projects) do
+    %{ target: "ModalMenus" }
+    |> init(socket, parent, projects)
+    |> new(socket)
+  end
+
+  defp new(message, _socket) do
+    message
+    |> Map.put(:object, %Document{})
+    |> Map.put(:action, :new)
+    |> Map.put(:title, "New Document")
+  end
+
+  defp init(message, _socket, parent, projects) do
+    select_lists = %{
+      projects:
+        projects
+        |> Helpers.select_list(:name, false),
+    }
+
+    message
+    |> Map.put(:type, :document)
+    |> Map.put(:parent, parent)
+    |> Map.put(:select_lists, select_lists)
+  end
+end
