2a960c2551d219fbf5ee8ae675518f9a8138bfb9
John Davenport <johns10@gmail.com>
2020-11-24 18:33:39 -0500

    Improved catching of invalid state in get

--- apps/state_handlers/lib/state_handlers/get.ex
+++ apps/state_handlers/lib/state_handlers/get.ex
@@ -10,15 +10,17 @@ defmodule StateHandlers.Get do
     |> get(id, opts[:data_type], opts[:type])
   end
 
-  def get(state, id, :list, _) do
+  def get(state, id, :list, _) when is_list(state) do
+    IO.inspect(state)
     state
     |> Enum.filter(fn(o) -> o.id == id end)
     |> Enum.at(0)
   end
-
-  def get(state, id, :map, _) do
+  def get(state, id, :map, _) when is_map(state) do
     Map.get(state, id)
   end
+  def get(state, _, _, _), do: raise(RuntimeError, "StateHandlers.get got an unexpected value in state: #{state}")
+
 
   def get(state, id, :by_key, type) do
     Map.get(state, Helpers.id_key(type, id))
