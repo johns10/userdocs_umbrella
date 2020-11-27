c2aaa9e7d1d63999d90505af05b40d59ddb9ce36
John Davenport <johns10@gmail.com>
2020-11-27 18:09:58 -0500

    Refactored event handlers to seperate Module

--- apps/userdocs_web/lib/userdocs_web/live/document_version_live/form_component.ex
+++ apps/userdocs_web/lib/userdocs_web/live/document_version_live/form_component.ex
@@ -2,8 +2,8 @@ defmodule UserDocsWeb.DocumentVersionLive.FormComponent do
   use UserDocsWeb, :live_component
 
   alias UserDocs.Documents
-  alias UserDocs.Documents.DocumentVersion
   alias UserDocsWeb.Layout
+  alias UserDocsWeb.DocumentVersionLive.EventHandlers
 
   @impl true
   def update(%{document_version: document_version} = assigns, socket) do
@@ -18,40 +18,5 @@ defmodule UserDocsWeb.DocumentVersionLive.FormComponent do
   end
 
   @impl true
-  def handle_event("validate", %{"document_version" => document_version_params}, socket) do
-    changeset =
-      socket.assigns.document_version
-      |> Documents.change_document_version(document_version_params)
-      |> Map.put(:action, :validate)
-
-    {:noreply, assign(socket, :changeset, changeset)}
-  end
-
-  def handle_event("save", %{"document_version" => document_version_params}, socket) do
-    save_document_version(socket, socket.assigns.action, document_version_params)
-  end
-
-  defp save_document_version(socket, :edit, document_version_params) do
-    case Documents.update_document_version(socket.assigns.document_version, document_version_params) do
-      {:ok, _document_version} ->
-        send(self(), :close_modal)
-        {:noreply, put_flash(socket, :info, "Document Version updated successfully") }
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, :changeset, changeset)}
-    end
-  end
-
-  defp save_document_version(socket, :new, document_version_params) do
-    case Documents.create_document_version(document_version_params) do
-      {:ok, document_version} ->
-        send(self(), :close_modal)
-        UserDocsWeb.Endpoint.broadcast(socket.assigns.channel, "create", document_version)
-        {:noreply, put_flash(socket, :info, "Document Version created successfully")}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        IO.puts("Failed Save")
-        {:noreply, assign(socket, changeset: changeset)}
-    end
-  end
+  def handle_event(n, p, s), do: EventHandlers.handle_event(n, p, s)
 end
