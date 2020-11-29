670bc45bc53a15f774efe64aa55be4df179532f2
John Davenport <johns10@gmail.com>
2020-11-29 11:50:20 -0500

    Refactored row with explicit add button

--- apps/userdocs/lib/userdocs/documents/docubit/renderers/row.ex
+++ apps/userdocs/lib/userdocs/documents/docubit/renderers/row.ex
@@ -1,13 +1,27 @@
 defmodule UserDocs.Documents.Docubit.Renderers.Row do
   require Logger
   use Phoenix.HTML
+  alias UserDocs.Documents.Docubit.Renderers.DocubitEditor
 
-  def render(assigns, :editor) do
+  def render(assigns, content, :editor) do
     content_tag(:div, [
-      class: "columns",
-      address: assigns.docubit.address
+      class: "columns has-background-light",
+      address: inspect(assigns.address)
     ]) do
-      assigns.docubit.content
+      DocubitEditor.container(assigns) do
+        [
+          content,
+          content_tag(:div, class: "") do
+            link(to: "#", class: "button",
+              phx_click: "create-docubit",
+              phx_value_id: assigns.id,
+              phx_value_docubit_type: "column"
+            ) do
+              "+"
+            end
+          end
+        ]
+      end
     end
   end
 end
