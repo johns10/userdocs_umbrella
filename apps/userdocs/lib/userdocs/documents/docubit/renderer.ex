2342669c0824d3cf4926b20d0ee77634a8ae41fe
John Davenport <johns10@gmail.com>
2020-11-29 11:49:19 -0500

    Removed requirement for type (use type_id)

--- apps/userdocs/lib/userdocs/documents/docubit/renderer.ex
+++ apps/userdocs/lib/userdocs/documents/docubit/renderer.ex
@@ -3,7 +3,7 @@ defmodule UserDocs.Documents.Docubit.Renderer do
   alias UserDocs.Documents.Docubit, as: Docubit
 
   def apply(docubit = %Docubit{}) do
-    Map.put(docubit, :renderer, fetch_renderer(docubit.type.id))
+    Map.put(docubit, :renderer, fetch_renderer(docubit.type_id))
   end
 
   defp fetch_renderer(type_id) do
