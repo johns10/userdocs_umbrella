3ff3de8b4197c4abb0c7209ca73c1f4ceb12619e
John Davenport <johns10@gmail.com>
2020-11-29 19:38:24 -0500

    Fixed Map and tightened up tests

--- apps/userdocs/test/userdocs/document_body_test.exs
+++ apps/userdocs/test/userdocs/document_body_test.exs
@@ -17,6 +17,38 @@ defmodule UserDocs.DocumentVersionBodyTest do
 
     def empty_document_version(), do: DocumentVersionFixtures.empty_document_version
 
+    def state_opts() do
+      [ data_type: :list, strategy: :by_type, location: :data ]
+    end
+
+    def document_version_with_one_row do
+      document_version = empty_document_version()
+      body = Documents.get_docubit!(document_version.body.id, %{docubits: true})
+      row =
+        body
+        |> add_row(document_version.id)
+        |> Map.get(:docubits)
+        |> Enum.at(0)
+
+      Documents.get_document_version!(document_version.id, %{ docubits: true })
+    end
+
+    def document_version_with_one_column_and_one_row do
+      document_version = empty_document_version()
+      body = Documents.get_docubit!(document_version.body.id, %{docubits: true})
+      row =
+        body
+        |> add_row(document_version.id)
+        |> Map.get(:docubits)
+        |> Enum.at(0)
+
+      row =
+        Documents.get_docubit!(row.id, %{docubits: true})
+        |> add_column(document_version.id)
+
+      Documents.get_document_version!(document_version.id, %{ docubits: true })
+    end
+
     def document_version_with_columns_and_rows do
       document_version = empty_document_version()
       body = Documents.get_docubit!(document_version.body.id, %{docubits: true})
@@ -45,12 +77,28 @@ defmodule UserDocs.DocumentVersionBodyTest do
       Documents.get_document_version!(document_version.id, %{ docubits: true })
     end
 
+    def add_row(docubit, document_version_id) do
+      attrs = %{ docubits: [
+        DocubitFixtures.docubit_attrs(:row, document_version_id)
+      ] }
+      { :ok, docubit } = Documents.update_docubit(docubit, attrs)
+      docubit
+    end
+
     def add_rows(docubit, document_version_id) do
       attrs = %{ docubits: [
         DocubitFixtures.docubit_attrs(:row, document_version_id),
         DocubitFixtures.docubit_attrs(:row, document_version_id),
         DocubitFixtures.docubit_attrs(:row, document_version_id),
       ] }
+      { :ok, docubit } = Documents.update_docubit(docubit, attrs)
+      docubit
+    end
+
+    def add_column(docubit, document_version_id) do
+      attrs = %{ docubits: [
+        DocubitFixtures.docubit_attrs(:column, document_version_id)
+      ]}
       { :ok, docubit } = Documents.update_docubit(docubit, attrs)
       docubit
     end
@@ -134,13 +182,39 @@ defmodule UserDocs.DocumentVersionBodyTest do
       document_version = document_version_with_columns_and_rows()
       assert Enum.count(document_version.docubits) == 7
     end
-    """
-    test "map_docubits maps the docubits" do
-      document_version = document_version_with_columns_and_rows()
+
+    test "map_docubits base case" do
+      document_version_attrs = DocumentVersionFixtures.document_version_attrs(:valid)
+      { :ok, document_version } = Documents.create_document_version(document_version_attrs)
+      document_version = Documents.get_document_version!(document_version.id, %{docubits: true})
       docubit_map = DocumentVersion.map_docubits(document_version)
-      IO.inspect(docubit_map)
       assert docubit_map[0].docubit.id == document_version.docubit_id
-      Enum.each(docubit_map[0][0], fn({ k, v }) ->
+    end
+
+    test "map_docubits single row" do
+      document_version = document_version_with_one_row()
+      document_version = Documents.get_document_version!(document_version.id, %{docubits: true})
+      docubit_map = DocumentVersion.map_docubits(document_version)
+      row = document_version.docubits |> Enum.at(1)
+      assert row.type_id == "row"
+      assert docubit_map[0].docubit.id == document_version.docubit_id
+      assert docubit_map[0].docubit.docubits[0].docubit.id == row.id
+    end
+
+    test "map_docubits single row, single column" do
+      document_version = document_version_with_one_column_and_one_row()
+      docubit_map = DocumentVersion.map_docubits(document_version)
+      row = document_version.docubits |> Enum.at(1)
+      column = document_version.docubits |> Enum.at(2)
+      assert docubit_map[0].docubit.id == document_version.docubit_id
+      assert docubit_map[0].docubit.docubits[0].docubit.docubits[0].docubit.id == column.id
+    end
+
+    test "map_docubits maps the docubits" do
+      document_version = document_version_with_one_column_and_one_row()
+      docubit_map = DocumentVersion.map_docubits(document_version)
+      assert docubit_map[0].docubit.id == document_version.docubit_id
+      Enum.each(docubit_map[0].docubit.docubits[0], fn({ k, v }) ->
         case is_integer(k) do
           true ->
             docubit = Documents.get_docubit!(Map.get(v, :id))
@@ -149,6 +223,17 @@ defmodule UserDocs.DocumentVersionBodyTest do
         end
       end)
     end
-    """
+
+    test "traverse_document_body bodies the docubits" do
+      # TODO: Fixt this.  use the loader
+      document_version = document_version_with_columns_and_rows()
+      state = Map.put(DocubitFixtures.state(), :docubits, document_version.docubits)
+      state = Map.put(%{}, :data, state)
+      document_version = DocumentVersion.load(document_version, state, state_opts)
+      row = Enum.at(document_version.body.docubits, 0)
+      column = Enum.at(row.docubits, 0)
+      assert row.type_id == "row"
+      assert column.type_id == "column"
+    end
   end
 end
