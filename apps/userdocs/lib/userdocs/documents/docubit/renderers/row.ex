91daae49a4410fb2e4b6d5f33faaddfa85dbaeeb
John Davenport <johns10@gmail.com>
2020-11-29 19:36:35 -0500

    These aren't finalized

--- apps/userdocs/lib/userdocs/documents/docubit/renderers/row.ex
+++ apps/userdocs/lib/userdocs/documents/docubit/renderers/row.ex
@@ -5,22 +5,24 @@ defmodule UserDocs.Documents.Docubit.Renderers.Row do
 
   def render(assigns, content, :editor) do
     content_tag(:div, [
-      class: "columns has-background-light",
+      class: "column",
       address: inspect(assigns.address)
     ]) do
       DocubitEditor.container(assigns) do
-        [
-          content,
-          content_tag(:div, class: "") do
-            link(to: "#", class: "button",
-              phx_click: "create-docubit",
-              phx_value_id: assigns.id,
-              phx_value_docubit_type: "column"
-            ) do
-              "+"
+        content_tag(:div, [ class: "columns is-gapless" ]) do
+          [
+            content,
+            content_tag(:div, class: "column is-narrow") do
+              link(to: "#", class: "button",
+                phx_click: "create-docubit",
+                phx_value_id: assigns.id,
+                phx_value_docubit_type: "column"
+              ) do
+                "+"
+              end
             end
-          end
-        ]
+          ]
+        end
       end
     end
   end
