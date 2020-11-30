5cfc285b8a1dfcf2683b37ab63a5f20fa74605cf
John Davenport <johns10@gmail.com>
2020-11-29 19:36:55 -0500

    Stil;l in flux

--- apps/userdocs/lib/userdocs/documents/docubit/renderers/docubit_editor.ex
+++ apps/userdocs/lib/userdocs/documents/docubit/renderers/docubit_editor.ex
@@ -2,8 +2,8 @@ defmodule UserDocs.Documents.Docubit.Renderers.DocubitEditor do
   require Logger
   use Phoenix.HTML
 
-  def container(_assigns, do: content) do
-    content_tag(:div, [ class: "column" ]) do
+  def container(assigns, do: content) do
+    content_tag(:div, [ class: "px-1 py-0 has-background-light" ]) do
       [
         content_tag(:nav, [
           class: "navbar",
@@ -11,13 +11,19 @@ defmodule UserDocs.Documents.Docubit.Renderers.DocubitEditor do
           role: "navigation",
           aria_label: "dropdown navigation"
         ]) do
-          content_tag(:div, [class: "navbar-end"]) do
-            content_tag(:div, [class: "navbar-item has-dropdown"]) do
-              content_tag(:a, [class: "navbar-link py-0"]) do
-                content_tag(:i, "", [class: "fa fa-gear"])
+          [
+
+            content_tag(:div, [ class: "py-1 navbar-item" ]) do
+              assigns.type_id
+            end,
+            content_tag(:div, [class: "navbar-end"]) do
+              content_tag(:div, [class: "navbar-item has-dropdown"]) do
+                content_tag(:a, [class: "navbar-link py-0"]) do
+                  content_tag(:i, "", [class: "fa fa-gear"])
+                end
               end
             end
-          end
+          ]
         end,
         content
       ]
