ac32f5ba7b0e3953951e5aa57581c8f14e87319f
John Davenport <johns10@gmail.com>
2020-11-29 19:36:35 -0500

    These aren't finalized

--- apps/userdocs/lib/userdocs/documents/docubit/renderers/column.ex
+++ apps/userdocs/lib/userdocs/documents/docubit/renderers/column.ex
@@ -1,13 +1,26 @@
 defmodule UserDocs.Documents.Docubit.Renderers.Column do
   require Logger
   use Phoenix.HTML
+  alias UserDocs.Documents.Docubit.Renderers.DocubitEditor
 
   def render(assigns, content, :editor) do
     content_tag(:div, [
       class: "column",
       address: assigns.address
     ]) do
-      [ "column", content ]
+      DocubitEditor.container(assigns) do
+        [
+          content,
+          link(to: "#",
+            class: "button is-fullwidth py-0 px-0",
+            phx_click: "create-docubit",
+            phx_value_id: assigns.id,
+            phx_value_docubit_type: ""
+          ) do
+            "+"
+          end
+        ]
+      end
     end
   end
 end
--- apps/userdocs/lib/userdocs/documents/docubit/renderers/container.ex
+++ apps/userdocs/lib/userdocs/documents/docubit/renderers/container.ex
@@ -2,16 +2,18 @@ defmodule UserDocs.Documents.Docubit.Renderers.Container do
   require Logger
   use Phoenix.HTML
 
-  def render(assigns, :editor) do
-    content_tag(:div, [ class: "container is-widescreen" ]) do
-      "Container"
-      link(to: "#", class: "button",
-        phx_click: "create-docubit",
-        phx_value_id: assigns.id,
-        phx_value_docubit_type: "row"
-      ) do
-        "+"
-      end
+  def render(assigns, content, :editor) do
+    content_tag(:div, [ class: "container" ]) do
+      [
+        content,
+        link(to: "#", class: "button",
+          phx_click: "create-docubit",
+          phx_value_id: assigns.id,
+          phx_value_docubit_type: "row"
+        ) do
+          "+"
+        end
+      ]
     end
   end
 end
