57c12c773580eedcd7d7b5d5775492ef04e68f01
John Davenport <johns10davenport@gmail.com>
2020-12-01 10:41:42 -0500

    Modified renderer to return the renderer function

--- apps/userdocs/lib/userdocs/documents/docubit/renderer.ex
+++ apps/userdocs/lib/userdocs/documents/docubit/renderer.ex
@@ -3,11 +3,12 @@ defmodule UserDocs.Documents.Docubit.Renderer do
   alias UserDocs.Documents.Docubit, as: Docubit
 
   def apply(docubit = %Docubit{}) do
-    Map.put(docubit, :renderer, fetch_renderer(docubit.type_id))
+    fetch_renderer(docubit.type_id)
   end
 
   defp fetch_renderer(type_id) do
-    "Elixir.UserDocs.Documents.Docubit.Renderers."
+    IO.inspect(type_id)
+    "Elixir.UserDocsWeb.DocubitLive.Renderers."
     <> String.capitalize(type_id)
     |> String.to_existing_atom()
   end
