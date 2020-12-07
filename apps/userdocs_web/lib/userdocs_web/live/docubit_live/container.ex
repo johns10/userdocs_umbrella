4ac624f585fb7ae9a64e04de87c37993d95942f5
John Davenport <johns10davenport@gmail.com>
2020-12-07 18:19:30 -0500

    Added container component

---
+++ apps/userdocs_web/lib/userdocs_web/live/docubit_live/container.ex
@@ -0,0 +1,19 @@
+defmodule UserDocsWeb.DocubitLive.Renderers.Container do
+  use UserDocsWeb, :live_component
+  alias UserDocsWeb.DocubitLive.AddDocubitButton
+
+
+  def render(assigns) do
+    ~L"""
+      <div class="container">
+        <%= @inner_content.([]) %>
+        <%= AddDocubitButton.render(%{
+          text: "+",
+          class: "button",
+          parent_cid: @parent_cid,
+          docubit: @docubit,
+          type_id: "row"}) %>
+      </div>
+    """
+  end
+end
