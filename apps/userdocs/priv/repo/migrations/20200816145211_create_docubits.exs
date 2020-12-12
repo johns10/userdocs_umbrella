5dc19f345ad32da8ab56bb2066f6f02d06cd315b
John Davenport <johns10davenport@gmail.com>
2020-12-11 20:20:13 -0500

    updated to use DocubitType and settings to map

--- apps/userdocs/priv/repo/migrations/20200816145211_create_docubits.exs
+++ apps/userdocs/priv/repo/migrations/20200816145211_create_docubits.exs
@@ -1,21 +1,22 @@
-defmodule UserDocs.Repo.Migrations.CreateDocubits do
+defmodule UserDocs.Repo.Migrations.CreateDocubitTypes do
   use Ecto.Migration
 
   def change do
     create table(:docubits) do
-      add :type_id, :string
+      add :docubit_type_id, references(:docubit_types, on_delete: :nothing)
       add :docubit_id, references(:docubits, on_delete: :nothing)
       add :content_id, references(:content, on_delete: :nothing)
       add :file_id, references(:files, on_delete: :nothing)
       add :through_annotation_id, references(:annotations, on_delete: :nothing)
       add :through_step_id, references(:steps, on_delete: :nothing)
-      add :settings, {:array, :map}
+      add :settings, :map
       add :address, {:array, :integer}
       add :order, :integer
 
       timestamps()
     end
 
+    create index(:docubits, [:docubit_type_id])
     create index(:docubits, [:content_id])
     create index(:docubits, [:file_id])
     create index(:docubits, [:through_annotation_id])
