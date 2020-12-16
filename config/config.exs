f94c2c1baf08a7678c64874bf18726ae900b60a7
John Davenport <johns10@gmail.com>
2020-12-15 18:09:13 -0600

    modified:   ../../../config/config.exs

--- config/config.exs
+++ config/config.exs
@@ -21,7 +21,10 @@ config :userdocs_web,
 
 # Configures the endpoint
 config :userdocs_web, UserDocsWeb.Endpoint,
-  url: [host: "localhost"],
+  url: [
+    host: "localhost",
+    host: "userdocs.gigalixirapp.com"
+    ],
   secret_key_base: "uYNO0z5S7TuzLxe//ihPPyhDY+9/juUgfW4fFJkw+nKlP/omZmmVMmcIWuBolrzY",
   render_errors: [view: UserDocsWeb.ErrorView, accepts: ~w(html json), layout: false],
   pubsub_server: UserDocs.PubSub,
