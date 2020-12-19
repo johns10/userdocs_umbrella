cd7e6324e4146049503c8452f30c3444a66035a7
John Davenport <johns10davenport@gmail.com>
2020-12-19 10:42:05 -0600

    WIP changes

--- apps/userdocs_web/lib/userdocs_web/live/docubit_live/docubit_editor.ex
+++ apps/userdocs_web/lib/userdocs_web/live/docubit_live/docubit_editor.ex
@@ -3,6 +3,7 @@ defmodule UserDocsWeb.DocubitEditorLive do
 
   alias UserDocsWeb.DocubitEditorLive
   alias UserDocsWeb.Defaults
+  alias UserDocsWeb.Root
   alias UserDocs.Documents.Docubit
   alias UserDocs.Documents
 
@@ -18,28 +19,37 @@ defmodule UserDocsWeb.DocubitEditorLive do
   @impl true
   def update(assigns, socket) do
     IO.puts("Running Docubit Editor Update")
+    docubit = assigns.docubit
+
     preloads = [
-      :file,
-      :through_annotation,
       :docubits,
-      :through_step,
-      :content,
-      [ content: :content_versions ]
+      [ docubits: :content ],
+      [ docubits: :file ],
+      [ docubits: :through_annotation ],
+      [ docubits: :through_step ],
+      [ docubits: :docubit_type ],
+      [ docubits: [ content: :content_versions ] ],
     ]
+
     opts =
       assigns.opts
-      |> Keyword.put(:filter, { :docubit_id, assigns.docubit.id })
       |> Keyword.put(:preloads, preloads)
-      |> Keyword.put(:order, [ docubits:  %{ field: :order, order: :asc } ])
+      |> Keyword.put(:order, docubits: %{field: :order, order: :asc})
 
-    docubit = Documents.get_docubit!(assigns.docubit.id, assigns, opts)
+    preloaded_docubit = Documents.get_docubit!(assigns.docubit.id, assigns, opts)
+
+    docubits =
+      preloaded_docubit.docubits
+      |> Enum.map(fn(d) -> Docubit.apply_context(d, docubit.context) end)
+
+    docubit = Map.put(docubit, :docubits, docubits)
 
     {
       :ok,
       socket
       |> assign(assigns)
       |> assign(:docubit, docubit)
-      |> assign(:renderer, Docubit.renderer(assigns.docubit))
+      |> assign(:renderer, Docubit.renderer(docubit))
     }
   end
 
@@ -54,7 +64,7 @@ defmodule UserDocsWeb.DocubitEditorLive do
         id=<%= @id %>
       >
         <div class="is-flex is-flex-direction-row is-justify-content-space-between py-1">
-          <div><%= @docubit.type_id %></div>
+          <div><%= @docubit.docubit_type.name %></div>
           <div class="py-0">
             <div class="dropdown is-active">
               <i class="fa fa-gear"
@@ -68,13 +78,17 @@ defmodule UserDocsWeb.DocubitEditorLive do
           </div>
         </div>
         <%= live_component(@socket, @renderer, [
+          current_language_code_id: @current_language_code_id,
+          current_version_id: @current_version_id,
           docubit: @docubit,
           parent_cid: @myself.cid,
-          id: "docubit-" <> @docubit.type_id <> Integer.to_string(@docubit.id)
+          id: "docubit-" <> @docubit.docubit_type.name <> Integer.to_string(@docubit.id)
         ]) do %>
           <%= for docubit <- @docubit.docubits do %>
             <%= live_component(@socket, DocubitEditorLive, [
               id: "docubit-editor-" <> Integer.to_string(docubit.id),
+              current_language_code_id: @current_language_code_id,
+              current_version_id: @current_version_id,
               current_team_id: @current_team_id,
               docubit: docubit,
               opts: @opts,
@@ -99,34 +113,49 @@ defmodule UserDocsWeb.DocubitEditorLive do
             phx-value-id=<%= @docubit.id %>>
             Delete
           </a>
+          <a href="#"
+            class="dropdown-item"
+            phx-click="edit-docubit"
+            phx-target=<%= @parent_cid %>
+            phx-value-id=<%= @docubit.id %>>
+            Settings
+          </a>
         </div>
       </div>
     """
   end
 
-  def handle_event("delete-docubit", %{"id" => id}, socket) do
+  def handle_event("delete-docubit", %{"id" => id }, socket) do
     id = String.to_integer(id)
+
     attrs = %{
       id: socket.assigns.docubit.id,
-      docubits: Enum.map(socket.assigns.docubit.docubits,
-        fn(docubit) ->
-          case docubit.id == id do
-            true -> Map.take(docubit, Docubit.__schema__(:fields)) |> Map.put(:delete, true)
-            false -> Map.take(docubit, Docubit.__schema__(:fields))
+      document_version_id: socket.assigns.docubit.document_version_id,
+      docubits:
+        Enum.map(
+          socket.assigns.docubit.docubits,
+          fn docubit ->
+            case docubit.id == id do
+              true -> Map.take(docubit, UserDocs.Documents.Docubit.__schema__(:fields)) |> Map.put(:delete, true)
+              false -> Map.take(docubit, Docubit.__schema__(:fields))
+            end
           end
-        end
-      )
+        )
     }
+
     case Documents.delete_docubit_from_docubits(socket.assigns.docubit, attrs) do
-      { :error, _changeset } ->
+      {:error, changeset} ->
         IO.puts("Delete failed")
+
         {
           :noreply,
           socket
           |> put_flash(:info, "Deleting Docubit Failed")
         }
-      { deleted_docubit, _ } ->
+
+      {deleted_docubit, _} ->
         UserDocsWeb.Endpoint.broadcast(Defaults.channel(socket), "delete", deleted_docubit)
+
         {
           :noreply,
           socket
@@ -134,34 +163,71 @@ defmodule UserDocsWeb.DocubitEditorLive do
         }
     end
   end
-  def handle_event("create-docubit", %{"type-id" => type_id, "docubit-id" => docubit_id}, socket) do
-    send(self(), {:create_docubit, %{ type_id: type_id, docubit: socket.assigns.docubit }})
-    { :noreply, socket }
+  def handle_event("create-docubit", %{"type" => type, "docubit-id" => _docubit_id}, socket) do
+    send(self(), {:create_docubit, %{type: type, docubit: socket.assigns.docubit}})
+    {:noreply, socket}
   end
-  def handle_event("docubit_drop", %{"docubit-id" => docubit_id, "object-id" => object_id, "type" => type}, socket)  do
+
+  def handle_event(
+        "docubit_drop",
+        %{"docubit-id" => _docubit_id, "object-id" => object_id, "type" => type},
+        socket
+      ) do
     schema =
       case type do
         "Step" -> UserDocs.Automation.Step
         "Annotation" -> UserDocs.Web.Annotation
         "Content" -> UserDocs.Documents.Content
       end
-    object = StateHandlers.get(socket, String.to_integer(object_id), schema, socket.assigns.opts)
-    docubit = Documents.get_docubit!(String.to_integer(docubit_id), socket, socket.assigns.opts)
-    IO.inspect(object)
-    IO.inspect(docubit)
-    IO.inspect(
-      UserDocs.Documents.Docubit.Hydrate.apply(docubit, object)
+
+    preloads =
+      case type do
+        "Content" -> [:annotation, :content_versions]
+        "Step" -> []
+        "Annotation" -> []
+      end
+
+    opts =
+      socket.assigns.opts
+      |> Keyword.put(:preloads, preloads)
+
+    object = StateHandlers.get(socket, String.to_integer(object_id), schema, opts)
+    object = StateHandlers.preload(socket, object, opts)
+    docubit = socket.assigns.docubit
+
+    docubit =
+      case Docubit.hydrate(docubit, object) do
+        %Docubit{} = docubit -> docubit
+        { :error, message } -> IO.puts(inspect(message))
+      end
+
+    Phoenix.LiveView.send_update(
+      socket.assigns.renderer,
+      current_language_code_id: socket.assigns.current_language_code_id,
+      current_version_id: socket.assigns.current_version_id,
+      docubit: docubit,
+      parent_cid: self(),
+      id: "docubit-" <> docubit.docubit_type.name <> Integer.to_string(docubit.id)
     )
 
     {
       :noreply,
       socket
+      |> assign(:docubit, docubit)
     }
   end
-
-  @impl true
   def handle_event("display-settings-menu", _, socket) do
     IO.puts("Create Docubit")
-    { :noreply, assign(socket, :display_settings_menu, not socket.assigns.display_settings_menu) }
+    {:noreply, assign(socket, :display_settings_menu, not socket.assigns.display_settings_menu)}
   end
+  def handle_event("edit-docubit" = name, %{"id" => id}, socket) do
+    params =
+      %{}
+      |> Map.put(:docubit, socket.assigns.docubit)
+      |> Map.put(:channel, Defaults.channel(socket))
+      |> Map.put(:opts, socket.assigns.opts)
+
+    Root.handle_event(name, params, socket)
+  end
+  def handle_event(n, p, s), do: Root.handle_event(n, p, s)
 end
