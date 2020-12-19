63014e3ce92af19bf58661ce3e340e8b76123071
John Davenport <johns10davenport@gmail.com>
2020-12-19 09:21:25 -0600

    Added add docubit button for li

--- apps/userdocs_web/lib/userdocs_web/live/docubit_live/ul.ex
+++ apps/userdocs_web/lib/userdocs_web/live/docubit_live/ul.ex
@@ -2,11 +2,20 @@ defmodule UserDocsWeb.DocubitLive.Renderers.Ul do
   use UserDocsWeb, :live_component
   use Phoenix.HTML
 
+  alias UserDocsWeb.DocubitLive.AddDocubitButton
 
   def render(assigns) do
     ~L"""
       <ul>
         <%= @inner_content.([]) %>
+        <div class="is-justify-self-flex-center is-flex-grow-0">
+          <%= AddDocubitButton.render(%{
+            text: "+",
+            class: "button",
+            parent_cid: @parent_cid,
+            docubit: @docubit,
+            type: "li"}) %>
+        </div>
       </ul>
     """
   end
