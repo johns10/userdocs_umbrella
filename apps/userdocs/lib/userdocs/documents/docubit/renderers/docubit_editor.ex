651860dd14c6fd5f900b4e865720745248358c45
John Davenport <johns10davenport@gmail.com>
2020-12-19 10:29:36 -0600

    fixed css

--- apps/userdocs/lib/userdocs/documents/docubit/renderers/docubit_editor.ex
+++ apps/userdocs/lib/userdocs/documents/docubit/renderers/docubit_editor.ex
@@ -2,26 +2,21 @@ defmodule UserDocs.Documents.Docubit.Renderers.DocubitEditor do
   require Logger
   use Phoenix.HTML
 
-  def container(assigns, do: content) do
-    content_tag(:div, [ class: "px-1 py-0 has-background-light" ]) do
+  def render(assigns, do: content) do
+    content_tag(:div, [
+      class: "px-1 py-0 has-background-light",
+      id: "docubit-" <> String.to_integer(assigns.docubit.id)
+    ]) do
       [
-        content_tag(:nav, [
-          class: "navbar",
-          height: "1px",
-          role: "navigation",
-          aria_label: "dropdown navigation"
-        ]) do
+        content_tag(:div, [
+          class: "is-flex is-flex-direction-row is-justify-content-space-between py-1" ]) do
           [
 
-            content_tag(:div, [ class: "py-1 navbar-item" ]) do
+            content_tag(:div, [ class: "" ]) do
               assigns.type_id
             end,
-            content_tag(:div, [class: "navbar-end"]) do
-              content_tag(:div, [class: "navbar-item has-dropdown"]) do
-                content_tag(:a, [class: "navbar-link py-0"]) do
-                  content_tag(:i, "", [class: "fa fa-gear"])
-                end
-              end
+            content_tag(:a, [class: "py-0"]) do
+              content_tag(:i, "", [class: "fa fa-gear"])
             end
           ]
         end,
