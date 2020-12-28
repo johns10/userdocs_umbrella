353a2d9847ccbd9d5d134f92e2134cf611e89a8d
John Davenport <johns10davenport@gmail.com>
2020-12-28 11:58:51 -0600

    Added to support context refactor

--- apps/userdocs/lib/userdocs/documents/docubit/ecto_context.ex
+++ apps/userdocs/lib/userdocs/documents/docubit/ecto_context.ex
@@ -1,10 +1,11 @@
 defmodule EctoContext do
   use Ecto.Type
-  def type, do: :jsonb
+  alias UserDocs.Documents.Docubit.Context
+  def type, do: :map
 
   # Provide custom casting rules.
   # Cast strings into the URI struct to be used at runtime
-  def cast(context)  do
+  def cast(context) when is_map(context) do
     #IO.puts("Casting Ecto context")
     { :ok, context }
   end
@@ -12,26 +13,28 @@ defmodule EctoContext do
   # Loading data from the database
 
   # Loading data from the database
-  def load(%{}), do: { :ok, %{} }
+  def load(context = %{ "settings" => settings }) when is_map(context) and is_map(settings) do
+    #IO.puts("Loading Ecto context #{inspect(context)}")
+    settings =
+      case EctoSettings.load(settings) do
+        { :ok, settings } -> settings
+        _ -> raise(RuntimeError, "Uncaught error in #{__MODULE__}.load")
+      end
+    {:ok, struct!(Context, %{ settings: settings })}
+  end
   def load(context) do
-    #IO.puts("Loading context")
-    context
+    { :error, context }
   end
 
   # dumping data to the database
-  def dump(%{ settings: settings }) when is_map(settings) do
-    #IO.inspect("Dumping contexts")
+  def dump(context = %{ settings: settings }) when is_map(context) and is_map(settings) do
+    #IO.inspect("Dumping contexts #{inspect(context)}")
     settings =
-      Enum.reduce(settings, %{},
-        fn({ k, v }, settings) when is_atom(k) and is_atom(v) ->
-          Map.put(settings, Atom.to_string(k), Atom.to_string(v))
-        end
-      )
+      case EctoSettings.dump(settings) do
+        { :ok, settings } -> settings
+        _ -> raise(RuntimeError, "Uncaught error in #{__MODULE__}.dump")
+      end
     {:ok, %{ settings: settings }}
-  end
-  def dump(%{}) do
-    #IO.inspect("Dumping empty ap")
-    {:ok, %{}}
   end
   def dump(_) do
     #IO.inspect("Dumping ERROR")
---
+++ apps/userdocs/lib/userdocs/documents/docubit/ecto_settings.ex
@@ -0,0 +1,51 @@
+defmodule EctoSettings do
+  use Ecto.Type
+  alias UserDocs.Documents.DocubitSetting
+  def type, do: :jsonb
+
+  # Provide custom casting rules.
+  # Cast strings into the URI struct to be used at runtime
+  def cast(settings = %DocubitSetting{}) do
+    #IO.puts("Casting Ecto settings")
+    settings =
+      settings
+      |> Map.take(DocubitSetting.__schema__(:fields))
+
+    { :ok, settings }
+  end
+  def cast(settings) when is_map(settings)  do
+    #IO.puts("Casting Ecto map settings")
+    { :ok, settings }
+  end
+
+  # Loading data from the database
+
+  # Loading data from the database
+  def load(settings) when is_map(settings) do
+    #IO.puts("Loading Settings #{inspect(settings)}")
+    settings =
+      Enum.reduce(settings, %{},
+        fn({key, val}, acc) ->
+          Map.put(acc, String.to_existing_atom(key), handle_value(val))
+        end
+      )
+    {:ok, struct!(DocubitSetting, settings)}
+  end
+  def load(settings) do
+    { :error, settings }
+  end
+
+  def handle_value(nil), do: nil
+  def handle_value(value) when is_atom(value), do: value
+  def handle_value(value), do: String.to_existing_atom(value)
+
+  # dumping data to the database
+  def dump(settings) when is_map(settings) do
+    #IO.puts("Dumping settings")
+    {:ok, settings}
+  end
+  def dump(_) do
+    #IO.puts("Dumping ERROR")
+    :error
+  end
+end
