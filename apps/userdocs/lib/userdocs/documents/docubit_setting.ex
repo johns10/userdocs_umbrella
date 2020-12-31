555d73a5e2ec7b610ea12d59b1d6be1e1fedc78b
John Davenport <johns10davenport@gmail.com>
2020-12-31 16:23:30 -0600

    Added show_title, img args

--- apps/userdocs/lib/userdocs/documents/docubit_setting.ex
+++ apps/userdocs/lib/userdocs/documents/docubit_setting.ex
@@ -2,23 +2,34 @@ defmodule UserDocs.Documents.DocubitSetting do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @fields [
+    :li_value,
+    :name_prefix,
+    :show_title,
+    :img_border,
+    :border_width,
+    :border_color
+  ]
+
+
   @primary_key false
-  @derive {Jason.Encoder, only: [:li_value, :name_prefix]}
+  @derive {Jason.Encoder, only: @fields}
   embedded_schema do
     field :li_value, :string
     field :name_prefix, :boolean
+    field :show_title, :boolean
+    field :img_border, :boolean
+    field :border_width, :integer
+    field :border_color, :string
   end
 
   def changeset(docubit_type, attrs \\ %{}) do
     docubit_type
-    |> cast(attrs, [ :li_value, :name_prefix ])
+    |> cast(attrs, [ :li_value, :name_prefix, :show_title ])
   end
 
   def valid_settings() do
-    [
-      :li_value,
-      :name_prefix
-    ]
+    @fields
   end
 
   #def li_value(), do: Kernel.struct(DocubitSetting, li_value_attrs())
@@ -34,6 +45,38 @@ defmodule UserDocs.Documents.DocubitSetting do
     %{
       field_type: :select,
       select_options: [{ "False", false }, { "True", true }]
+    }
+  end
+
+  #def name_prefix(), do: Kernel.struct(DocubitSetting, name_prefix_attrs())
+  def show_title() do
+    %{
+      field_type: :select,
+      select_options: [{ "False", false }, { "True", true }]
+    }
+  end
+
+  #def name_prefix(), do: Kernel.struct(DocubitSetting, name_prefix_attrs())
+  def img_border() do
+    %{
+      field_type: :select,
+      select_options: [{ "False", false }, { "True", true }]
+    }
+  end
+
+  #def name_prefix(), do: Kernel.struct(DocubitSetting, name_prefix_attrs())
+  def border_width() do
+    %{
+      field_type: :number_input,
+      select_options: []
+    }
+  end
+
+  #def name_prefix(), do: Kernel.struct(DocubitSetting, name_prefix_attrs())
+  def border_color() do
+    %{
+      field_type: :text_input,
+      select_options: []
     }
   end
 
