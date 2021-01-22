3c83b4ec91dfa9e9f06489ea7087544b907ed318
John Davenport <johns10davenport@gmail.com>
2021-01-21 22:37:19 -0500

    Removed an inspect

--- apps/userdocs_web/lib/userdocs_web/live/docubit_live/renderers/image.ex
+++ apps/userdocs_web/lib/userdocs_web/live/docubit_live/renderers/image.ex
@@ -72,7 +72,6 @@ defmodule UserDocsWeb.DocubitLive.Renderers.Image do
     case content_versions |> Enum.at(0) do
       nil -> raise(RuntimeError, "Content Version Not Found in maybe_body")
       result ->
-        IO.inspect(content_name)
         ": "
         <> content_name
         <> " ("
