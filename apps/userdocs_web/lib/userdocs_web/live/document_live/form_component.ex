b22ee657968a432e2320692941ba96cd93217e46
John Davenport <johns10davenport@gmail.com>
2021-02-04 16:33:41 -0600

    Fixed test statements

--- apps/userdocs_web/lib/userdocs_web/live/document_live/form_component.ex
+++ apps/userdocs_web/lib/userdocs_web/live/document_live/form_component.ex
@@ -35,7 +35,6 @@ defmodule UserDocsWeb.DocumentLive.FormComponent do
     case Documents.update_document(socket.assigns.document, document_params) do
       {:ok, document} ->
         send(self(), :close_modal)
-        UserDocsWeb.Endpoint.broadcast(socket.assigns.channel, "update", document)
         {:noreply, put_flash(socket, :info, "Document updated successfully") }
 
       {:error, %Ecto.Changeset{} = changeset} ->
