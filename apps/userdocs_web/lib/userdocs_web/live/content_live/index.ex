e6e1cb2460d9a2ccd616771e2f467eedb21922e7
John Davenport <johns10davenport@gmail.com>
2021-02-04 18:14:36 -0600

    Fixed state opts

--- apps/userdocs_web/lib/userdocs_web/live/content_live/index.ex
+++ apps/userdocs_web/lib/userdocs_web/live/content_live/index.ex
@@ -1,5 +1,6 @@
 defmodule UserDocsWeb.ContentLive.Index do
   use UserDocsWeb, :live_view
+  import Phoenix.LiveView.Helpers, only: [sigil_L: 2]
   use UserdocsWeb.LiveViewPowHelper
 
   alias UserDocs.Users
@@ -14,15 +15,22 @@ defmodule UserDocsWeb.ContentLive.Index do
   alias UserDocsWeb.Loaders
   alias UserDocsWeb.ModalMenus
 
-  defp state_opts() do
+  defp base_opts() do
     Defaults.state_opts
     |> Keyword.put(:location, :data)
+  end
+
+  defp state_opts(socket) do
+    base_opts()
+    |> Keyword.put(:broadcast, true)
+    |> Keyword.put(:channel, UserDocsWeb.Defaults.channel(socket))
+    |> Keyword.put(:broadcast_function, &UserDocsWeb.Endpoint.broadcast/3)
   end
 
   @impl true
   def mount(_params, session, socket) do
     opts =
-      state_opts()
+      base_opts()
       |> Keyword.put(:types, [  Content, ContentVersion, LanguageCode ])
 
     {:ok,
@@ -35,13 +43,15 @@ defmodule UserDocsWeb.ContentLive.Index do
   end
 
   def initialize(%{ assigns: %{ auth_state: :logged_in }} = socket) do
+    opts = state_opts(socket)
     socket
-    |> load_language_codes(state_opts())
-    |> load_content(state_opts())
-    |> load_content_versions(state_opts())
+    |> load_language_codes(opts)
+    |> load_content(opts)
+    |> load_content_versions(opts)
     |> current_selections()
     |> prepare_content()
-    |> StateHandlers.inspect(state_opts())
+    |> assign(:state_opts, opts)
+    |> StateHandlers.inspect(opts)
   end
   def initialize(socket), do: socket
 
@@ -54,13 +64,13 @@ defmodule UserDocsWeb.ContentLive.Index do
     socket
     |> assign(:page_title, "Edit Content")
     |> initialize()
-    |> ModalMenus.edit_content(edit_params(socket, id, state_opts()))
+    |> ModalMenus.edit_content(edit_params(socket, id, state_opts(socket)))
   end
   defp apply_action(%{ assigns: %{ auth_state: :logged_in }} = socket, :new, _params) do
     socket
     |> assign(:page_title, "New Content")
     |> initialize()
-    |> ModalMenus.new_content(params(socket, state_opts()))
+    |> ModalMenus.new_content(params(socket, state_opts(socket)))
   end
   defp apply_action(socket, :index, _params) do
     socket
@@ -83,19 +93,21 @@ defmodule UserDocsWeb.ContentLive.Index do
     |> Map.put(:version_id, socket.assigns.current_version_id)
     |> Map.put(:versions, Projects.list_versions(socket, opts))
     |> Map.put(:content, Documents.list_content(socket, opts))
-    |> Map.put(:opts, opts)
+    |> Map.put(:state_opts, opts)
   end
 
   @impl true
   def handle_event("edit-content" = n, %{ "id" => id }, socket) do
-    Root.handle_event(n, edit_params(socket, id, state_opts()), socket)
+    Root.handle_event(n, edit_params(socket, id, state_opts(socket)), socket)
   end
   def handle_event("new-content" = n, _params, socket) do
-    Root.handle_event(n, params(socket, state_opts()), socket)
+    Root.handle_event(n, params(socket, state_opts(socket)), socket)
   end
   def handle_event("delete", %{"id" => id}, socket) do
     content = Documents.get_content!(id)
-    {:ok, _} = Documents.delete_content(content)
+    {:ok, deleted_content} = Documents.delete_content(content)
+    send(self(), { :broadcast, "delete", deleted_content })
+    send(self(), :close_modal)
     {:noreply, socket}
   end
   def handle_event("select-version" = n, p, s) do
@@ -115,9 +127,10 @@ defmodule UserDocsWeb.ContentLive.Index do
 
   defp prepare_content(socket) do
     opts =
-      state_opts()
+      state_opts(socket)
       |> Keyword.put(:filter, { :team_id, socket.assigns.current_team_id })
       |> Keyword.put(:preloads, [ :content_versions, [ content_versions: :version ] ])
+      |> Keyword.put(:order, [ %{ field: :id, order: :asc } ])
 
     socket
     |> assign(:content, Documents.list_content(socket, opts))
@@ -126,7 +139,7 @@ defmodule UserDocsWeb.ContentLive.Index do
   defp current_selections(socket) do
     language_code_id =
       socket.assigns.current_team_id
-      |> Users.get_team!(socket, state_opts())
+      |> Users.get_team!(socket, state_opts(socket))
       |> Map.get(:default_language_code_id)
 
     socket
