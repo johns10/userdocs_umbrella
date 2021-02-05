95a88efe63fda2ba5338ed33b78972db0abcd654
John Davenport <johns10davenport@gmail.com>
2021-02-04 18:04:37 -0600

    Updated to add state opts

--- apps/userdocs/lib/userdocs/documents/content/messages.ex
+++ apps/userdocs/lib/userdocs/documents/content/messages.ex
@@ -5,25 +5,25 @@ defmodule UserDocs.Documents.Content.Messages do
   alias UserDocs.Helpers
 
   def new_modal_menu(socket, params) do
-    required_keys = [ :version_id, :team, :teams, :language_codes, :versions, :content, :channel, :opts ]
+    required_keys = [ :version_id, :team, :teams, :language_codes, :versions, :content, :channel, :state_opts ]
     params = Helpers.validate_params(params, required_keys, __MODULE__)
 
     %{ target: "ModalMenus" }
     |> init(socket, params.team, params.teams, params.channel,
         params.version_id, params.language_codes, params.versions,
-        params.content)
+        params.content, params.state_opts)
     |> new(socket)
   end
 
   def edit_modal_menu(socket, params) do
-    required_keys = [ :version_id, :team, :teams, :language_codes, :versions, :content, :channel, :opts ]
+    required_keys = [ :version_id, :team, :teams, :language_codes, :versions, :content, :channel, :state_opts ]
     params = Helpers.validate_params(params, required_keys, __MODULE__)
 
     %{ target: "ModalMenus" }
     |> init(socket, params.team, params.teams, params.channel,
         params.version_id, params.language_codes, params.versions,
-        params.content)
-    |> edit(socket, params.content_id, params.opts)
+        params.content, params.state_opts)
+    |> edit(socket, params.content_id, params.state_opts)
   end
 
   defp new(message, _socket) do
@@ -33,18 +33,18 @@ defmodule UserDocs.Documents.Content.Messages do
     |> Map.put(:title, "New Document")
   end
 
-  defp edit(message, socket, content_id, opts) do
-    opts =
-      opts
+  defp edit(message, socket, content_id, state_opts) do
+    state_opts =
+      state_opts
       |> Keyword.put(:preloads, [ :content_versions ])
 
     message
-    |> Map.put(:object, Documents.get_content!(content_id, socket, opts))
+    |> Map.put(:object, Documents.get_content!(content_id, socket, state_opts))
     |> Map.put(:action, :edit)
     |> Map.put(:title, "Edit Document")
   end
 
-  defp init(message, _socket, team, teams, channel, version_id, language_codes, versions, content) do
+  defp init(message, _socket, team, teams, channel, version_id, language_codes, versions, content, state_opts) do
     select_lists = %{
       teams: Helpers.select_list(teams, :name, false),
       language_codes: Helpers.select_list(language_codes, :name, false),
@@ -58,5 +58,6 @@ defmodule UserDocs.Documents.Content.Messages do
     |> Map.put(:select_lists, select_lists)
     |> Map.put(:channel, channel)
     |> Map.put(:version_id, version_id)
+    |> Map.put(:state_opts, state_opts)
   end
 end
