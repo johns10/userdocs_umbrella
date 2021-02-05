c829e47bc98eb9d8a574074e4c3d5467575edb26
John Davenport <johns10davenport@gmail.com>
2021-02-04 19:08:27 -0600

    More slime configs

--- config/dev.exs
+++ config/dev.exs
@@ -72,7 +72,7 @@ config :userdocs_web, UserDocsWeb.Endpoint,
       # ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
       ~r"lib/userdocs_web/(live|views)/.*(ex)$",
-      ~r"lib/userdocs_web/templates/.*(eex)$"
+      ~r"lib/userdocs_web/templates/.*(eex|slim|slime|slimleex)$"
     ]
   ]
 
