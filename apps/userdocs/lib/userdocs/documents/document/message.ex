27136d2b16fb868f72351083040be6d91274c8c6
John Davenport <johns10davenport@gmail.com>
2021-02-04 18:04:58 -0600

    changed variable name to state opts

--- apps/userdocs/lib/userdocs/documents/document/message.ex
+++ apps/userdocs/lib/userdocs/documents/document/message.ex
@@ -5,7 +5,7 @@ defmodule UserDocs.Document.Messages do
   alias UserDocs.Helpers
 
   def new_modal_menu(socket, params) do
-    required_keys = [ :team, :projects, :channel, :opts]
+    required_keys = [ :team, :projects, :channel, :state_opts]
     params = Helpers.validate_params(params, required_keys, __MODULE__)
 
     %{ target: "ModalMenus" }
@@ -14,12 +14,12 @@ defmodule UserDocs.Document.Messages do
   end
 
   def edit_modal_menu(socket, params) do
-    required_keys = [ :team, :projects, :channel, :document_id, :opts]
+    required_keys = [ :team, :projects, :channel, :document_id, :state_opts]
     params = Helpers.validate_params(params, required_keys, __MODULE__)
 
     %{ target: "ModalMenus" }
     |> init(socket, params.team, params.projects, params.channel)
-    |> edit(socket, params.document_id, params.opts)
+    |> edit(socket, params.document_id, params.state_opts)
   end
 
   defp new(message, _socket) do
@@ -29,9 +29,9 @@ defmodule UserDocs.Document.Messages do
     |> Map.put(:title, "New Document")
   end
 
-  defp edit(message, socket, document_id, opts) do
+  defp edit(message, socket, document_id, state_opts) do
     message
-    |> Map.put(:object, Documents.get_document!(document_id, socket, opts))
+    |> Map.put(:object, Documents.get_document!(document_id, socket, state_opts))
     |> Map.put(:action, :edit)
     |> Map.put(:title, "Edit Document")
   end
