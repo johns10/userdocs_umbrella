62265fb669ef7c1a2e6388a407730f028dc11a25
John Davenport <johns10davenport@gmail.com>
2021-02-11 22:59:20 -0500

    added html_export changes

--- apps/userdocs_web/lib/userdocs_web/live/docubit_live/renderers/container.ex
+++ apps/userdocs_web/lib/userdocs_web/live/docubit_live/renderers/container.ex
@@ -5,19 +5,37 @@ defmodule UserDocsWeb.DocubitLive.Renderers.Container do
 
   def header(_), do: "Container"
 
+  def render(%{ role: :html_export } = assigns) do
+    ~L"""
+    <style>
+      .row {
+        display: flex;
+      }
+
+      .column {
+        flex: 50%;
+      }
+    </style>
+
+    <div class="container">
+      <%= Base.render_inner_content(assigns) %>
+    </div>
+    """
+  end
+
   def render(assigns) do
     ~L"""
-      <div class="container">
-        <%= Base.render_inner_content(assigns) %>
-        <%= if @editor do %>
-          <%= AddDocubitButton.render(%{
-            text: "+",
-            class: "button",
-            parent_cid: @parent_cid,
-            docubit: @docubit,
-            type: "row"}) %>
-        <% end %>
-      </div>
+    <div class="container">
+      <%= Base.render_inner_content(assigns) %>
+      <%= if @editor do %>
+        <%= AddDocubitButton.render(%{
+          text: "+",
+          class: "button",
+          parent_cid: @parent_cid,
+          docubit: @docubit,
+          type: "row"}) %>
+      <% end %>
+    </div>
     """
   end
 end
