c7bab3670ab37d769e134de5820d23bd46ded13b
John Davenport <johns10davenport@gmail.com>
2021-02-11 22:37:34 -0500

    update phoenix liveview and live view versions

--- apps/userdocs_web/mix.exs
+++ apps/userdocs_web/mix.exs
@@ -40,11 +40,11 @@ defmodule UserDocsWeb.MixProject do
     [
       {:phoenix, "~> 1.5.3"},
       {:phoenix_ecto, "~> 4.0"},
-      {:phoenix_live_view, "~> 0.14.7"},
+      {:phoenix_live_view, "~> 0.15.4"},
       {:floki, ">= 0.0.0", only: :test},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.7"},
+      {:phoenix_live_dashboard, "~> 0.4.0"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
