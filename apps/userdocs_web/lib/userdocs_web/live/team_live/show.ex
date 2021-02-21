50744ebdfeae1a790ad814bed01ec80ab64e0a73
John Davenport <johns10davenport@gmail.com>
2021-02-21 16:52:24 -0500

    Refactored for new app nav

--- apps/userdocs_web/lib/userdocs_web/live/team_live/show.ex
+++ apps/userdocs_web/lib/userdocs_web/live/team_live/show.ex
@@ -1,26 +1,53 @@
 defmodule UserDocsWeb.TeamLive.Show do
   use UserDocsWeb, :live_view
 
+  use UserdocsWeb.LiveViewPowHelper
+
   alias UserDocs.Users
 
-  @impl true
-  def mount(_params, _session, socket) do
-    socket =
-      socket
-      |> assign(:teams, Users.list_teams())
-      |> assign(:users, Users.list_users())
+  alias UserDocsWeb.ComposableBreadCrumb
+  alias UserDocsWeb.Defaults
+  alias UserDocsWeb.Root
+  alias UserDocs.Helpers
 
-    {:ok, socket}
+  @types [
+    UserDocs.Users.Team
+  ]
+
+  @impl true
+  def mount(_params, session, socket) do
+    {
+      :ok,
+      socket
+      |> Root.authorize(session)
+      |> Root.initialize(Defaults.base_opts(@types))
+      |> initialize()
+    }
   end
+
+  def initialize(%{ assigns: %{ auth_state: :logged_in }} = socket) do
+    opts = Defaults.state_opts(socket)
+
+    socket
+    |> assign(:modal_action, :show)
+    |> assign(:state_opts, opts)
+  end
+  def initialize(socket), do: socket
 
   @impl true
   def handle_params(%{"id" => id}, _, socket) do
-    {:noreply,
-     socket
-     |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:team, Users.get_team!(id))}
+    team = Users.get_team!(id, %{ preloads: [ users: true, default_project: true ] })
+    {
+      :noreply,
+      socket
+      |> assign(:page_title, page_title(socket.assigns.live_action))
+      |> assign(:team, team)
+    }
   end
 
   defp page_title(:show), do: "Show Team"
   defp page_title(:edit), do: "Edit Team"
+
+  @impl true
+  def handle_info(n, s), do: Root.handle_info(n, s)
 end
