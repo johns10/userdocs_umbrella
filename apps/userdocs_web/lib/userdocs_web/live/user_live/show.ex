63d6a87b48eb82de6d237543c1e6585b3304313b
John Davenport <johns10davenport@gmail.com>
2021-02-21 16:53:01 -0500

    Refactored for new app nav

--- apps/userdocs_web/lib/userdocs_web/live/user_live/show.ex
+++ apps/userdocs_web/lib/userdocs_web/live/user_live/show.ex
@@ -1,19 +1,30 @@
 defmodule UserDocsWeb.UserLive.Show do
   use UserDocsWeb, :live_view
 
+  use UserdocsWeb.LiveViewPowHelper
+
   alias UserDocs.Users
 
+  alias UserDocsWeb.Defaults
+  alias UserDocsWeb.Root
+
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, socket}
+  def mount(_params, session, socket) do
+    opts = Defaults.base_opts()
+    {
+      :ok,
+      socket
+    }
   end
 
   @impl true
   def handle_params(%{"id" => id}, _, socket) do
-    {:noreply,
-     socket
-     |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:user, Users.get_user!(id))}
+    {
+      :noreply,
+      socket
+      |> assign(:page_title, page_title(socket.assigns.live_action))
+      |> assign(:user, Users.get_user!(id))
+    }
   end
 
   defp page_title(:show), do: "Show User"
