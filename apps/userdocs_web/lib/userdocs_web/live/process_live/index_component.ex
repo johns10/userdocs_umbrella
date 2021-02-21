20e2bf8ef46a64dd4cb2af80f1c379666ca22adf
John Davenport <johns10davenport@gmail.com>
2021-02-21 16:48:20 -0500

    Create process index component

---
+++ apps/userdocs_web/lib/userdocs_web/live/process_live/index_component.ex
@@ -0,0 +1,14 @@
+defmodule UserDocsWeb.ProcessLive.IndexComponent do
+  use UserDocsWeb, :live_component
+
+  use UserdocsWeb.LiveViewPowHelper
+
+  @impl true
+  def update(assigns, socket) do
+    {
+      :ok,
+      socket
+      |> assign(assigns)
+    }
+  end
+end
