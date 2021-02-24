30b162b14e55fbef2422d5ca5ebbd83341e5d3b3
John Davenport <johns10davenport@gmail.com>
2021-02-23 23:22:53 -0500

    Refactored, was busted

--- apps/userdocs_web/lib/userdocs_web/live/docubit_live/renderers/ul.ex
+++ apps/userdocs_web/lib/userdocs_web/live/docubit_live/renderers/ul.ex
@@ -3,21 +3,24 @@ defmodule UserDocsWeb.DocubitLive.Renderers.Ul do
   use Phoenix.HTML
 
   alias UserDocsWeb.DocubitLive.AddDocubitButton
+  alias UserDocsWeb.DocubitLive.Renderers.Base
 
-  def header(_), do: "OL"
+  def header(_), do: "UL"
 
   def render(assigns) do
     ~L"""
       <ul>
-        <%= render_block(@inner_block, %{}) %>
-        <div class="is-justify-self-flex-center is-flex-grow-0">
-          <%= AddDocubitButton.render(%{
-            text: "+",
-            class: "button",
-            parent_cid: @parent_cid,
-            docubit: @docubit,
-            type: "li"}) %>
-        </div>
+        <%= Base.render_inner_content(assigns) %>
+        <%= if @editor do %>
+          <div class="is-justify-self-flex-center is-flex-grow-0">
+            <%= AddDocubitButton.render(%{
+              text: "+",
+              class: "button",
+              parent_cid: @parent_cid,
+              docubit: @docubit,
+              type: "li"}) %>
+          </div>
+        <% end %>
       </ul>
     """
   end
