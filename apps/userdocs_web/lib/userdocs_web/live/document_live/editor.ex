dde43fe95061433ed9a5efb15df8c10d8d8f7154
johns10 <john.davenport@funnelcloud.io>
2021-02-27 00:43:23 -0500

    Changed to correct module

--- apps/userdocs_web/lib/userdocs_web/live/document_live/editor.ex
+++ apps/userdocs_web/lib/userdocs_web/live/document_live/editor.ex
@@ -84,7 +84,7 @@ defmodule UserDocsWeb.DocumentLive.Editor do
       |> Loaders.load_content(opts)
       |> Loaders.load_content_versions(opts)
       |> Loaders.load_steps(opts)
-      |> Loaders.load_screenshots(opts)
+      |> UserDocsWeb.Loaders.load_screenshots(opts)
       |> SelectLists.process(opts)
       |> current_selections()
       |> SelectLists.page(opts)
