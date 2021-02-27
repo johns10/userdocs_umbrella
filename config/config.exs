bdfd1bd43d0c732dc7ea5694e70b534f3d121811
John Davenport <johns10davenport@gmail.com>
2021-02-27 18:29:34 -0500

    For some reason I can't get root configs

--- config/config.exs
+++ config/config.exs
@@ -67,6 +67,17 @@ config :userdocs, :ex_aws,
   secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
   region: System.get_env("AWS_REGION")
 
+config :waffle,
+  storage: Waffle.Storage.S3, # or Waffle.Storage.Local
+  bucket: System.get_env("AWS_BUCKET_NAME") # if using S3
+
+# If using S3:
+config :ex_aws,
+  json_codec: Jason,
+  access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
+  secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
+  region: System.get_env("AWS_REGION")
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
