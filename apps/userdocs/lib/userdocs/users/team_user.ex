fc70894cefa8dc2603fa14a3daa55ab65d192416
John Davenport <johns10davenport@gmail.com>
2021-03-01 16:30:31 -0500

    updated team_user changeset

--- apps/userdocs/lib/userdocs/users/team_user.ex
+++ apps/userdocs/lib/userdocs/users/team_user.ex
@@ -5,9 +5,10 @@ defmodule UserDocs.Users.TeamUser do
   alias UserDocs.Users.User
   alias UserDocs.Users.Team
 
-  @primary_key false
-
   schema "team_users" do
+    field :temp_id, :string, virtual: true
+    field :delete, :boolean, virtual: true
+
     field :default, :boolean
     belongs_to :team, Team
     belongs_to :user, User
@@ -18,9 +19,19 @@ defmodule UserDocs.Users.TeamUser do
   @doc false
   def changeset(team_user, attrs) do
     team_user
-    |> cast(attrs, [:team_id, :user_id])
+    |> Map.put(:temp_id, (team_user.temp_id || attrs["temp_id"]))
+    |> cast(attrs, [ :team_id, :user_id, :delete, :default ])
     |> foreign_key_constraint(:team_id)
     |> foreign_key_constraint(:user_id)
-    |> validate_required([])
+    |> maybe_mark_for_deletion()
+  end
+
+  defp maybe_mark_for_deletion(%{data: %{id: nil}} = changeset), do: changeset
+  defp maybe_mark_for_deletion(changeset) do
+    if get_change(changeset, :delete) do
+      %{changeset | action: :delete}
+    else
+      changeset
+    end
   end
 end
