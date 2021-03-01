2291152e746be6fa092f8357c58ce2ea25d4448d
John Davenport <johns10davenport@gmail.com>
2021-03-01 14:42:52 -0500

    Bad change, removed file_id from original migratio

--- apps/userdocs/priv/repo/migrations/20210223000001_alter_screenshot_aws_file.exs
+++ apps/userdocs/priv/repo/migrations/20210223000001_alter_screenshot_aws_file.exs
@@ -2,19 +2,9 @@
 defmodule UserDocs.Repo.Migrations.AddAWSFile do
   use Ecto.Migration
 
-  def up do
+  def change do
     alter table(:screenshots) do
       add :aws_file, :string
-      remove :file_id, references(:files, on_delete: :nothing)
     end
-    create index(:screenshots, [:file_id])
-  end
-
-  def down do
-    alter table(:screenshots) do
-      remove :aws_file, :string
-      add :file_id, references(:files, on_delete: :nothing)
-    end
-    create index(:screenshots, [:file_id])
   end
 end
