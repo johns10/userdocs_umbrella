b606566d37b3e6874cc818ed5e4b7019be9ea005
John Davenport <johns10davenport@gmail.com>
2021-03-01 14:42:52 -0500

    Bad change, removed file_id from original migratio

--- apps/userdocs/priv/repo/migrations/20200814214324_create_screenshots.exs
+++ apps/userdocs/priv/repo/migrations/20200814214324_create_screenshots.exs
@@ -4,13 +4,11 @@ defmodule UserDocs.Repo.Migrations.CreateScreenshots do
   def change do
     create table(:screenshots) do
       add :name, :string
-      add :file_id, references(:files, on_delete: :nothing)
       add :step_id, references(:steps, on_delete: :delete_all)
 
       timestamps()
     end
 
-    create index(:screenshots, [:file_id])
     create unique_index(:screenshots, [:step_id])
   end
 end
