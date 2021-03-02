fa1959dca71f93da289d97832ba39c522ea5ef06
John Davenport <johns10davenport@gmail.com>
2021-03-02 11:50:26 -0500

    Refactor to make work

--- apps/userdocs_web/lib/userdocs_web/live/user_live/show.ex
+++ apps/userdocs_web/lib/userdocs_web/live/user_live/show.ex
@@ -1,32 +1,49 @@
 defmodule UserDocsWeb.UserLive.Show do
   use UserDocsWeb, :live_view
-
   use UserdocsWeb.LiveViewPowHelper
-
-  alias UserDocs.Users
-
   alias UserDocsWeb.Defaults
   alias UserDocsWeb.Root
 
+  alias UserDocs.Users
+  alias UserDocs.Helpers
+
+
+  @types [
+    UserDocs.Users.User,
+    UserDocs.Users.Team,
+  ]
+
   @impl true
   def mount(_params, session, socket) do
-    opts = Defaults.base_opts()
+    opts = Defaults.opts(socket, @types)
     {
       :ok,
       socket
+      |> Root.authorize(session)
+      |> Root.initialize(opts)
     }
   end
 
   @impl true
   def handle_params(%{"id" => id}, _, socket) do
+    user = Users.get_user!(id, %{ team_users: true, teams: true })
+    select_lists = %{
+      teams: Helpers.select_list(user.teams, :name, true)
+    }
+
     {
       :noreply,
       socket
       |> assign(:page_title, page_title(socket.assigns.live_action))
-      |> assign(:user, Users.get_user!(id))
+      |> assign(:user, user)
+      |> assign(:select_lists, select_lists)
     }
   end
 
+  @impl true
+  def handle_event(n, p, s), do: Root.handle_event(n, p, s)
+
   defp page_title(:show), do: "Show User"
   defp page_title(:edit), do: "Edit User"
+  defp page_title(:options), do: "User Options"
 end
