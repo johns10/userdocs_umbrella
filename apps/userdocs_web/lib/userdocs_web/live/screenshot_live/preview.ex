bf7898628d07ba82af42587104aba5a04ffbd4bc
John Davenport <johns10davenport@gmail.com>
2021-03-03 18:50:46 -0500

    handle screenshot not loaded

--- apps/userdocs_web/lib/userdocs_web/live/screenshot_live/preview.ex
+++ apps/userdocs_web/lib/userdocs_web/live/screenshot_live/preview.ex
@@ -18,7 +18,8 @@ defmodule UserDocsWeb.ScreenshotLive.Preview do
     { img_url, img_alt} =
       case Media.get_screenshot_url(screenshot) do
         { :ok, url } -> { url, screenshot.aws_file.file_name}
-        { :nofile, path } -> { path, "No file created for this screenshot, go collect your screensshot" }
+        { :not_loaded, path } -> { path, "Screenshot association not loaded, this is probably a new file." }
+        { :nofile, path } -> { path, "No file created for this screenshot, go collect your screenshot" }
       end
 
     {
