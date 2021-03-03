3d23c2c301907a44f9a0cccfeb1e16c3126afa26
John Davenport <johns10davenport@gmail.com>
2021-03-03 18:00:59 -0500

    Initial Commit

---
+++ apps/userdocs/lib/userdocs/changeset_helpers.ex
@@ -0,0 +1,83 @@
+defmodule UserDocs.ChangesetHelpers do
+  def check_only_one_default(%Ecto.Changeset{ action: :insert } = changeset, _) do
+    IO.puts("insert")
+    changeset
+  end
+  def check_only_one_default(changeset, assoc_key) do
+    IO.inspect(changeset)
+    num_defaults =
+      case Ecto.Changeset.get_change(changeset, assoc_key) do
+        nil -> count_defaults(Map.get(changeset.data, assoc_key))
+        assoc -> count_defaults(assoc)
+      end
+
+    IO.inspect(num_defaults)
+
+    if num_defaults != 1 do
+      Ecto.Changeset.add_error(changeset, assoc_key, "May only have 1 default")
+    else
+      changeset
+    end
+  end
+
+  def count_defaults(%Ecto.Association.NotLoaded{}), do: 0
+  def count_defaults([ %Ecto.Changeset{} | _ ] = assoc) do
+    Enum.reduce(assoc, 0,
+      fn(item, acc) ->
+        case { Ecto.Changeset.get_change(item, :default, nil), item.data.default } do
+          { true, _ } -> acc + 1
+          { nil, true } -> acc + 1
+          { _, _ } -> acc
+        end
+      end
+    )
+  end
+  def count_defaults(data) do
+    Enum.reduce(data, 0,
+      fn(item, acc) ->
+        case item.default do
+          true -> acc + 1
+          false -> acc
+          nil -> acc
+        end
+      end
+    )
+  end
+
+  def maybe_mark_for_deletion(%{data: %{id: nil}} = changeset), do: changeset
+  def maybe_mark_for_deletion(changeset) do
+    if Ecto.Changeset.get_change(changeset, :delete) do
+      %{changeset | action: :delete}
+    else
+      changeset
+    end
+  end
+
+  def add_object(changeset, object, key, fresh_changeset) do
+    existing_objects =
+      Map.get(changeset.changes, key,
+        Map.get(object, key)
+      )
+
+    objects =
+      existing_objects
+      |> Enum.concat([ fresh_changeset ])
+
+    IO.puts("Changegset")
+    IO.inspect(changeset)
+
+    changeset
+    |> Ecto.Changeset.put_assoc(key, objects)
+  end
+
+  def remove_object(changeset, key, remove_id) do
+    objects =
+      Map.get(changeset.changes, key)
+      |> Enum.reject(fn %{data: object} ->
+        object.temp_id == remove_id
+      end)
+
+    changeset
+    |> Ecto.Changeset.put_assoc(key, objects)
+  end
+end
