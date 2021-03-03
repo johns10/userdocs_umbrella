b562fc9250b51670849f24a94fbcaff78e4bb324
John Davenport <johns10davenport@gmail.com>
2021-03-03 18:48:53 -0500

    Refactored for use in app

--- apps/userdocs_web/lib/userdocs_web/live/team_live/index.ex
+++ apps/userdocs_web/lib/userdocs_web/live/team_live/index.ex
@@ -27,41 +27,45 @@ defmodule UserDocsWeb.TeamLive.Index do
     }
   end
 
-  def initialize(%{ assigns: %{ auth_state: :logged_in }} = socket) do
-    opts = Defaults.state_opts(socket)
-
+  def initialize(%{ assigns: %{ auth_state: :logged_in, state_opts: opts }} = socket) do
     socket
     |> assign(:modal_action, :show)
-    |> assign(:state_opts, opts)
-    |> prepare_teams()
+    |> load_teams()
     |> Users.load_users(opts)
   end
   def initialize(socket), do: socket
 
   @impl true
+  def handle_params(params, _url, %{ assigns: %{ auth_state: :not_logged_in }} = socket) do
+    IO.puts("handle_params")
+    {:noreply, socket}
+  end
   def handle_params(params, _url, socket) do
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end
 
   defp apply_action(socket, :edit, %{"id" => id}) do
-    team = Users.get_team!(id, %{ preloads: [ users: true, default_project: true, projects: true, team_users: true ] })
+    team = Users.get_team!(id, %{ preloads: [ users: true, projects: true, team_users: true ] })
     socket
     |> assign(:page_title, "Edit Team")
     |> assign(:team, team)
     |> assign(:projects_select_options, Helpers.select_list(team.projects, :name, false))
+    |> prepare_teams()
   end
 
   defp apply_action(socket, :new, _params) do
     socket
     |> assign(:page_title, "New Team")
-    |> assign(:team, %Team{ users: [] })
+    |> assign(:team, %Team{ users: [], projects: [] })
     |> assign(:projects_select_options, [])
+    |> prepare_teams()
   end
 
   defp apply_action(socket, :index, _params) do
     socket
     |> assign(:page_title, "Listing Teams")
     |> assign(:team, nil)
+    |> prepare_teams()
   end
 
   @impl true
@@ -73,9 +77,17 @@ defmodule UserDocsWeb.TeamLive.Index do
   end
   def handle_event(n, p, s), do: Root.handle_event(n, p, s)
 
-  defp prepare_teams(socket) do
+  def prepare_teams(socket) do
     socket
     |> assign(:teams, Users.list_teams(socket, socket.assigns.state_opts))
+  end
+
+  defp load_teams(socket) do
+    opts =
+      socket.assigns.state_opts
+      |> Keyword.put(:filters, %{ user_id: socket.assigns.current_user.id})
+    socket
+    |> Users.load_teams(opts)
   end
 
   @impl true
