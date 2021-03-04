ff15be8852653c7437ea0bdda7fc92244b29b25e
John Davenport <johns10davenport@gmail.com>
2021-03-03 20:54:42 -0500

    Fuckin a

--- apps/userdocs/priv/repo/migrations/20210223000001_alter_screenshot_aws_file.exs
+++ apps/userdocs/priv/repo/migrations/20210223000001_alter_screenshot_aws_file.exs
@@ -4,7 +4,6 @@ defmodule UserDocs.Repo.Migrations.AddAWSFile do
 
   def change do
     alter table(:screenshots) do
-      remove :file_id, references(:files, on_delete: :nothing)
       add :aws_file, :string
     end
   end
