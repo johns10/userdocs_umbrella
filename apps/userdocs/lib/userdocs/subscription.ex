703a6621853c5757ec1a12def764cc969bf33e22
John Davenport <johns10davenport@gmail.com>
2021-03-06 00:27:30 -0500

    Added conditional broadcast on changeset

--- apps/userdocs/lib/userdocs/subscription.ex
+++ apps/userdocs/lib/userdocs/subscription.ex
@@ -4,6 +4,64 @@ defmodule UserDocs.Subscription do
 
   alias UserDocsWeb.Endpoint
 
+  def broadcast_children(object, changeset, opts) do
+    IO.inspect("Broadcasting restuls")
+    actions = check_changes(changeset)
+    queue = traverse_changes(object, actions)
+    Enum.each(queue,
+      fn({ action, object }) ->
+        opts = Keyword.put(opts, :action, Atom.to_string(action))
+        StateHandlers.broadcast(%{}, object, opts)
+      end
+    )
+  end
+
+  def traverse_changes(object, actions) do
+    Enum.reduce(actions, [],
+      fn({ key, %{ action: action, changes: changes } }, acc) ->
+        queue_change(action, Map.get(object, key), changes, acc)
+      end
+    )
+  end
+
+  def queue_change(_, object, _, acc) when object == %{}, do: acc
+  def queue_change(action, object, changes, acc) do
+    traverse_changes(object, changes) ++ [ { action, object } | acc]
+  end
+
+  def check_changes(changeset) do
+    schema = changeset.data.__struct__
+    associations = changeset.data.__struct__.__schema__(:associations)
+    Logger.debug("Checking changes on #{schema} with associations #{inspect(associations)}")
+
+    Enum.reduce(associations, %{},
+      fn(association, acc) ->
+        association_field = schema.__schema__(:association, association).field
+
+        Logger.debug("  Checking changes on #{association_field}")
+        Ecto.Changeset.get_change(changeset, association_field)
+        |> check_change(association_field, acc)
+      end
+    )
+  end
+
+  def check_change(nil, field, acc) do
+    Logger.debug("  No change detected")
+    acc
+  end
+  def check_change(changeset, field, acc) do
+    changes = check_changes(changeset)
+    schema = changeset.data.__struct__
+    Logger.debug("  Change detected on #{schema}")
+
+    attrs = %{
+      action: changeset.action,
+      changes: changes
+    }
+
+    Map.put(acc, field, attrs)
+  end
+
   def broadcast({ status, result }, type, operation) do
     Logger.debug("#{operation} broadcast triggered on #{type}")
     case status do
