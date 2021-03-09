a4b74583614cfbcf94610283b2a31e13250e1a6d
John Davenport <johns10davenport@gmail.com>
2021-03-09 00:12:40 -0500

    Not sure this is fixed

--- apps/userdocs_web/lib/userdocs_web/live/signin_redirect.ex
+++ apps/userdocs_web/lib/userdocs_web/live/signin_redirect.ex
@@ -2,5 +2,5 @@ defmodule UserDocsWeb.Pow.Routes do
   use Pow.Phoenix.Routes
   alias UserDocsWeb.Router.Helpers, as: Routes
 
-  def after_sign_in_path(conn), do: Routes.process_administrator_index_path(conn, :index)
+  def after_sign_in_path(conn), do: Routes.user_index_path(conn, :index)
 end
