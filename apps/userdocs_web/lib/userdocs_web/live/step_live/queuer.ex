e01355f81ddcda86e155d860ac4c38ab40ea0ee4
John Davenport <johns10davenport@gmail.com>
2021-03-25 19:30:34 -0400

    Initial Commit

---
+++ apps/userdocs_web/lib/userdocs_web/live/step_live/queuer.ex
@@ -0,0 +1,21 @@
+defmodule UserDocsWeb.StepLive.Queuer do
+  use UserDocsWeb, :live_component
+  use Phoenix.HTML
+
+  @impl true
+  def render(assigns) do
+    ~L"""
+    <a class="navbar-item"
+      id="<%= @id %>"
+      phx-click="add_step"
+      phx-value-step-id="<%= @step.id %>"
+      phx-value-app="<%= @app_name %>"
+      phx-target="#automation-manager"
+    >
+      <span class="icon">
+        <%= content_tag(:i, "", [class: "fa fa-plus", aria_hidden: "true"]) %>
+      </span>
+    </a>
+    """
+  end
+end
