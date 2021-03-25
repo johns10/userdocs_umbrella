74fa1820c1d51cede45f23725c3fcce0d14e0793
John Davenport <johns10davenport@gmail.com>
2021-03-25 19:30:43 -0400

    Initial Commit

---
+++ apps/userdocs_web/lib/userdocs_web/live/process_live/queuer.ex
@@ -0,0 +1,22 @@
+defmodule UserDocsWeb.ProcessLive.Queuer do
+  use UserDocsWeb, :live_component
+
+  alias UserDocs.Automation.Runner
+
+  @impl true
+  def render(assigns) do
+    ~L"""
+    <a class="navbar-item"
+      id="<%= @id %>"
+      phx-click="queue_process_instance"
+      phx-value-process-id="<%= @process.id %>"
+      phx-value-app="<%= @app_name %>"
+      phx-target="#automation-manager"
+    >
+      <span class="icon">
+        <%= content_tag(:i, "", [class: "fa fa-plus", aria_hidden: "true"]) %>
+      </span>
+    </a>
+    """
+  end
+end
