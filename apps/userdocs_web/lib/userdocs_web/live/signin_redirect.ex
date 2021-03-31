29985af310069f3574650ddcf578c568b5587280
John Davenport <johns10davenport@gmail.com>
2021-03-31 19:31:49 -0400

    Revert to pow defaults

--- apps/userdocs_web/lib/userdocs_web/live/signin_redirect.ex
+++ apps/userdocs_web/lib/userdocs_web/live/signin_redirect.ex
@@ -1,6 +1,4 @@
 defmodule UserDocsWeb.Pow.Routes do
   use Pow.Phoenix.Routes
   alias UserDocsWeb.Router.Helpers, as: Routes
-
-  def after_sign_in_path(conn), do: Routes.user_index_path(conn, :index)
 end
