ff33b6c5b86e0a4e7a1dc88df395c45a3f5b3228
John Davenport <johns10davenport@gmail.com>
2021-04-10 09:41:33 -0400

    Refactor

--- apps/userdocs/lib/userdocs/jobs.ex
+++ apps/userdocs/lib/userdocs/jobs.ex
@@ -4,155 +4,110 @@ defmodule UserDocs.Jobs do
   import Ecto.Query, warn: false
   alias UserDocs.Repo
 
-  alias UserDocs.Jobs.StepInstance
+  alias UserDocs.ProcessInstances
+  alias UserDocs.ProcessInstances.ProcessInstance
 
-  def create_step_instance_from_step(step, order \\ 0) do
-    attrs = base_step_instance_attrs(step, order)
-    create_step_instance(attrs)
-  end
-
-  def base_step_instance_attrs(step, order) do
-    %{
-      order: order,
-      step_id: step.id,
-      name: step.name,
-      step: step,
-      attrs: %{},
-      status: "not_started",
-      errors: [],
-      warnings: []
-    }
-  end
-
-  def create_step_instance(attrs \\ %{}) do
-    %StepInstance{ step: nil }
-    |> StepInstance.changeset(attrs)
-    |> IO.inspect()
-    |> Ecto.Changeset.put_change(:id, UUID.uuid4())
-    |> Ecto.Changeset.apply_action(:insert)
-  end
-
-  def update_step_instance(%StepInstance{} = step_instance, attrs) do
-    step_instance
-    |> StepInstance.changeset(attrs)
-    |> Ecto.Changeset.apply_action(:update)
-  end
-
-  def format_step_instance_for_export(%StepInstance{} = step_instance) do
-    step_instance
-    |> Map.put(:attrs, UserDocs.Automation.Runner.parse(step_instance.step))
-    |> Map.take(StepInstance.__schema__(:fields))
-    |> Map.put(:type, StepInstance |> to_string() |> String.split(".") |> Enum.at(-1))
-  end
-
-  alias UserDocs.Jobs.ProcessInstance
-
-  def create_process_instance(attrs \\ %{}) do
-    %ProcessInstance{ process: nil, step_instances: nil, expanded: false }
-    |> ProcessInstance.changeset(attrs)
-    |> Ecto.Changeset.put_change(:id, UUID.uuid4())
-    |> Ecto.Changeset.apply_action(:insert)
-  end
-
-  def toggle_process_instance_expanded(%ProcessInstance{} = process_instance) do
-    attrs = %{ expanded: not process_instance.expanded }
-    update_process_instance(process_instance, attrs)
-  end
-
-  def update_process_instance(%ProcessInstance{} = process_instance, attrs) do
-    process_instance
-    |> ProcessInstance.changeset(attrs)
-    |> Ecto.Changeset.apply_action(:update)
-  end
-
-  def format_process_instance_for_export(%ProcessInstance{} = process_instance) do
-    start_attrs = %{
-      order: 0,
-      status: "not_started",
-      name: "Start Process " <> process_instance.name,
-      attrs: %{
-        step_type: %{
-          name: "Start Process"
-        },
-        step: %{
-          process: %{
-            id: process_instance.process_id
-          }
-        }
-      },
-      errors: [],
-      warnings: [],
-    }
-
-    complete_attrs = %{
-      order: 0,
-      status: "not_started",
-      name: "Complete Process " <> process_instance.name,
-      attrs: %{
-        step_type: %{
-          name: "Complete Process"
-        },
-        step: %{
-          process: %{
-            id: process_instance.process_id
-          }
-        }
-      },
-      errors: [],
-      warnings: [],
-    }
-
-    [ start_attrs ]
-    ++ Enum.map(process_instance.step_instances, &format_step_instance_for_export/1)
-    ++ [ complete_attrs ]
-  end
-
-  #
-
-  def create_process_instance_from_process(process, order) do
-    { step_instance_attrs, _max_order } =
-      process.steps
-      |> Enum.sort(fn(x, y) -> x.order < y.order end)
-      |> Enum.reduce({ [], 1 },
-        fn(step, { acc, inner_order }) ->
-          { [ base_step_instance_attrs(step, inner_order) | acc ], inner_order + 1 }
-        end)
-
-    step_instance_attrs = Enum.reverse(step_instance_attrs)
-
-    base_process_instance_attrs(process, step_instance_attrs, order)
-    |> create_process_instance()
-  end
-
-  def base_process_instance_attrs(process, step_instance_attrs, order) do
-    %{
-      order: order,
-      process_id: process.id,
-      process: process,
-      step_instances: step_instance_attrs,
-      name: process.name,
-      attrs: %{},
-      status: "not_started",
-      errors: [],
-      warnings: []
-    }
-  end
-
-  def format_instance(%StepInstance{} = step_instance), do: format_step_instance_for_export(step_instance)
-  def format_instance(%ProcessInstance{} = process_instance), do: format_process_instance_for_export(process_instance)
+  alias UserDocs.StepInstances
+  alias UserDocs.StepInstances.StepInstance
 
   alias UserDocs.Jobs.Job
 
+  def format_instance(%StepInstance{} = step_instance), do: StepInstances.format_step_instance_for_export(step_instance)
+  def format_instance(%ProcessInstance{} = process_instance), do: ProcessInstances.format_process_instance_for_export(process_instance)
+
+  def list_jobs(params \\ %{}) do
+    preloads = Map.get(params, :preloads, [])
+    base_jobs_query()
+    |> maybe_preload_step_instances(preloads[:step_instances])
+    |> maybe_preload_process_instances(preloads[:process_instances])
+    |> Repo.all()
+  end
+
+  def get_job!(id) do
+    base_job_query(id)
+    |> Repo.one!()
+  end
+
+  defp base_job_query(id) do
+    from(job in Job, where: job.id == ^id)
+  end
+
+  defp maybe_preload_step_instances(query, nil), do: query
+  defp maybe_preload_step_instances(query, _), do: from(users in query, preload: [:step_instances])
+
+  defp maybe_preload_process_instances(query, nil), do: query
+  defp maybe_preload_process_instances(query, _), do: from(users in query, preload: [:process_instances])
+
+  defp base_jobs_query(), do: from(jobs in Job)
+
   def create_job(attrs \\ %{}) do
-    %Job{ process_instances: [], step_instances: []}
+    %Job{}
     |> Job.changeset(attrs)
-    |> Ecto.Changeset.apply_action(:insert)
+    |> Repo.insert()
   end
 
   def update_job(%Job{} = job, attrs) do
     job
     |> Job.changeset(attrs)
-    |> Ecto.Changeset.apply_action(:update)
+    |> Repo.update()
+  end
+
+  def delete_job(%Job{} = job) do
+    Repo.delete(job)
+  end
+
+  def change_job(%Job{} = job, attrs \\ %{}) do
+    Job.changeset(job, attrs)
+  end
+
+  def remove_job_step_instance(%Job{} = job, step_id) do
+    IO.puts("remove_job_step_instance")
+    { step_instance, step_instances } =
+      job.step_instances
+      |> Enum.reduce({ nil, [] },
+        fn(step_instance, { found, remaining }) ->
+          if step_instance.id == step_id do
+            { step_instance, remaining }
+          else
+            { found, [ step_instance | remaining ] }
+          end
+        end)
+
+    case StepInstances.delete_step_instance(step_instance) do
+      { :ok, step_instance } ->
+        update_job_step_instances(job, step_instances)
+      { :error, _changeset } -> { :error, job }
+    end
+  end
+
+  def update_job_step_instances(%Job{} = job, step_instances) do
+    IO.puts("update_job_step_instances")
+    IO.inspect(step_instances)
+    job
+    |> Ecto.Changeset.change()
+    |> Ecto.Changeset.put_assoc(:step_instances, step_instances)
+    |> Repo.update()
+  end
+
+  def update_job_step_instance(
+    %Job{ step_instances: step_instances } = job,
+    %{ "id" => id } = step_instance_attrs
+  ) do
+    IO.inspect(step_instance_attrs)
+    updated_step_instances =
+      Enum.map(step_instances,
+        fn(step_instance) ->
+          case step_instance.id == id do
+            true ->
+              { :ok, updated_step_instance } = StepInstances.update_step_instance(step_instance, step_instance_attrs)
+              updated_step_instance
+              |> Map.put(:step, step_instance.step) # TODO: Go get the step?
+            false -> step_instance
+          end
+        end
+      )
+    { :ok, job } = update_job(job, %{ step_instances: updated_step_instances })
+    { :ok, Map.put(job, :step_instances, updated_step_instances)}
   end
 
   def expand_process_instance(%Job{} = job, id) do
@@ -162,7 +117,7 @@ defmodule UserDocs.Jobs do
         fn(process_instance) ->
           if (process_instance.id == id) do
             { :ok, updated_process_instance } =
-              toggle_process_instance_expanded(process_instance)
+              ProcessInstances.toggle_process_instance_expanded(process_instance)
 
             updated_process_instance
           else
@@ -189,6 +144,7 @@ defmodule UserDocs.Jobs do
   end
 
   def export_job(%Job{} = job) do
+    IO.inspect("Exporting job")
     Enum.reduce(get_executable_items(job), [],
       fn(instance, acc) ->
         case format_instance(instance) do
