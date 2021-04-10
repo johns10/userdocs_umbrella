11c5fd79fa4da4c48731e6cbeff94e1c3f6a5db1
John Davenport <johns10davenport@gmail.com>
2021-04-10 09:43:35 -0400

    Moved to seperate module

--- apps/userdocs/lib/userdocs/step_instance/step_instance.ex
+++ apps/userdocs/lib/userdocs/step_instance/step_instance.ex
@@ -1,12 +1,12 @@
-defmodule UserDocs.Jobs.StepInstance do
+defmodule UserDocs.StepInstances.StepInstance do
   use Ecto.Schema
   import Ecto.Changeset
 
   alias UserDocs.Automation.Step
-  alias UserDocs.Jobs.ProcessInstance
+  alias UserDocs.ProcessInstances.ProcessInstance
   alias UserDocs.Jobs.Job
 
-  embedded_schema do
+  schema "step_instances" do
     field :order, :integer
     field :status, :string
     field :name, :string
@@ -23,14 +23,6 @@ defmodule UserDocs.Jobs.StepInstance do
   def changeset(step_instance, attrs) do
     step_instance
     |> cast(attrs, [ :order, :status, :name, :type, :attrs, :errors, :warnings, :step_id  ])
-    |> put_assoc(:step, Map.get(attrs, :step, nil))
-    |> validate_required([ :order, :name, :status, :attrs, :step_id ])
-  end
-
-  def create_changeset(_, attrs) do
-    %UserDocs.Jobs.StepInstance{ step: nil }
-    |> cast(attrs, [ :order, :status, :name, :type, :attrs, :errors, :warnings, :step_id  ])
-    |> put_assoc(:step, Map.get(attrs, :step, nil))
-    |> validate_required([ :order, :name, :status, :attrs, :step_id ])
+    |> validate_required([ :order, :status, :step_id ])
   end
 end
