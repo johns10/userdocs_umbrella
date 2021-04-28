854e7e9366eb219e330fa708467f6f671f4017e0
John Davenport <johns10davenport@gmail.com>
2021-04-27 21:22:23 -0400

    Add cancel, redirect to steps index

--- apps/userdocs_web/lib/userdocs_web/live/screenshot_live/approve.ex
+++ apps/userdocs_web/lib/userdocs_web/live/screenshot_live/approve.ex
@@ -4,11 +4,12 @@ defmodule UserDocsWeb.ScreenshotLive.Approve do
   alias UserDocs.Screenshots
 
   @impl true
-  def update(%{ step: %{ screenshot: screenshot }, team: team }, socket) do
+  def update(%{ step: %{ process_id: process_id, screenshot: screenshot }, team: team }, socket) do
     {
       :ok,
       socket
       |> assign(:screenshot, screenshot)
+      |> assign(:process_id, process_id)
       |> assign(:team, team)
       |> assign_url(screenshot, :aws_screenshot, team)
       |> assign_url(screenshot, :aws_provisional_screenshot, team)
@@ -25,23 +26,28 @@ defmodule UserDocsWeb.ScreenshotLive.Approve do
 
   @impl true
   def handle_event("approve-provisional", _, %{ assigns: %{ screenshot: screenshot, team: team } } = socket) do
-    IO.puts("approve Provisional")
     updated_screenshot = Screenshots.apply_provisional_screenshot(screenshot, team)
     send(self(), { :broadcast, "update", updated_screenshot })
     {
       :noreply,
       socket
-      |> push_patch(to: Routes.step_index_path(socket, :edit, screenshot.step_id))
+      |> push_patch(to: Routes.step_index_path(socket, :index, socket.assigns.process_id))
     }
   end
   def handle_event("reject-provisional", _, %{ assigns: %{ screenshot: screenshot } } = socket) do
-    IO.puts("reject Provisional")
     updated_screenshot = Screenshots.reject_provisional_screenshot(screenshot)
     send(self(), { :broadcast, "update", updated_screenshot })
       {
         :noreply,
         socket
-        |> push_patch(to: Routes.step_index_path(socket, :edit, screenshot.step_id))
+        |> push_patch(to: Routes.step_index_path(socket, :index, socket.assigns.process_id))
+      }
+  end
+  def handle_event("cancel-review", _, %{ assigns: %{ screenshot: screenshot } } = socket) do
+      {
+        :noreply,
+        socket
+        |> push_patch(to: Routes.step_index_path(socket, :index, socket.assigns.process_id))
       }
   end
 end
