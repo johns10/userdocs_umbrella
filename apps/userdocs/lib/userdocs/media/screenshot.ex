ed0943a3f5eaa9fd2b465347d560f68ef423e6f9
johns10 <johns10davenport@gmail.com>
2021-05-14 18:14:47 -0400

    Refactor screenshot creation changeset cast_assoc

--- apps/userdocs/lib/userdocs/media/screenshot.ex
+++ apps/userdocs/lib/userdocs/media/screenshot.ex
@@ -27,6 +27,38 @@ defmodule UserDocs.Media.Screenshot do
     |> cast(attrs, [:name, :step_id, :base_64, :aws_screenshot, :aws_provisional_screenshot, :aws_diff_screenshot])
     |> foreign_key_constraint(:step_id)
     |> unique_constraint(:step_id)
+    |> maybe_update_screenshots()
+    |> maybe_change_aws_filename()
+    |> validate_required([:step_id])
+  end
+
+  def maybe_change_aws_filename(%{ data: %{ aws_screenshot: nil }} = changeset), do: changeset
+  def maybe_change_aws_filename(%{ data: %{ aws_screenshot: current_aws_path, id: id }} = changeset) when is_integer(id) do
+    current_file_name = Screenshots.unpath(current_aws_path)
+    { :ok, screenshot } = apply_action(changeset, :update)
+    new_file_name = Screenshots.file_name(screenshot, :production)
+    if current_file_name != new_file_name do
+      new_path = Screenshots.path(new_file_name)
+      team = UserDocs.Users.get_screenshot_team!(id)
+      { :ok, dest_path } = Screenshots.rename_aws_object(current_aws_path, new_path, team)
+      put_change(changeset, :aws_screenshot, dest_path)
+    else
+      changeset
+    end
+  end
+
+  def maybe_update_screenshots(changeset) do
+    case Ecto.Changeset.get_change(changeset, :base_64) do
+      nil -> changeset
+      _base_64 -> create_aws_screenshot_or_diff_screenshot(changeset)
+    end
+  end
+
+  def create_aws_screenshot_or_diff_screenshot(%{ data: %{ id: _ , aws_screenshot: nil }} = changeset) do
+    Screenshots.create_aws_screenshot(changeset)
+  end
+  def create_aws_screenshot_or_diff_screenshot(%{ data: %{ aws_screenshot: _aws_screenshot }} = changeset) do
+    Screenshots.update_aws_screenshot(changeset)
   end
 
   def safe(annotation, handlers \\ %{})
