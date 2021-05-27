04eee639782a2a487707e28a36d7f436bc3e2268
johns10 <johns10davenport@gmail.com>
2021-05-27 19:56:42 -0400

    Add refs to jobinstance, new casts, and safe

--- apps/userdocs/lib/userdocs/step_instance/step_instance.ex
+++ apps/userdocs/lib/userdocs/step_instance/step_instance.ex
@@ -1,12 +1,15 @@
 defmodule UserDocs.StepInstances.StepInstance do
+  #@derive {Inspect, only: [:id, :order, :status, :name ]}
+
   use Ecto.Schema
   import Ecto.Changeset
 
   alias UserDocs.Automation.Step
   alias UserDocs.ProcessInstances.ProcessInstance
-  alias UserDocs.Jobs.Job
+  alias UserDocs.Jobs.JobInstance
 
   schema "step_instances" do
+    field :uuid, :binary_id
     field :order, :integer
     field :status, :string
     field :name, :string
@@ -15,18 +18,22 @@ defmodule UserDocs.StepInstances.StepInstance do
     field :errors, { :array, :map }
     field :warnings, { :array, :map }
 
-    belongs_to :job, Job, on_replace: :nilify
+    belongs_to :job_instance, JobInstance, on_replace: :nilify
     belongs_to :process_instance, ProcessInstance, on_replace: :nilify
     belongs_to :step, Step, on_replace: :nilify
   end
 
   def changeset(step_instance, attrs) do
     step_instance
-    |> cast(attrs, [ :order, :status, :name, :type, :attrs, :errors, :warnings, :step_id, :job_id, :process_instance_id  ])
+    |> cast(attrs, [ :uuid, :order, :status, :name, :type, :attrs, :errors, :warnings, :step_id, :process_instance_id, :job_instance_id  ])
     |> foreign_key_constraint(:step_id)
     |> foreign_key_constraint(:job_id)
     |> foreign_key_constraint(:process_instance_id)
     |> cast_assoc(:step)
-    |> validate_required([ :order, :status, :step_id ])
+    |> validate_required([ :status, :step_id ])
+  end
+
+  def safe(step_instance, handlers) do
+    UserDocs.StepInstances.StepInstance.Safe.apply(step_instance, handlers)
   end
 end
