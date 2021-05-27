a3311e264e688c5dc45863b5c8ffd76f9e7c376c
johns10 <johns10davenport@gmail.com>
2021-05-27 19:53:57 -0400

    add process instance

--- apps/userdocs/lib/userdocs/job/job_process.ex
+++ apps/userdocs/lib/userdocs/job/job_process.ex
@@ -4,23 +4,26 @@ defmodule UserDocs.Jobs.JobProcess do
 
   alias UserDocs.Automation.Process
   alias UserDocs.Jobs.Job
+  alias UserDocs.ProcessInstances.ProcessInstance
 
   schema "job_processes" do
     field :order, :integer
 
     belongs_to :job, Job
     belongs_to :process, Process
+    belongs_to :process_instance, ProcessInstance
 
-    field :collapsed, :boolean, virtual: true, default: true
+    field :collapsed, :boolean, default: true
 
     timestamps()
   end
 
   def changeset(step_instance, attrs) do
     step_instance
-    |> cast(attrs, [ :order, :job_id, :process_id  ])
+    |> cast(attrs, [ :order, :job_id, :process_id, :process_instance_id, :collapsed  ])
     |> foreign_key_constraint(:process_id)
     |> foreign_key_constraint(:job_id)
+    |> foreign_key_constraint(:process_instance_id)
     |> validate_required([ :order ])
   end
 end
