ec35f7840a94bbb8b2d24dc386cf378d2f6cccd1
johns10 <johns10davenport@gmail.com>
2021-05-27 19:37:04 -0400

    Lil reorganization.  This will get wiped later

--- apps/userdocs/lib/userdocs/automation_manager.ex
+++ apps/userdocs/lib/userdocs/automation_manager.ex
@@ -6,12 +6,7 @@ defmodule UserDocs.AutomationManager do
 
   alias UserDocs.Automation.Process
   alias UserDocs.Automation.Step
-  alias UserDocs.Automation.StepType
-  alias UserDocs.Web.Strategy
-  alias UserDocs.Web.Annotation
-  alias UserDocs.Web.AnnotationType
-  alias UserDocs.Web.Page
-  alias UserDocs.Web.Element
+  alias UserDocs.ProcessInstances.ProcessInstance
 
   def get_step!(id) do
     from(s in Step, as: :steps)
@@ -23,14 +18,17 @@ defmodule UserDocs.AutomationManager do
 
   def get_step_joins(query) do
     query
-    |> join(:left, [steps: s], st in StepType, on: s.step_type_id == st.id, as: :step_type)
-    |> join(:left, [steps: s], a in Annotation, on: s.annotation_id == a.id, as: :annotation)
-    |> join(:left, [steps: s], p in Page, on: s.page_id == p.id, as: :page)
-    |> join(:left, [steps: s], e in Element, on: s.element_id == e.id, as: :element)
-    |> join(:left, [steps: s], pr in assoc(s, :screenshot), as: :screenshot)
-    |> join(:left, [steps: s], pr in Process, on: s.process_id == pr.id, as: :process)
-    |> join(:left, [element: e], st in Strategy, on: e.strategy_id == st.id, as: :strategy)
-    |> join(:left, [annotation: a ], at in AnnotationType, on: a.annotation_type_id == at.id, as: :annotation_type)
+    |> join(:left, [steps: s], st in assoc(s, :step_type), as: :step_type)
+    |> join(:left, [steps: s], a in assoc(s, :annotation), as: :annotation)
+    |> join(:left, [steps: s], p in assoc(s, :page), as: :page)
+    |> join(:left, [steps: s], e in assoc(s, :element), as: :element)
+    |> join(:left, [steps: s], sc in assoc(s, :screenshot), as: :screenshot)
+    |> join(:left, [steps: s], pr in assoc(s, :process), as: :process)
+    |> join(:left, [steps: s], si in assoc(s, :step_instances), as: :last_step_instance)
+    |> order_by([last_step_instance: si], desc: si.id)
+    |> limit([last_step_instance: si], 1)
+    |> join(:left, [element: e], st in assoc(e, :strategy), as: :strategy)
+    |> join(:left, [annotation: a ], at in assoc(a, :annotation_type), as: :annotation_type)
   end
 
   def get_step_preloads(query) do
@@ -41,28 +39,40 @@ defmodule UserDocs.AutomationManager do
     |> preload([ element: element ], [ element: element ])
     |> preload([ screenshot: screenshot ], [ screenshot: screenshot ])
     |> preload([ process: process ], [ process: process ])
+    |> preload([ last_step_instance: lsi ], [ last_step_instance: lsi ])
     |> preload([ annotation: a, annotation_type: at ], [ annotation: { a, annotation_type: at } ])
     |> preload([ element: e, strategy: st ], [ element: { e, strategy: st } ])
   end
 
   def get_process!(id) do
-    Process
-    |> where([p], p.id == ^id)
-    |> join(:left, [p], s in Step, on: s.process_id == p.id, as: :steps)
-    |> join(:left, [steps: s], st in StepType, on: s.step_type_id == st.id, as: :step_type)
-    |> join(:left, [steps: s], a in Annotation, on: s.annotation_id == a.id, as: :annotation)
-    |> join(:left, [steps: s], p in Page, on: s.page_id == p.id, as: :page)
-    |> join(:left, [steps: s], e in Element, on: s.element_id == e.id, as: :element)
-    |> join(:left, [steps: s, element: e], st in Strategy, on: e.strategy_id == st.id, as: :strategy)
-    |> join(:left, [steps: s, annotation: a ], at in AnnotationType, on: a.annotation_type_id == at.id, as: :annotation_type)
-    |> preload([ p, steps: s ], [ steps: s])
-    |> preload([ p, steps: s, step_type: step_type ], [ steps: { s, step_type: step_type } ])
-    |> preload([ p, steps: s, page: page ], [ steps: { s, page: page } ])
-    |> preload([ p, steps: s, element: element ], [ steps: { s, element: element } ])
-    |> preload([ p, steps: s, element: element ], [ steps: { s, process: p } ])
-    |> preload([ p, steps: s, annotation: annotation ], [ steps: { s, annotation: annotation } ])
-    |> preload([ p, steps: s, annotation: a, annotation_type: at ], [ steps: { s, annotation: { a, annotation_type: at} } ])
-    |> preload([ p, steps: s, element: e, strategy: st ], [ steps: { s, element: { e, strategy: st} } ])
+    from(p in Process, as: :process)
+    |> where([process: p], p.id == ^id)
+    |> join(:left, [process: p], s in assoc(p, :steps), as: :steps)
+    |> order_by([steps: s], asc: s.order)
+    |> join(:left, [steps: s], st in assoc(s, :step_type), as: :step_type)
+    |> join(:left, [steps: s], a in assoc(s, :annotation), as: :annotation)
+    |> join(:left, [steps: s], p in assoc(s, :page), as: :page)
+    |> join(:left, [steps: s], e in assoc(s, :element), as: :element)
+    |> join(:left, [steps: s], sc in assoc(s, :screenshot), as: :screenshot)
+    |> join(:left, [steps: s, element: e], st in assoc(e, :strategy), as: :strategy)
+    |> join(:left, [steps: s, annotation: a ], at in assoc(a, :annotation_type), as: :annotation_type)
+    |> join(:left, [ process: p ], pi in assoc(p, :process_instances), as: :process_instances)
+    |> preload([ steps: s ], [ steps: s])
+    |> preload([ steps: s, step_type: st ], [ steps: { s, step_type: st } ])
+    |> preload([ steps: s, screenshot: sc ], [ steps: { s, screenshot: sc } ])
+    |> preload([ steps: s, page: page ], [ steps: { s, page: page } ])
+    |> preload([ steps: s, element: element ], [ steps: { s, element: element } ])
+    |> preload([ process: p, steps: s, element: element ], [ steps: { s, process: p } ])
+    |> preload([ steps: s, annotation: annotation ], [ steps: { s, annotation: annotation } ])
+    |> preload([ steps: s, annotation: a, annotation_type: at ], [ steps: { s, annotation: { a, annotation_type: at} } ])
+    |> preload([ steps: s, element: e, strategy: st ], [ steps: { s, element: { e, strategy: st} } ])
+    |> preload([ last_process_instance: lpi ], [ last_process_instance: ^get_last_process_instance() ])
     |> Repo.one()
+  end
+
+  def get_last_process_instance() do
+    from(pi in ProcessInstance, as: :process_instances)
+    |> order_by([ process_instances: pi ], desc: pi.id)
+    |> limit([ process_instances: pi ], 1)
   end
 end
