f4938e7dc870528719ef86ba9d42990f2e4540e8
johns10 <johns10davenport@gmail.com>
2021-05-27 20:03:30 -0400

    Refactor to runner (incomplete)

--- apps/userdocs_web/assets/js/electron/main.js
+++ apps/userdocs_web/assets/js/electron/main.js
@@ -1,5 +1,4 @@
 const { app, ipcMain } = require('electron');
-const { handleStepStatusUpdate } = require('./main_window/events.js');
 const { 
   stepInstanceHandler, 
   succeed, fail, start, 
@@ -12,11 +11,22 @@ const {
 const puppeteer = require('./puppeteer/puppeteer.js')
 const path = require('path')
 const isDev = require('electron-is-dev');
+const Runner = require('../runner/dist/runner')
 
 if (isDev) {
   require('electron-reload')(__dirname, {
-    electron: path.join(__dirname, '..', '..', 'node_modules', '.bin', 'electron')
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
   });
+}
+
+stepUpdated = function(step) { 
+  mainWindow().send('stepStatusUpdated', step); 
+  return step 
+}
+
+processUpdated = function(process) { 
+  mainWindow().send('processUpdated', process); 
+  return process 
 }
 
 userdocs = {
@@ -24,152 +34,115 @@ userdocs = {
   browserExecutionQueue: [],
   process: {},
   runState: 'stopped',
-  configuration: {}
+  runner: null,
+  configuration: {
+    automationFrameworkName: 'puppeteer',
+    maxRetries: 3,
+    environment: 'desktop',
+    imagePath: null,
+    userDataDirPath: 'default',
+    callbacks: {
+      step: {
+        preExecutionCallbacks: [ 'startLastStepInstance', stepUpdated ],
+        executionCallback: 'run',
+        successCallbacks: [ 'completeLastStepInstance', stepUpdated ],
+        failureCallbacks: [ 'failLastStepInstance', stepUpdated ]
+      },
+      process: {
+        preExecutionCallbacks: [ 'startLastProcessInstance', processUpdated ],
+        executionCallback: 'run',
+        successCallbacks: [ 'completeLastProcessInstance', processUpdated ],
+        failureCallbacks: [ 'failProcessInstance', processUpdated ]
+      },
+      job: {
+        preExecutionCallbacks: [ 'startLastJobInstance' ],
+        executionCallback: 'run',
+        successCallbacks: [ 'completeLastJobInstance' ],
+        failureCallbacks: [ 'failLastJobInstance' ]
+      }
+    }
+  }
 }
-
 
 function main() {
   if(isDev) {
+    userdocs.configuration.environment = 'development'
     createMainWindow()
       .then( mainWindow => navigateToLoginPage(mainWindow) )
       .then( mainWindow => authenticateJohnDavenport(mainWindow))
-      .then( mainWindow => startQueueProcessorEventLoop() )
       .catch( e => console.log(e))
   } else {
     createMainWindow()
       .then( mainWindow => navigateToLoginPage(mainWindow) )
-      .then( mainWindow => startQueueProcessorEventLoop() )
       .catch( e => console.log(e))
   }
-}
-
-function startQueueProcessorEventLoop() {
-  setTimeout(startQueueProcessorEventLoop, 500);
-  if(userdocs.runState === 'running') {
-    if (userdocs.browserExecutionQueue === undefined || userdocs.browserExecutionQueue.length == 0) {
-      userdocs.runState = 'stopped'
-    } else {
-      userdocs.runState = 'executing'
-      processQueue()
-    }
-  }
-}
-
-//TODO: Fix retry mechanisms
-async function processQueue(retries) {
-  const stepInstance = userdocs.browserExecutionQueue.shift()
-  const max_retries = 10
-  const base_backoff = 1
-  if (stepInstance) {
-    console.log("Shifted " + stepInstance.name)
-    if (retries > 0) {
-      console.log("We're retrying, so delete the errors so we don't spam")
-      stepInstance.errors = []
-    }
-    completeStepInstance = await executeStepInstance(stepInstance)
-    if (completeStepInstance.status === COMPLETE) {
-      console.log("Step completed, processing next item")
-      // await new Promise(resolve => setTimeout(resolve, 250)); This is the wait I'm trying to eliminate
-      await processQueue(null)
-    } else if ((completeStepInstance.status === FAILED) && retries <= max_retries) {
-      console.log(`Step Failed, stop and unshift item. On retry ${retries}. Waiting ${backoffTime(retries)}ms before continuing.`)
-      userdocs.browserExecutionQueue.unshift(completeStepInstance)
-      await new Promise(resolve => setTimeout(resolve, backoffTime(retries)));
-      if (retries != null) {
-        retries = retries + 1
-      } else {
-        retries = 1
-      }
-      await processQueue(retries)
-    } else if ((completeStepInstance.status === FAILED) && retries == max_retries) {
-      console.log(`Step Failed, after ${max_retries}.`)
-      userdocs.browserExecutionQueue.unshift(completeStepInstance)
-      userdocs.runState = 'stopped'
-    } else {
-      console.log("Uncaught condition, stop")
-      userdocs.browserExecutionQueue.unshift(completeStepInstance)
-      userdocs.runState = 'stopped'
-    }
-  } else {
-    console.log("Probably null stepinstance, stop")
-    userdocs.runState = 'stopped'
-  }
-}
-
-function backoffTime(retry) {
-  return retry * retry * retry
+  userdocs.runner = Runner.initialize(userdocs.configuration)
 }
 
 ipcMain.on('openBrowser', async (event) => { 
-  userdocs.browser = await openBrowser()
+  userdocs.runner = await openBrowser()
   return true
 })
 
 async function openBrowser() {
-  automationModule = puppeteer
-
-  var browser = await automationModule.openBrowser(userdocs.configuration)
-  browser = await automationModule.preload(browser) 
-  browser = await automationModule.configureDisconnectEvent(browser, mainWindow())
-  mainWindow().webContents.send('browserOpened', { sessionId: automationModule.id(browser) })  
-  return browser
+  if(!userdocs.runner) throw ("No RUnner")
+  runner = userdocs.runner
+  configuration = userdocs.configuration
+  runner = await Runner.openBrowser(runner, configuration)
+  browser = runner.automationFramework.browser
+  browser.on('disconnected', () => browserClosed())
+  mainWindow().webContents.send('browserOpened', { sessionId: browser.process().pid.toString() })
+  console.log(`Browser opened, runner: ${runner}`)  
+  return runner
 }
 
 ipcMain.on('closeBrowser', async (event) => { 
-  automationModule = puppeteer
-  browser = userdocs.browser
-
-  await automationModule.closeBrowser(browser)
-  mainWindow().webContents.send('browserClosed', automationModule.id(browser))
-  userdocs.browser = null
+  if(!userdocs.runner) throw ("No RUnner")
+  runner = userdocs.runner
+  configuration = userdocs.configuration
+  id = runner.automationFramework.browser.process().pid.toString()
+  await Runner.closeBrowser(runner, configuration)
+  browserClosed(id)
   return true
 })
 
-ipcMain.on('execute', async (event, stepInstance) => {
-  executeStepInstance(stepInstance)
-})
-
-async function executeStepInstance (stepInstance) {
-  automationModule = puppeteer
-  if(userdocs.browser === null) { userdocs.browser = await openBrowser() }
-  browser = userdocs.browser
-
-  config = { browser: browser, window: mainWindow() }
-
-  stepInstance = start(stepInstance)
-  handleStepStatusUpdate(stepInstance, config)
-  
-  try {
-    const stepFunction = automationModule.stepInstanceHandler(stepInstance, config)
-    stepInstance = await stepFunction()
-    stepInstance = succeed(stepInstance)
-    handleStepStatusUpdate(stepInstance, config)
-    return stepInstance
-  } catch(error) { 
-    stepInstance = fail(stepInstance, error)
-    console.log(stepInstance.name + " Failed to execute because " + error)
-    handleStepStatusUpdate(stepInstance, config)
-    return stepInstance
-  }
+function browserClosed(id) { 
+  const browserId = id ? id : null
+  mainWindow().webContents.send('browserClosed', browserId)
 }
 
-ipcMain.on('putJob', (event, job) => {
-  userdocs.browserExecutionQueue = job.data
+ipcMain.on('execute', async (event, step) => {
+  if(!userdocs.runner) throw ("No RUnner")
+  if(!userdocs.runner.automationFramework.browser) userdocs.runner = await openBrowser()
+  await Runner.executeStep(step, userdocs.runner)
+})
+
+ipcMain.on('executeProcess', async (event, process) => {
+  if(!userdocs.runner.automationFramework.browser) userdocs.runner = await openBrowser()
+  console.log(`bout to run runner process ${userdocs.runner}`)
+  await Runner.executeProcess(process, userdocs.runner)
+})
+
+ipcMain.on('executeJob', async (event, job) => {
+  if(!userdocs.runner.automationFramework.browser) userdocs.runner = await openBrowser()
+  console.log(`bout to run runner job`)
+  await Runner.executeJob(job, userdocs.runner)
 })
 
 ipcMain.on('start', (event) => {
   userdocs.runState = 'running'
 })
 
-ipcMain.on('executeProcess', async (event, job) => {
-  if(userdocs.browser === null) { userdocs.browser = await openBrowser() }
-  userdocs.browserExecutionQueue = job.data
-  userdocs.runState = 'running'
-})
 
 ipcMain.on('configure', async (event, message) => {
-  console.log("configuring")
-  userdocs.configuration = message
+  if (message.image_path) userdocs.configuration.imagePath = message.image_path
+  if (message.user_data_dir_path) userdocs.configuration.userDataDirPath = message.user_data_dir_path
+  if (message.strategy) userdocs.configuration.strategy = message.strategy
+  try {
+    userdocs.runner = Runner.reconfigure(userdocs.runner, userdocs.configuration)
+  } catch(e) {
+    userdocs.runner = Runner.initialize(userdocs.configuration)
+  } 
 })
 
 ipcMain.on('testSelector', async (event, message) => {
