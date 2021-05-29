55aa850b8fa8f3506ff86e5cc8f8a0a6a1afcfc0
johns10 <johns10davenport@gmail.com>
2021-05-29 17:58:07 -0400

    add list_process_instances for JobInstance

--- apps/userdocs_web/lib/userdocs_web/api/resolvers/process_instance.ex
+++ apps/userdocs_web/lib/userdocs_web/api/resolvers/process_instance.ex
@@ -1,5 +1,6 @@
 defmodule UserDocsWeb.API.Resolvers.ProcessInstance do
   alias UserDocs.Jobs.JobProcess
+  alias UserDocs.Jobs.JobInstance
   alias UserDocs.ProcessInstances
   alias UserDocs.Automation.Process
 
@@ -11,6 +12,10 @@ defmodule UserDocsWeb.API.Resolvers.ProcessInstance do
   def get_process_instance!(%Process{ last_process_instance: process_instance }, _args, _resolution) do
     IO.puts("Got lpi call")
     { :ok, process_instance }
+  end
+
+  def list_process_instances(%JobInstance{ process_instances: process_instances }, _args, _resolution) do
+    { :ok, process_instances }
   end
 
   def update_process_instance(_parent, %{id: id} = args, _resolution) do
--- apps/userdocs_web/lib/userdocs_web/api/schemas/job_instance.ex
+++ apps/userdocs_web/lib/userdocs_web/api/schemas/job_instance.ex
@@ -13,7 +13,7 @@ defmodule UserDocsWeb.API.Schema.JobInstance do
     field :errors, list_of(:error), resolve: &Resolvers.Error.get_error!/3
 
     field :step_instances, list_of(:step_instance), resolve: &Resolvers.StepInstance.list_step_instances/3
-    field :process_instances, list_of(:process_instance), resolve: &Resolvers.ProcessInstance.list_process_instance/3
+    field :process_instances, list_of(:process_instance), resolve: &Resolvers.ProcessInstance.list_process_instances/3
   end
 
   input_object :job_instance_input do
