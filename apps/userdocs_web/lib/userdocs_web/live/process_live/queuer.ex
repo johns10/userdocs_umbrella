9b7004a3a7c67a0122f80e875f787356cf9769a4
johns10 <johns10davenport@gmail.com>
2021-05-30 11:40:57 -0400

    Support passing + default class

--- apps/userdocs_web/lib/userdocs_web/live/process_live/queuer.ex
+++ apps/userdocs_web/lib/userdocs_web/live/process_live/queuer.ex
@@ -2,14 +2,23 @@ defmodule UserDocsWeb.ProcessLive.Queuer do
   use UserDocsWeb, :live_component
 
   @impl true
+  def mount(socket) do
+    socket =
+      socket
+      |> assign(:class, "")
+
+    {:ok, socket}
+  end
+
+  @impl true
   def render(assigns) do
     ~L"""
-    <a class="navbar-item"
-      id="<%= @id %>"
+    <a id="<%= @id %>"
       phx-click="create-job-process"
       phx-value-id="<%= @process.id %>"
       phx-value-name="<%= @process.name %>"
       phx-target="#automation-manager"
+      class="<%= @class %>"
     >
       <span class="icon">
         <%= content_tag(:i, "", [class: "fa fa-plus", aria_hidden: "true"]) %>
