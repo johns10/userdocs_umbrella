86adc4cbf5eb8e1df7541990b777273dbc53bf7f
johns10 <johns10davenport@gmail.com>
2021-06-10 07:12:54 -0400

    add up and down

--- apps/userdocs/priv/repo/migrations/20210608000001_alter_team_aalter_css_to_text.exs
+++ apps/userdocs/priv/repo/migrations/20210608000001_alter_team_aalter_css_to_text.exs
@@ -2,9 +2,15 @@
 defmodule UserDocs.Repo.Migrations.AlterTeamAddCss do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:teams) do
       modify :css, :text
+    end
+  end
+
+  def down do
+    alter table(:teams) do
+      modify :css, :string
     end
   end
 end
