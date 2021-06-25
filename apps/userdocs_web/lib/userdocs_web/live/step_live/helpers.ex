809f2743693dd40a28ba759bf659c3e8438f1972
johns10 <johns10davenport@gmail.com>
2021-06-25 15:09:24 -0400

    Might never care again

--- apps/userdocs_web/lib/userdocs_web/live/step_live/helpers.ex
+++ apps/userdocs_web/lib/userdocs_web/live/step_live/helpers.ex
@@ -4,32 +4,24 @@ defmodule UserDocsWeb.StepLive.FormComponent.Helpers do
   alias UserDocs.Web.Annotation
 
   def enabled_step_fields(socket, changeset) do
-    case socket.assigns.action do
-      :new -> []
-      :edit ->
-        UserDocsWeb.LiveHelpers.enabled_fields(
-          Automation.list_step_types(socket, socket.assigns.state_opts),
-          Ecto.Changeset.get_field(changeset, :step_type_id)
-        )
-    end
+    UserDocsWeb.LiveHelpers.enabled_fields(
+      Automation.list_step_types(socket, socket.assigns.state_opts),
+      Ecto.Changeset.get_field(changeset, :step_type_id)
+    )
   end
 
   def enabled_annotation_fields(socket, changeset) do
-    case socket.assigns.action do
-      :new -> []
-      :edit ->
-        annotation_type_id =
-          changeset
-          |> Ecto.Changeset.get_field(:annotation)
-          |> case do
-            nil -> nil
-            %Annotation{} = annotation -> Map.get(annotation, :annotation_type_id)
-          end
+    annotation_type_id =
+      changeset
+      |> Ecto.Changeset.get_field(:annotation)
+      |> case do
+          nil -> nil
+          %Annotation{} = annotation -> Map.get(annotation, :annotation_type_id)
+        end
 
-        UserDocsWeb.LiveHelpers.enabled_fields(
-          Web.list_annotation_types(socket, socket.assigns.state_opts),
-          annotation_type_id
-        )
-    end
+    UserDocsWeb.LiveHelpers.enabled_fields(
+      Web.list_annotation_types(socket, socket.assigns.state_opts),
+      annotation_type_id
+    )
   end
 end
