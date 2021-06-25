4e0812b03a5eb67606cf9d47936c22d6210e9c8d
johns10 <johns10davenport@gmail.com>
2021-06-25 19:02:25 -0400

    add screenshot fields

--- apps/userdocs_web/lib/userdocs_web/live/step_live/form_component.ex
+++ apps/userdocs_web/lib/userdocs_web/live/step_live/form_component.ex
@@ -15,7 +15,8 @@ defmodule UserDocsWeb.StepLive.FormComponent do
   @enablers [
     :url_enabled, :text_enabled, :width_enabled, :height_enabled,
     :page_id_enabled, :page_form_enabled, :element_id_enabled,
-    :element_form_enabled, :annotation_id_enabled, :annotation_form_enabled
+    :element_form_enabled, :annotation_id_enabled, :annotation_form_enabled,
+    :screenshot_form_enabled
   ]
 
   @impl true
