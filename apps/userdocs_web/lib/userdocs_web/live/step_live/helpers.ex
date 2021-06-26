e65eafd1db8e1cb4ed110f4f37db91a150cce9a3
johns10 <johns10davenport@gmail.com>
2021-06-26 19:03:33 -0400

    Refactor enabled fields functions

--- apps/userdocs_web/lib/userdocs_web/live/step_live/helpers.ex
+++ apps/userdocs_web/lib/userdocs_web/live/step_live/helpers.ex
@@ -1,27 +1,78 @@
 defmodule UserDocsWeb.StepLive.FormComponent.Helpers do
   alias UserDocs.Automation
   alias UserDocs.Web
-  alias UserDocs.Web.Annotation
+  alias UserDocs.Web.AnnotationForm
+  alias UserDocs.Automation.StepForm
 
-  def enabled_step_fields(socket, changeset) do
-    UserDocsWeb.LiveHelpers.enabled_fields(
-      Automation.list_step_types(socket, socket.assigns.state_opts),
-      Ecto.Changeset.get_field(changeset, :step_type_id)
-    )
+
+  def handle_enabled_fields(%Ecto.Changeset{} = changeset, state) do
+    IO.puts("handle_enabled_fields")
+    IO.inspect(changeset.data)
+    changeset
+    |> maybe_update_enabled_step_fields(state)
+    |> maybe_update_enabled_annotation_fields(state)
   end
 
-  def enabled_annotation_fields(socket, changeset) do
-    annotation_type_id =
-      changeset
-      |> Ecto.Changeset.get_field(:annotation)
-      |> case do
-          nil -> nil
-          %Annotation{} = annotation -> Map.get(annotation, :annotation_type_id)
-        end
+  def maybe_update_enabled_step_fields(%Ecto.Changeset{ changes: %{ step_type_id: _ }} = changeset, state) do
+    enabled_step_fields(changeset, state)
+  end
+  def maybe_update_enabled_step_fields(%Ecto.Changeset{} = changeset, _), do: changeset
 
-    UserDocsWeb.LiveHelpers.enabled_fields(
-      Web.list_annotation_types(socket, socket.assigns.state_opts),
-      annotation_type_id
+  def maybe_update_enabled_annotation_fields(%Ecto.Changeset{ changes: %{ annotation: %{ changes: %{ annotation_type_id: _ } } } } = changeset, state) do
+    enabled_annotation_fields(changeset, state)
+  end
+  def maybe_update_enabled_annotation_fields(%Ecto.Changeset{} = changeset, _), do: changeset
+
+  def enabled_step_fields(%Ecto.Changeset{} = changeset, state) do
+    step_type = Automation.get_step_type!(changeset.changes.step_type_id, state, state.state_opts)
+    enable_fields(changeset, StepForm.enabler_fields(), step_type.args)
+  end
+  def enabled_step_fields(%StepForm{ step_type_id: nil} = step_form, _), do: step_form
+  def enabled_step_fields(%StepForm{ step_type_id: step_type_id} = step_form, state) do
+    step_type = Automation.get_step_type!(step_type_id, state, state.state_opts)
+    enable_fields(step_form, StepForm.enabler_fields(), step_type.args)
+  end
+
+  def enabled_annotation_fields(%Ecto.Changeset{} = changeset, state) do
+    IO.inspect("enabled_annotation_fields, changeset version")
+    annotation_type_id = changeset.changes.annotation.changes.annotation_type_id
+    annotation_type = Web.get_annotation_type!(annotation_type_id, state, state.state_opts)
+    annotation_form = enable_fields(changeset.changes.annotation, AnnotationForm.enabler_fields(), annotation_type.args)
+    IO.inspect(annotation_form)
+    Ecto.Changeset.put_change(changeset, :annotation, annotation_form)
+  end
+  def enabled_annotation_fields(%StepForm{ annotation: nil } = step_form, _), do: step_form
+  def enabled_annotation_fields(%StepForm{ annotation: %{ annotation_type_id: nil }} = step_form, _), do: step_form
+  def enabled_annotation_fields(%StepForm{ annotation: %{ annotation_type_id: annotation_type_id } = annotation } = step_form, state) do
+    IO.inspect("enabled_annotation_fields")
+    annotation_type = Web.get_annotation_type!(annotation_type_id, state, state.state_opts)
+    annotation_form = enable_fields(annotation, AnnotationForm.enabler_fields(), annotation_type.args)
+    Map.put(step_form, :annotation, annotation_form)
+  end
+
+  def enable_fields(%Ecto.Changeset{} = changeset, fields, fields_to_enable) do
+    IO.inspect(fields)
+    IO.inspect(fields_to_enable)
+    Enum.reduce(fields, changeset,
+      fn(enabler, inner_changeset) ->
+        if String.replace(to_string(enabler), "_enabled", "") in fields_to_enable do
+          IO.inspect("Replacing a true for #{enabler}")
+          Ecto.Changeset.put_change(inner_changeset, enabler, true)
+        else
+          Ecto.Changeset.put_change(inner_changeset, enabler, false)
+        end
+      end
+    )
+  end
+  def enable_fields(object, fields, fields_to_enable) do
+    Enum.reduce(fields, object,
+      fn(enabler, inner_object) ->
+        if String.replace(to_string(enabler), "_enabled", "") in fields_to_enable do
+          Map.put(inner_object, enabler, true)
+        else
+          Map.put(inner_object, enabler, false)
+        end
+      end
     )
   end
 end
