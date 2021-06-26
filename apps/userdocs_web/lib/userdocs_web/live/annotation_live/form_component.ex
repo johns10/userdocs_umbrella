36b3b23e27ddd152648ebafa3ce05489cdf157fb
johns10 <johns10davenport@gmail.com>
2021-06-26 19:07:23 -0400

    refactor to slim + annotationForm

--- apps/userdocs_web/lib/userdocs_web/live/annotation_live/form_component.ex
+++ apps/userdocs_web/lib/userdocs_web/live/annotation_live/form_component.ex
@@ -1,5 +1,5 @@
 defmodule UserDocsWeb.AnnotationLive.FormComponent do
-  use UserDocsWeb, :live_component
+  use UserDocsWeb, :live_slime_component
 
   require Logger
 
@@ -10,171 +10,111 @@ defmodule UserDocsWeb.AnnotationLive.FormComponent do
   @impl true
   def render(assigns) do
     ~L"""
-      <%= form = form_for @changeset, "#",
-        id: @id,
-        phx_target: @myself.cid,
-        phx_change: "validate",
-        phx_submit: "save" %>
+    = form = form_for @changeset, "#",
+      id: @id,
+      phx_target: @myself.cid,
+      phx_change: "validate",
+      phx_submit: "save"
 
-        <%= render_fields(assigns, form) %>
+      = render_fields(assigns, form)
 
-        <%= submit "Save", phx_disable_with: "Saving...", class: "button is-link" %>
+      = submit "Save", phx_disable_with: "Saving...", class: "button is-link"
 
-      </form>
+    </form>
     """
   end
 
   def render_fields(assigns, form, prefix \\ "") do
     ~L"""
+    = hidden_input(form, :name, [ value: Ecto.Changeset.get_field(@changeset, :name, "") ])
+    .field.is-grouped
+      = Layout.select_input(form, :page_id, @select_lists.pages_select,
+        [ selected: form.data.page_id || @default_page_id || "" ], "control")
+      = Layout.select_input(form, :annotation_type_id, @select_lists.annotation_types,
+        [ placeholder: form.data.annotation_type_id || "None" ], "control")
 
-      <%= hidden_input(form, :name, [
-        id: prefix <> "name-input",
-        value: Ecto.Changeset.get_field(@changeset, :name, "")
-      ]) %>
+    .field.is-grouped.is-grouped-multiline
+      = if @last_step_form.annotation.label_enabled do
+        div.control style="width: 15%;"
+          = label form, :label, class: "label"
+          div.control
+            = text_input form, :label, class: "input", type: "text"
 
-      <div class="field is-grouped">
+      = if @last_step_form.annotation.size_enabled do
+        div.control style="width: 15%;"
+          = label form, :size, class: "label"
+          div.control
+            = number_input form, :size, class: "input", type: "text"
 
-        <%= Layout.select_input(form, :page_id, @select_lists.pages_select, [
-          selected: form.data.page_id || @default_page_id || "",
-          id: prefix <> "page-select",
-        ], "control") %>
+      = if @last_step_form.annotation.x_offset_enabled do
+        div.control style="width: 15%;"
+          = label form, :x_offset, class: "label"
+          div.control
+            = number_input form, :x_offset, class: "input", type: "text"
 
-        <%= Layout.select_input(form, :annotation_type_id, @select_lists.annotation_types, [
-            placeholder: form.data.annotation_type_id || "",
-            id: prefix <> "annotation-type-select",
-          ], "control") %>
+      = if @last_step_form.annotation.y_offset_enabled do
+        div.control style="width: 15%;"
+          = label form, :y_offset, class: "label"
+          div.control
+            = number_input form, :y_offset, class: "input", type: "text"
 
-      </div>
-      <div class="field is-grouped is-grouped-multiline">
+      = if @last_step_form.annotation.font_size_enabled do
+        div.control style="width: 15%;"
+          = label form, :font_size, class: "label"
+            div.control
+              = text_input form, :font_size, class: "input", type: "text"
 
-        <%= content_tag(:div, style: "width: 15%;", class: Layout.is_hidden?("control", not("label" in @enabled_annotation_fields))) do %>
-          <%= label form, :label, class: "label" %>
-          <div class="control">
-            <%= text_input form, :label,
-              class: "input",
-              type: "text",
-              id: prefix <> "label-input" %>
-          </div>
-        <% end %>
+      = if @last_step_form.annotation.x_orientation_enabled do
+        = Layout.select_input(form, :x_orientation,
+          [{"None", ""}, { "Right", "R" }, {"Middle", "M"}, { "Left", "L" }],
+          [ placeholder: form.data.x_orientation || "" ], "control")
 
-        <%= content_tag(:div, style: "width: 15%;", class: Layout.is_hidden?("control", not("size" in @enabled_annotation_fields))) do %>
-          <%= label form, :size, class: "label" %>
-          <div class="control">
-            <%= number_input form, :size,
-              class: "input",
-              type: "text",
-              id: prefix <> "size-input" %>
-          </div>
-        <% end %>
+      = if @last_step_form.annotation.y_orientation_enabled do
+        = Layout.select_input(form, :y_orientation,
+          [{"None", ""}, { "Top", "T" }, {"Middle", "M"}, { "Bottom", "B" }],
+          [ placeholder: form.data.y_orientation || "" ], "control")
 
-        <%= content_tag(:div, style: "width: 15%;", class: Layout.is_hidden?("control", not("x_offset" in @enabled_annotation_fields))) do %>
-          <%= label form, :x_offset, class: "label" %>
-          <div class="control">
-            <%= number_input form, :x_offset,
-              class: "input",
-              type: "text",
-              id: prefix <> "x-offset-input" %>
-          </div>
-        <% end %>
+      = if @last_step_form.annotation.color_enabled do
+        = Layout.text_input(form, :color, [], "control")
 
-        <%= content_tag(:div, style: "width: 15%;", class: Layout.is_hidden?("control", not("y_offset" in @enabled_annotation_fields))) do %>
-          <%= label form, :y_offset, class: "label" %>
-          <div class="control">
-            <%= number_input form, :y_offset,
-              class: "input",
-              type: "text",
-              id: prefix <> "y-offset-input" %>
-          </div>
-        <% end %>
+      = if @last_step_form.annotation.thickness_enabled do
+        div.control style="width: 15%;"
+          = label form, :thickness, class: "label"
+          div.control
+            = text_input form, :thickness, class: "input", type: "text"
 
-        <%= content_tag(:div, style: "width: 15%;", class: Layout.is_hidden?("control", not("font_size" in @enabled_annotation_fields))) do %>
-          <%= label form, :font_size, class: "label" %>
-          <div class="control">
-            <%= text_input form, :font_size,
-              class: "input",
-              type: "text",
-              id: prefix <> "font-size-input" %>
-          </div>
-        <% end %>
-
-        <%=
-          Layout.select_input(form, :x_orientation,
-            [{"None", ""}, { "Right", "R" }, {"Middle", "M"}, { "Left", "L" }], [
-              placeholder: form.data.x_orientation || "",
-              id: prefix <> "x-orientation-input",
-              hidden: "x_orientation" not in @enabled_annotation_fields
-            ], "control")
-        %>
-
-        <%=
-          Layout.select_input(form, :y_orientation,
-            [{"None", ""}, { "Top", "T" }, {"Middle", "M"}, { "Bottom", "B" }], [
-              placeholder: form.data.y_orientation || "",
-              id: prefix <> "y-orientation-input",
-              hidden: not("y_orientation" in @enabled_annotation_fields),
-            ], "control")
-        %>
-
-        <%= Layout.text_input(form, :color, [
-          id: prefix <> "color-input",
-          hidden: not("color" in @enabled_annotation_fields)
-        ], "control") %>
-
-        <%= content_tag(:div, style: "width: 15%;", class: Layout.is_hidden?("control", not("thickness" in @enabled_annotation_fields))) do %>
-          <%= label form, :thickness, class: "label" %>
-          <div class="control">
-            <%= text_input form, :thickness,
-              class: "input",
-              type: "text",
-              id: prefix <> "thickness-input" %>
-          </div>
-        <% end %>
-      </div>
-
-      <div class="field is-horizontal">
-        <div class="field-label is-normal">
-          <%= label form, :content_id, class: "label" %>
-        </div>
-        <div class="field-body">
-          <div class="field has-addons">
-            <div class="control">
-              <%= content_tag :div,
+      div.field.is-horizontal
+        div.field-label.is-normal
+          = label form, :content_id, class: "label"
+        div.field-body
+          div.field.has-addons
+            div.control
+              = content_tag :div,
                 [ value: form.data.content_id,
                 button_class: "div",
                 class: "button",
                 phx_click: "new-content",
                 phx_target: @myself.cid,
-                phx_value_annotation_id: form.data.id ] do %>
-                <i class="fa fa-plus"></i>
-              <% end %>
-            </div>
+                phx_value_annotation_id: form.data.id ] do
 
-            <%= Layout.select_input(form, :content_id, @select_lists.content,
-              [
-                value: form.data.content_id,
-                id: prefix <> "content-select",
-                label: false
-              ]) %>
+                i.fa.fa-plus
 
-          </div>
-        </div>
-      </div>
-      <%= error_tag(form, :content) %>
-      <div class="pl-2">
-        <%= if form.data.id do %>
-          <%= inputs_for form, :content, fn fc -> %>
-            <div class="field is-grouped">
-              <%= hidden_input(fc, :team_id, value: @current_team.id) %>
-              <div class="control is-expanded">
-                <%= Layout.text_input(fc, :name, [ ]) %>
-              </div>
-              <div class="control">
-                <%= Layout.text_input(fc, :title, [ ]) %>
-              </div>
-            </div>
-          <% end %>
-        <% end %>
-      </div>
+            = Layout.select_input(form, :content_id, @select_lists.content,
+              [ value: form.data.content_id,
+              id: prefix <> "content-select",
+              label: false ])
+
+      = error_tag(form, :content)
+
+      div.pl-2
+        = inputs_for form, :content, fn fc ->
+          div.field.is-grouped
+            = hidden_input(fc, :team_id, value: @current_team.id)
+            div.control.is-expanded
+              = Layout.text_input(fc, :name, [ ])
+            div.control
+              = Layout.text_input(fc, :title, [ ])
+
     """
   end
 
@@ -182,17 +122,11 @@ defmodule UserDocsWeb.AnnotationLive.FormComponent do
   def update(%{annotation: annotation} = assigns, socket) do
     changeset = Web.change_annotation(annotation)
 
-    form_ids =
-      %{}
-      |> Map.put(:content, ID.prefix(annotation))
-
     {
       :ok,
       socket
       |> assign(assigns)
       |> assign(:changeset, changeset)
-      |> assign(:field_ids, field_ids(annotation))
-      |> assign(:form_ids, form_ids)
     }
   end
 
@@ -236,36 +170,5 @@ defmodule UserDocsWeb.AnnotationLive.FormComponent do
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, changeset: changeset)}
     end
-  end
-
-  def field_ids(annotation = %Web.Annotation{}) do
-    %{}
-    |> Map.put(:name, ID.form_field(annotation, :name))
-    |> Map.put(:page_id, ID.form_field(annotation, :page_id))
-    |> Map.put(:label, ID.form_field(annotation, :label))
-    |> Map.put(:x_orientation, ID.form_field(annotation, :x_orientation))
-    |> Map.put(:y_orientation, ID.form_field(annotation, :y_orientation))
-    |> Map.put(:size, ID.form_field(annotation, :size))
-    |> Map.put(:color, ID.form_field(annotation, :color))
-    |> Map.put(:thickness, ID.form_field(annotation, :thickness))
-    |> Map.put(:x_offset, ID.form_field(annotation, :x_offset))
-    |> Map.put(:y_offset, ID.form_field(annotation, :y_offset))
-    |> Map.put(:font_size, ID.form_field(annotation, :font_size))
-    |> Map.put(:content_id, ID.form_field(annotation, :content_id))
-  end
-  def field_ids(_) do
-    %{}
-    |> Map.put(:name, "")
-    |> Map.put(:page_id, "")
-    |> Map.put(:label,  "")
-    |> Map.put(:x_orientation, "")
-    |> Map.put(:y_orientation, "")
-    |> Map.put(:size, "")
-    |> Map.put(:color, "")
-    |> Map.put(:thickness, "")
-    |> Map.put(:x_offset, "")
-    |> Map.put(:y_offset, "")
-    |> Map.put(:font_size, "")
-    |> Map.put(:content_id, "")
   end
 end
