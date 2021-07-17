4ecf2833ae5bc301a01ade7938f7272f3dc18af2
johns10 <johns10davenport@gmail.com>
2021-07-17 08:55:27 -0400

    Liner + warnings

--- apps/userdocs_web/lib/userdocs_web/live/step_live/browser_events.ex
+++ apps/userdocs_web/lib/userdocs_web/live/step_live/browser_events.ex
@@ -1,16 +1,20 @@
 defmodule UserDocsWeb.StepLive.BrowserEvents do
+  @moduledoc """
+    This module handles events pushed from the browser into the LiveView application, most noteably, authoring events that occur in the extension
+  """
   require Logger
   alias Phoenix.LiveView.Socket
   alias UserDocs.Automation
   alias UserDocs.Web
+  alias UserDocsWeb.Router.Helpers
 
   def step_type_id(payload) do
     step_type(payload)
     |> Map.get(:id, nil)
   end
-  def step_type(%{ "action" => "ITEM_SELECTED" }), do: nil
+  def step_type(%{"action" => "ITEM_SELECTED"}), do: nil
   def step_type(payload) do
-    Automation.list_step_types(%{ filters: [ name: payload["action"] ]})
+    Automation.list_step_types(%{filters: [name: payload["action"]]})
     |> Enum.at(0)
   end
 
@@ -24,17 +28,17 @@ defmodule UserDocsWeb.StepLive.BrowserEvents do
     |> Enum.at(0)
   end
 
-  def params(%{ payload: %{ "action" => "Navigate", "href" => href } = payload }) do
+  def params(%{payload: %{"action" => "Navigate", "href" => href} = payload}) do
     %{
       "step_type_id" => step_type_id(payload),
       "page_reference" => "page",
       "page_id" => nil,
       "page" => %{
         "url" => href
-      }
-    }
+     }
+   }
   end
-  def params(%{ payload: %{ "action" => "Click", "selector" => selector } = payload, page_id: page_id }) do
+  def params(%{payload: %{"action" => "Click", "selector" => selector} = payload, page_id: page_id}) do
     %{
       "step_type_id" => step_type_id(payload),
       "page_id" => page_id,
@@ -42,10 +46,10 @@ defmodule UserDocsWeb.StepLive.BrowserEvents do
         "page_id" => page_id,
         "strategy_id" => Web.css_strategy() |> Map.get(:id),
         "selector" => selector
-      }
-    }
+     }
+   }
   end
-  def params(%{ payload: %{ "action" => "Element Screenshot", "selector" => selector } = payload, page_id: page_id }) do
+  def params(%{payload: %{"action" => "Element Screenshot", "selector" => selector} = payload, page_id: page_id}) do
     %{
       "step_type_id" => step_type_id(payload),
       "page_id" => page_id,
@@ -53,10 +57,10 @@ defmodule UserDocsWeb.StepLive.BrowserEvents do
         "page_id" => page_id,
         "strategy_id" => Web.css_strategy() |> Map.get(:id),
         "selector" => selector
-      }
-    }
+     }
+   }
   end
-  def params(%{ payload: %{ "action" => "Apply Annotation", "selector" => selector } = payload, page_id: page_id }) do
+  def params(%{payload: %{"action" => "Apply Annotation", "selector" => selector} = payload, page_id: page_id}) do
     %{
       "step_type_id" => step_type_id(payload),
       "page_id" => page_id,
@@ -64,45 +68,45 @@ defmodule UserDocsWeb.StepLive.BrowserEvents do
         "page_id" => page_id,
         "strategy_id" => Web.css_strategy() |> Map.get(:id),
         "selector" => selector
-      },
+     },
       "annotation" => %{
         "page_id" => page_id,
         "annotation_type_id" => annotation_type_id(payload)
-      }
-    }
+     }
+   }
   end
-  def params(%{ payload: %{ "action" => "ITEM_SELECTED", "selector" => selector }, page_id: page_id }) do
+  def params(%{payload: %{"action" => "ITEM_SELECTED", "selector" => selector}, page_id: page_id}) do
     %{
       "page_id" => page_id,
       "action" => "item_selected",
       "element" => %{
         "page_id" => page_id,
         "selector" => selector
-      }
-    }
+     }
+   }
   end
 
-  def handle_action(%Socket{ assigns: %{ live_action: :index }} = socket, %{ "action" => "item_selected" }), do: socket
-  def handle_action(%Socket{ assigns: %{ live_action: action }} = socket, %{ "action" => "item_selected" } = params) when action in [ :new, :edit ] do
+  def handle_action(%Socket{assigns: %{live_action: :index}} = socket, %{"action" => "item_selected"}), do: socket
+  def handle_action(%Socket{assigns: %{live_action: action}} = socket, %{"action" => "item_selected"} = params) when action in [:new, :edit] do
     Phoenix.LiveView.assign(socket, :step_params, params)
   end
-  def handle_action(%Socket{ assigns: %{ live_action: :index }} = socket, %{} = params) do
-    route = UserDocsWeb.Router.Helpers.step_index_path(socket, :new, socket.assigns.process, %{ step_params: params })
+  def handle_action(%Socket{assigns: %{live_action: :index}} = socket, %{} = params) do
+    route = Helpers.step_index_path(socket, :new, socket.assigns.process, %{step_params: params})
     socket
     |> Phoenix.LiveView.push_patch(to: route)
   end
-  def handle_action(%Socket{ assigns: %{ live_action: :new }} = socket, %{} = params) do
+  def handle_action(%Socket{assigns: %{live_action: :new}} = socket, %{} = params) do
     socket
     |> Phoenix.LiveView.assign(:step_params, params)
   end
-  def handle_action(%Socket{ assigns: %{ live_action: :edit }} = socket, %{} = params) do
+  def handle_action(%Socket{assigns: %{live_action: :edit}} = socket, %{} = params) do
     socket
     |> Phoenix.LiveView.assign(:step_params, params)
   end
-  def handle_action(%Socket{ assigns: %{ live_action: action }}, _params) do
+  def handle_action(%Socket{assigns: %{live_action: action}}, _params) do
     throw("Action handler not implemented for #{action}")
   end
-  def handle_action(socket, _params) do
+  def handle_action(_socket, _params) do
     throw("Action Probably not on socket")
   end
 
