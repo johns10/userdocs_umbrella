473d438eb5c9ad359badbda68373d56ecfd68bda
johns10 <johns10davenport@gmail.com>
2021-07-18 12:05:16 -0400

    linter

--- apps/state_handlers/lib/state_handlers/list.ex
+++ apps/state_handlers/lib/state_handlers/list.ex
@@ -5,7 +5,7 @@ defmodule StateHandlers.List do
   def apply(state, schema, opts) do
     log_string = "StateHandlers.List in location #{opts[:location]} on #{Helpers.type(schema)} with opts #{inspect(opts)}"
     if opts[:debug], do: IO.puts(log_string)
-    { filter, opts } = Keyword.pop(opts, :filter, nil)
+    {filter, opts} = Keyword.pop(opts, :filter, nil)
     state
     |> Helpers.maybe_access_assigns()
     |> Helpers.maybe_access_location(opts[:location])
@@ -28,9 +28,9 @@ defmodule StateHandlers.List do
   def filter_list_by_ids(data, ids), do: Enum.filter(data, fn(d) -> d.id in ids end)
 
   defp maybe_filter_by_field(data, nil, _), do: data
-  defp maybe_filter_by_field(data, { field, value }, nil), do: filter_list_by_field(data, { field, value})
-  defp maybe_filter_by_field(data, { field, value }, :map), do: filter_map_by_field(data, { field, value})
-  defp maybe_filter_by_field(data, { field, value }, :list), do: filter_list_by_field(data, { field, value})
+  defp maybe_filter_by_field(data, {field, value}, nil), do: filter_list_by_field(data, {field, value})
+  defp maybe_filter_by_field(data, {field, value}, :map), do: filter_map_by_field(data, {field, value})
+  defp maybe_filter_by_field(data, {field, value}, :list), do: filter_list_by_field(data, {field, value})
 
   # Iterates over the existing order clauses and calls handle_order_clause on each,
   # if it's valid.  Passes if it's nil.  Should raise for an invalid order clause if
@@ -38,7 +38,7 @@ defmodule StateHandlers.List do
 
   defp maybe_handle_order_clauses(data, nil, _), do: data
   defp maybe_handle_order_clauses(data, [], _), do: data
-  defp maybe_handle_order_clauses(data, [ _ | _ ] = clauses, type) do
+  defp maybe_handle_order_clauses(data, [_ | _] = clauses, type) do
     Enum.reduce(clauses, data, fn(clause, data) -> handle_order_clause(data, clause, type) end)
   end
   defp maybe_handle_order_clauses(_, clauses, _), do: raise(RuntimeError, "maybe_handle_order_clauses was passed invalid order clauses: #{inspect(clauses)}")
@@ -47,10 +47,10 @@ defmodule StateHandlers.List do
   # preloads, we pass, for example:
 
   #   maybe_order(data, nil, _)
-  #   maybe_order(data, {document_versions : %{ field: :id, order: :desc }})
+  #   maybe_order(data, {document_versions : %{field: :id, order: :desc}})
 
   # When we pass with nested orders, they should get passed on the appropriate child in
-  # preload.  If we pass in a order of the form %{ field: _, order: _}, we should order
+  # preload.  If we pass in a order of the form %{field: _, order: _}, we should order
   # the data according to the args.
 
   # Only supports :asc and :desc, and assumes that elixir will do the right thing with
@@ -60,10 +60,10 @@ defmodule StateHandlers.List do
   # have multiple order clauses
 
   defp handle_order_clause(data, nil, _), do: data
-  defp handle_order_clause(data, { _, %{}}, _), do: data
-  defp handle_order_clause(data, { _, [ _ ]}, _), do: data
-  defp handle_order_clause(data, [ _ ], _), do: data
-  defp handle_order_clause(data, %{ field: field, order: order }, :list) do
+  defp handle_order_clause(data, {_, %{}}, _), do: data
+  defp handle_order_clause(data, {_, [_]}, _), do: data
+  defp handle_order_clause(data, [_], _), do: data
+  defp handle_order_clause(data, %{field: field, order: order}, :list) do
     #IO.puts("handle_order_clause")
     Enum.sort(data,
       fn(i1, i2) ->
@@ -75,11 +75,11 @@ defmodule StateHandlers.List do
     )
   end
 
-  def filter_map_by_field(data, { field, value}) do
+  def filter_map_by_field(data, {field, value}) do
     Enum.filter(data, fn({_, o}) -> Map.get(o, field) == value end)
   end
 
-  def filter_list_by_field(data, { field, value}) do
+  def filter_list_by_field(data, {field, value}) do
     Enum.filter(data, fn(d) -> Map.get(d, field) == value end)
   end
 
