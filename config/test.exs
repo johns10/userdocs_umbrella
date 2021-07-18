61059e44da02059fe1eda16a3250a375498653a7
johns10 <johns10davenport@gmail.com>
2021-07-18 11:49:45 -0400

    Bamboo test adapter

--- config/test.exs
+++ config/test.exs
@@ -15,6 +15,9 @@ config :userdocs, UserDocs.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
+config :userdocs, UserDocs.Mailer,
+  adapter: Bamboo.TestAdapter
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :userdocs_web, UserDocsWeb.Endpoint,
