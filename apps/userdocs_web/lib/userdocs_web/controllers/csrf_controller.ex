99d5fbaff4c80189724f46060d6addd5a76f7573
johns10 <johns10davenport@gmail.com>
2021-07-25 18:01:23 -0400

    whitespace

--- apps/userdocs_web/lib/userdocs_web/controllers/csrf_controller.ex
+++ apps/userdocs_web/lib/userdocs_web/controllers/csrf_controller.ex
@@ -1,8 +1,7 @@
 # web/controllers/user_controller
 defmodule UserDocsWeb.CSRFController do
   use UserDocsWeb, :controller
-def index(conn, _params) do
-    
+  def index(conn, _params) do
     json conn, Map.get(get_session(conn), "_csrf_token")
   end
 end
