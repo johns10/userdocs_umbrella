909e5e3cfc54881c52f778aed313004159edd2b5
johns10 <johns10davenport@gmail.com>
2021-07-28 18:36:01 -0400

    Add policy check

--- apps/userdocs_web/lib/userdocs_web/api/resolvers/user.ex
+++ apps/userdocs_web/lib/userdocs_web/api/resolvers/user.ex
@@ -3,7 +3,11 @@ defmodule UserDocsWeb.API.Resolvers.User do
 
   alias UserDocs.Users
 
-  def get_user!(_parent, %{id: id}, _resolution) do
-    {:ok, Users.get_user!(id, %{selected_team: true})}
+  def get_user!(_parent, %{id: id}, %{context: %{current_user: current_user}}) do
+    user = Users.get_user!(id, %{selected_team: true})
+    case Bodyguard.permit(UserDocs.Users, :get_user!, current_user, user) do
+      :ok -> {:ok, user}
+      {:error, :unauthorized} -> {:error, %{message: "Unauthorized", status: 401}}
+    end
   end
 end
