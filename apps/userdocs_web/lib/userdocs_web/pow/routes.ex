bbb0577357d9975447ecc168e653ac9b00fbf007
johns10 <johns10davenport@gmail.com>
2021-07-30 00:14:33 -0400

    Upt the user on the after signup redirect

--- apps/userdocs_web/lib/userdocs_web/pow/routes.ex
+++ apps/userdocs_web/lib/userdocs_web/pow/routes.ex
@@ -1,6 +1,11 @@
 defmodule UserDocsWeb.Pow.Routes do
+  @moduledoc """
+  Contains the redirect routes for pow
+  """
   use Pow.Phoenix.Routes
   alias UserDocsWeb.Router.Helpers, as: Routes
 
-  def after_registration_path(conn), do: Routes.signup_index_path(conn, :setup)
+  def after_registration_path(conn) do
+    Routes.signup_index_path(conn, :edit, conn.assigns.current_user)
+  end
 end
