13bc78f068c61e9450351b8315733be2d5e0dafa
johns10 <johns10davenport@gmail.com>
2021-08-01 19:33:02 -0400

    Status Component

--- apps/userdocs_web/lib/userdocs_web/live/services_live/status_component.ex
+++ apps/userdocs_web/lib/userdocs_web/live/services_live/status_component.ex
@@ -16,9 +16,9 @@ defmodule UserDocsWeb.ServicesLive.StatusComponent do
     {
       :ok,
       socket
-      |> assign(:server_status, "not_running")
-      |> assign(:client_status, "not_running")
-      |> assign(:runner_status, "not_running")
+      |> assign(:server_status, :none)
+      |> assign(:client_status, :none)
+      |> assign(:runner_status, :none)
     }
   end
 
@@ -35,22 +35,26 @@ defmodule UserDocsWeb.ServicesLive.StatusComponent do
     {
       :noreply,
       socket
-      |> client_status(params["client"])
-      |> runner_status(params["server"])
-      |> server_status(params["runner"])
+      |> client_status(status(params["client"]))
+      |> runner_status(status(params["server"]))
+      |> server_status(status(params["runner"]))
     }
   end
 
   def status_element(symbol, status, id) do
-    class = status_class(Map.get(@status_map, status))
+    class = status_class(status)
     content_tag(:div, id: id, class: class, data_status: status) do
       symbol
     end
   end
+
   def status_class(:ok), do: "rounded-full bg-green-600 text-white h-3 w-3 flex items-center justify-center text-sm m-0.5"
   def status_class(:error), do: "rounded-full bg-red-600 text-white h-3 w-3 flex items-center justify-center text-sm m-0.5"
+  def status_class(_), do: "rounded-full bg-gray-300 text-white h-3 w-3 flex items-center justify-center text-sm m-0.5"
 
   def client_status(socket, status), do: assign(socket, :client_status, status)
   def runner_status(socket, status), do: assign(socket, :runner_status, status)
   def server_status(socket, status), do: assign(socket, :server_status, status)
+
+  def status(status), do: Map.get(@status_map, status)
 end
