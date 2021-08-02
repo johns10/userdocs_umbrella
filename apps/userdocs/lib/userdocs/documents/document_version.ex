e58467cb5995ac7f81112b9441f9cc6ced63f826
johns10 <johns10davenport@gmail.com>
2021-08-02 19:20:29 -0400

    lint

--- apps/userdocs/lib/userdocs/documents/document_version.ex
+++ apps/userdocs/lib/userdocs/documents/document_version.ex
@@ -16,7 +16,7 @@ defmodule UserDocs.Documents.DocumentVersion do
     field :delete, :boolean, virtual: true
 
     field :name, :string
-    field :map, { :map, :integer }
+    field :map, {:map, :integer}
 
     belongs_to :document, Document
     belongs_to :version, Version
@@ -31,7 +31,7 @@ defmodule UserDocs.Documents.DocumentVersion do
   def changeset(document_version, attrs) do
     document_version
     |> Map.put(:temp_id, (document_version.temp_id || attrs["temp_id"]))
-    |> cast(attrs, [ :name, :document_id, :version_id, :docubit_id, :delete ])
+    |> cast(attrs, [:name, :document_id, :version_id, :docubit_id, :delete])
     |> body_is_container_docubit_if_empty()
     |> cast_assoc(:body, with: &Docubit.changeset/2)
     |> foreign_key_constraint(:version_id)
@@ -42,7 +42,7 @@ defmodule UserDocs.Documents.DocumentVersion do
 
   defp body_is_container_docubit_if_empty(changeset) do
     container_type = Documents.get_docubit_type!("container")
-    attrs = %{ docubit_type_id: container_type.id, address: [0] }
+    attrs = %{docubit_type_id: container_type.id, address: [0]}
 
     case get_field(changeset, :docubit_id) do
       nil -> put_change(changeset, :body, attrs)
@@ -51,28 +51,28 @@ defmodule UserDocs.Documents.DocumentVersion do
     end
   end
 
-  def map_docubits(%DocumentVersion{ docubits: _ } = document_version), do: MapDocubits.apply(document_version)
-  def add_docubit_to_map(map, %Docubit{ address: _ } = docubit), do: MapDocubits.add_to_map(map, docubit)
+  def map_docubits(%DocumentVersion{docubits: _} = document_version), do: MapDocubits.apply(document_version)
+  def add_docubit_to_map(map, %Docubit{address: _} = docubit), do: MapDocubits.add_to_map(map, docubit)
 
   alias UserDocs.Documents.Docubit
 
-  def load(%DocumentVersion{ docubits: _ } = document_version, state, opts) do
-    # preloads = [ :content, :through_annotation ]
+  def load(%DocumentVersion{docubits: _} = document_version, state, opts) do
+    # preloads = [:content, :through_annotation]
     map = MapDocubits.apply(document_version)
 
     Map.put(document_version, :body, traverse_docubit_map(map, state, opts))
   end
 
   def traverse_docubit_map(map, state, opts) do
-    docubit_map_item({ 0, Map.get(map, 0) }, state, %Context{}, opts)
+    docubit_map_item({0, Map.get(map, 0)}, state, %Context{}, opts)
   end
-  def docubit_map_item({ _key, map }, state, parent_context, opts) do
+  def docubit_map_item({_key, map}, state, parent_context, opts) do
     docubit = StateHandlers.get(state, map.docubit.id, Docubit, opts)
-    { :ok, context } = Docubit.context(docubit, parent_context)
+    {:ok, context} = Docubit.context(docubit, parent_context)
     docubits =
       Enum.map(map.docubit.docubits,
-        fn({ address, docubit }) ->
-          docubit_map_item({ address, docubit }, state, context, opts)
+        fn({address, docubit}) ->
+          docubit_map_item({address, docubit}, state, context, opts)
         end
       )
     docubit
@@ -82,10 +82,10 @@ defmodule UserDocs.Documents.DocumentVersion do
 
   def update_docubit_item(map, state, context, opts) do
     Enum.reduce(map.docubit.docubits, map,
-      fn({ address, item }, map) ->
+      fn({address, item}, map) ->
         Kernel.put_in(map,
-          [ :docubit, :docubits, address ],
-          docubit_map_item({ address, item }, state, context, opts)
+          [:docubit, :docubits, address],
+          docubit_map_item({address, item}, state, context, opts)
         )
       end
     )
