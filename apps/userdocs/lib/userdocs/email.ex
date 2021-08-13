dff55a87b9797287ada0ec93663a67bfafc25aad
johns10 <johns10davenport@gmail.com>
2021-08-13 00:24:51 -0400

    Add onboarding to email

--- apps/userdocs/lib/userdocs/email.ex
+++ apps/userdocs/lib/userdocs/email.ex
@@ -1,4 +1,5 @@
 defmodule UserDocs.Email do
+  @moduledoc false
   import Bamboo.Email
   use Bamboo.Template
 
@@ -9,6 +10,27 @@ defmodule UserDocs.Email do
     |> html_body(html)
     |> text_body(text)
     |> to(address)
+  end
+
+  def cast_onboarding(%{user: user, invited_by: invited_by, module: module, url: url}) do
+    %{
+      to: user.email,
+      subject: invited_by.email <> " has invited you to join UserDocs!",
+      assigns: %{
+        url: url,
+        invited_by_email: invited_by.email
+      },
+      module: module
+    }
+  end
+
+  def onboarding(%{to: address, subject: subject, module: module, assigns: %{invited_by_email: email, url: url}}) do
+    new_email()
+    |> from("welcome@user-docs.com")
+    |> subject(subject)
+    |> to(address)
+    |> put_view(module)
+    |> render(:invitation, %{invited_by_email: email, url: url})
   end
 
   def send(email) do
