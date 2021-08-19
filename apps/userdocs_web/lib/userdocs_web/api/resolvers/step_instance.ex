c27a88ae29bed97921cc438f18f57a2c852cddee
johns10 <johns10davenport@gmail.com>
2021-08-19 18:14:17 -0400

    update api for channels integration

--- apps/userdocs_web/lib/userdocs_web/api/resolvers/step_instance.ex
+++ apps/userdocs_web/lib/userdocs_web/api/resolvers/step_instance.ex
@@ -5,40 +5,49 @@ defmodule UserDocsWeb.API.Resolvers.StepInstance do
   alias UserDocs.Jobs.JobStep
   alias UserDocs.Automation.Step
 
-  def list_step_instances(%ProcessInstance{ step_instances: step_instances }, _args, _resolution) when is_list(step_instances) do
-    IO.inspect("Listing step instances for a process instance")
-    { :ok, step_instances }
+  def list_step_instances(%ProcessInstance{step_instances: step_instances}, _args, _resolution) when is_list(step_instances) do
+    {:ok, step_instances}
   end
 
 
-  def list_step_instances(%JobStep{ step_instance: step_instance }, _args, _resolution)  do
-    IO.inspect("Listing step instances for a job")
-    { :ok, step_instance }
+  def list_step_instances(%JobStep{step_instance: step_instance}, _args, _resolution)  do
+    {:ok, step_instance}
   end
 
-  """
-  def list_step_instances(_parent, _args, _resolution) do
-    { :ok, UserDocs.StepInstances.list_step_instances() }
+  def get_step_instance!(%JobStep{step_instance: step_instance}, _args, resolution) do
+    {:ok, step_instance}
   end
-  """
-
-  def get_step_instance!(%JobStep{ step_instance: step_instance}, _args, resolution) do
-    { :ok, step_instance }
-  end
-  def get_step_instance!(%Step{ last_step_instance: step_instance }, _args, _resolution) do
-    { :ok, step_instance }
+  def get_step_instance!(%Step{last_step_instance: step_instance}, _args, _resolution) do
+    {:ok, step_instance}
   end
   def get_step_instance!(_parent, %{id: id}, resolution) do
-    step_instance = UserDocs.StepInstances.get_step_instance!(id, %{ preloads: "*"})
-    { :ok, step_instance }
+    step_instance = UserDocs.StepInstances.get_step_instance!(id, %{preloads: "*"})
+    {:ok, step_instance}
   end
 
-
-  def update_step_instance(_parent, %{id: id} = args, resolution) do
-    step_instance = StepInstances.get_step_instance!(args.id, %{ preloads: "*"})
-    StepInstances.update_step_instance(step_instance, map_base64(args))
+  def update_step_instance(_parent, %{id: id} = args, %{context: %{current_user: current_user}}) do
+    step_instance = UserDocs.StepInstances.get_step_instance!(id, %{preloads: "*"})
+    {:ok, step_instance} = StepInstances.update_step_instance(step_instance, args)
+    team_id = step_instance.step.page.version.project.team_id
+    channel =  "user:" <> to_string(current_user.id)
+    UserDocsWeb.Endpoint.broadcast(channel, "update", step_instance)
+    {:ok, step_instance}
   end
 
+  def create_step_instance(_parent, %{step_id: step_id} = args, %{context: %{current_user: current_user}}) do
+    step = UserDocs.Automation.get_step!(step_id)
+    case Bodyguard.permit(StepInstances, :create_step_instance!, current_user, step) do
+      :error -> {:error, %{message: "Unauthorized", code: 401}}
+      :ok ->
+        {:ok, step_instance} = StepInstances.create_step_instance(args)
+        step_instance = UserDocs.StepInstances.get_step_instance!(step_instance.id, %{preloads: "*"})
+        team_id = step_instance.step.page.version.project.team_id
+        channel = UserDocsWeb.Defaults.channel(team_id)
+        UserDocsWeb.Endpoint.broadcast(channel, "create", step_instance)
+        IO.puts("After broadcast")
+        {:ok, step_instance}
+    end
+  end
 
   def map_base64(args) do
     try do
@@ -49,7 +58,7 @@ defmodule UserDocsWeb.API.Resolvers.StepInstance do
         |> Map.put(:base64, args.step.screenshot.base64)
         |> Map.delete(:base64)
 
-      Kernel.put_in(args, [ :step, :screenshot ], screenshot)
+      Kernel.put_in(args, [:step, :screenshot], screenshot)
     catch
       _ -> args
     end
