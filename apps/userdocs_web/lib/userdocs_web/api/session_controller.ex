39c207a0ebff00e9f9abda16108431021c54b176
johns10 <johns10davenport@gmail.com>
2021-08-19 18:26:54 -0400

    ugh, guess I need it

---
+++ apps/userdocs_web/lib/userdocs_web/api/session_controller.ex
@@ -0,0 +1,52 @@
+defmodule UserDocsWeb.API.SessionController do
+  use UserDocsWeb, :controller
+
+  alias Plug.Conn
+
+  @spec create(Conn.t(), map()) :: Conn.t()
+  def create(conn, %{"user" => user_params}) do
+    conn
+    |> Pow.Plug.authenticate_user(user_params)
+    |> case do
+      {:ok, conn} ->
+        response = %{
+          data:
+          %{
+            access_token: conn.private.api_access_token,
+            renewal_token: conn.private.api_renewal_token,
+            user_id: conn.assigns.current_user.id
+          }
+        }
+        json(conn, response)
+
+      {:error, conn} ->
+        conn
+        |> put_status(401)
+        |> json(%{error: %{status: 401, message: "Invalid email or password"}})
+    end
+  end
+
+  @spec renew(Conn.t(), map()) :: Conn.t()
+  def renew(conn, _params) do
+    config = Pow.Plug.fetch_config(conn)
+
+    conn
+    |> UserDocsWeb.API.Auth.Plug.renew(config)
+    |> case do
+      {conn, nil} ->
+        conn
+        |> put_status(401)
+        |> json(%{error: %{status: 401, message: "Invalid token"}})
+
+      {conn, _user} ->
+        json(conn, %{data: %{access_token: conn.private.api_access_token, renewal_token: conn.private.api_renewal_token}})
+    end
+  end
+
+  @spec delete(Conn.t(), map()) :: Conn.t()
+  def delete(conn, _params) do
+    conn
+    |> Pow.Plug.delete()
+    |> json(%{data: %{}})
+  end
+end
