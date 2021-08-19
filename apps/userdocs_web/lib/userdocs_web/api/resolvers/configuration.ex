5420c4ca346732e9154046c75ee7c293f2d77fc7
johns10 <johns10davenport@gmail.com>
2021-08-19 18:22:04 -0400

    Update for channels integration

--- apps/userdocs_web/lib/userdocs_web/api/resolvers/configuration.ex
+++ apps/userdocs_web/lib/userdocs_web/api/resolvers/configuration.ex
@@ -2,11 +2,12 @@ defmodule UserDocsWeb.API.Resolvers.Configuration do
   @moduledoc "Graphql Resolver for Users"
 
   alias UserDocs.Users
+  alias UserDocs.Users.User
   alias UserDocs.Projects
 
-  def get_configuration!(parent, %{}, _resolution) do
-    team = Users.get_team!(parent.selected_team_id)
-    version = Projects.get_version!(parent.selected_version_id, %{strategy: true})
-    {:ok, %{css: team.css, strategy: version.strategy.name}}
+  def get_configuration!(%User{selected_team_id: team_id, selected_version_id: version_id, overrides: overrides}, %{}, _resolution) do
+    team = Users.get_team!(team_id)
+    version = Projects.get_version!(version_id, %{strategy: true})
+    {:ok, %{css: team.css, strategy: version.strategy.name, overrides: overrides}}
   end
 end
