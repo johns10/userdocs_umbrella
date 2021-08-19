b5fb43d81402acaefe6c4c0c294a350b9dc47139
johns10 <johns10davenport@gmail.com>
2021-08-19 18:01:29 -0400

    Add user channel, and connection validation

--- apps/userdocs_web/lib/userdocs_web/channels/user_socket.ex
+++ apps/userdocs_web/lib/userdocs_web/channels/user_socket.ex
@@ -1,8 +1,11 @@
 defmodule UserDocsWeb.UserSocket do
   use Phoenix.Socket
 
+  require Logger
+
   ## Channels
   # channel "room:*", UserDocsWeb.RoomChannel
+  channel "user:*", UserDocsWeb.UserChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
@@ -16,8 +19,18 @@ defmodule UserDocsWeb.UserSocket do
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
   @impl true
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+  def connect(%{"token" => token}, socket, _connect_info) do
+    salt = Atom.to_string(UserDocsWeb.API.Auth.Plug)
+    config = Application.get_env(:userdocs_web, :pow)
+    conn = %Plug.Conn{secret_key_base: UserDocsWeb.Endpoint.config(:secret_key_base)}
+           |> Pow.Plug.put_config(otp_app: :userdocs_web)
+
+    case Pow.Plug.verify_token(conn, salt, token, config) do
+      {:ok, _token} -> {:ok, socket}
+      result ->
+        Logger.error("Authentication Failed #{result}")
+        :error
+    end
   end
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
