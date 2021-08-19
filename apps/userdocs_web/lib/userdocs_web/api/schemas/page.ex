c6b1ab94d965c222f62b05cb331f0afd88355075
johns10 <johns10davenport@gmail.com>
2021-08-19 18:06:40 -0400

    Resolver for version

--- apps/userdocs_web/lib/userdocs_web/api/schemas/page.ex
+++ apps/userdocs_web/lib/userdocs_web/api/schemas/page.ex
@@ -1,10 +1,13 @@
 defmodule UserDocsWeb.API.Schema.Page do
+  @moduledoc false
   use Absinthe.Schema.Notation
+  alias UserDocsWeb.API.Resolvers
 
   object :page do
     field :id, :id
     field :order, :integer
     field :name, :string
     field :url, :string
+    field :version, :version, resolve: &Resolvers.Version.get_version!/3
   end
 end
