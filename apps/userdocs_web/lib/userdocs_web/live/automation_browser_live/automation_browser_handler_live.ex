53beb8fbc70d1828b10ea9ad9c251622972fcd89
johns10 <johns10davenport@gmail.com>
2021-08-19 17:56:35 -0400

    Use broadcast + session in the root

--- apps/userdocs_web/lib/userdocs_web/live/automation_browser_live/automation_browser_handler_live.ex
+++ apps/userdocs_web/lib/userdocs_web/live/automation_browser_live/automation_browser_handler_live.ex
@@ -1,4 +1,5 @@
 defmodule UserDocsWeb.AutomationBrowserHandlerLive do
+  @moduledoc false
   use UserDocsWeb, :live_component
 
   @impl true
@@ -13,38 +14,29 @@ defmodule UserDocsWeb.AutomationBrowserHandlerLive do
 
   @impl true
   def handle_event("clear-browser", _params, socket) do
-    attrs = %{ browser_session: nil }
-    user = update_user(socket.assigns.current_user, attrs)
-    {
-      :noreply,
-      socket
-      |> assign(:current_user, user)
-
-    }
+    user_id = socket.assigns.current_user.id |> to_string
+    UserDocsWeb.Endpoint.broadcast("user:" <> user_id, "command:close_browser", %{})
+    UserDocsWeb.Endpoint.broadcast("user:" <> user_id, "command:clear_browser", %{})
+    {:noreply, socket}
   end
   def handle_event("open-browser", _params, socket) do
-    send(self(), { :update_session, [ { "user_opened_browser", true } ] })
-    { :noreply, socket |> push_event("open-browser", %{}) }
-  end
-  def handle_event("browser-opened", _params, socket) do
-    send(self(), { :update_session, [ { "browser_opened", true } ] })
-    { :noreply, socket }
+    user_id = socket.assigns.current_user.id |> to_string
+    UserDocsWeb.Endpoint.broadcast("user:" <> user_id, "event:user_opened_browser", %{})
+    UserDocsWeb.Endpoint.broadcast("user:" <> user_id, "command:open_browser", %{})
+    {:noreply, socket}
   end
 
   def handle_event("close-browser", _params, socket) do
-    send(self(), { :update_session, [ { "user_opened_browser", false } ] })
-    { :noreply, socket |> push_event("close-browser", %{}) }
-  end
-
-  def handle_event("browser-closed", _params, socket) do
-    send(self(), { :update_session, [ { "browser_opened", false } ] })
-    { :noreply, socket }
+    user_id = socket.assigns.current_user.id |> to_string
+    UserDocsWeb.Endpoint.broadcast("user:" <> user_id, "event:user_closed_browser", %{})
+    UserDocsWeb.Endpoint.broadcast("user:" <> user_id, "command:close_browser", %{})
+    {:noreply, socket}
   end
 
   def update_user(user, attrs) do
     case UserDocs.Users.update_user_browser_session(user, attrs) do
-      { :ok, user } -> user
-      { _, changeset }-> raise(changeset)
+      {:ok, user} -> user
+      {_, changeset} -> raise(changeset)
     end
   end
 
