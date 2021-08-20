47e4e05af1b5820c07ed293a4861c7cfe26017b9
johns10 <johns10davenport@gmail.com>
2021-08-19 23:01:16 -0400

    baseurl

--- apps/userdocs_web/lib/userdocs_web/api/schemas/project.ex
+++ apps/userdocs_web/lib/userdocs_web/api/schemas/project.ex
@@ -1,10 +1,10 @@
 defmodule UserDocsWeb.API.Schema.Project do
   @moduledoc false
   use Absinthe.Schema.Notation
-  alias UserDocsWeb.API.Resolvers
 
   object :project do
     field :id, :id
     field :name, :string
+    field :base_url, :string
   end
 end
