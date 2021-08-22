0197916bafb7016dc4000036374854746716e836
johns10 <johns10davenport@gmail.com>
2021-08-22 19:42:11 -0400

    Delegate user channel handlers

---
+++ apps/userdocs_web/lib/userdocs_web/channels/user_channel_handlers.ex
@@ -0,0 +1,56 @@
+defmodule UserDocsWeb.UserChannelHandlers do
+  @moduledoc false
+
+  def apply(socket, %{topic: "user:" <> _user_id, event: "presence_diff", payload: %{joins: _joins, leaves: _leaves}}) do
+    IO.inspect("Presence diff")
+    socket
+  end
+  def apply(socket, %{topic: "user:" <> _user_id, event: "event:user_clicked"}) do
+    IO.puts("root:event:user_clicked")
+    socket
+  end
+  def apply(socket, %{topic: "user:" <> _user_id, event: "event:browser_opened"}) do
+    IO.inspect("root:event:browser_opened")
+    PhoenixLiveSession.put_session(socket, "browser_opened", true)
+  end
+  def apply(socket, %{topic: "user:" <> _user_id, event: "event:user_opened_browser"}) do
+    IO.inspect("root:event:user_opened_browser")
+    PhoenixLiveSession.put_session(socket, "user_opened_browser", true)
+  end
+  def apply(socket, %{topic: "user:" <> _user_id, event: "event:browser_closed"}) do
+    IO.inspect("root:event:browser_closed")
+    PhoenixLiveSession.put_session(socket, "browser_opened", false)
+  end
+  def apply(socket, %{topic: "user:" <> _user_id, event: "event:user_closed_browser"}) do
+    IO.inspect("root:event:user_opened_browser")
+    PhoenixLiveSession.put_session(socket, "user_opened_browser", false)
+  end
+  def apply(socket, %{topic: "user:" <> _user_id, event: "command:clear_browser"}) do
+    IO.inspect("root:command:clear_browser")
+    socket
+    |> PhoenixLiveSession.put_session("user_opened_browser", false)
+    |> PhoenixLiveSession.put_session("browser_opened", false)
+  end
+  def apply(socket, %{topic: _, event: "command:" <> _command, payload: _}), do: socket
+
+  def apply(socket, %{topic: topic, event: event, payload: payload}) do
+    schema = case payload do
+      %{objects: [object | _ ]} -> object.__meta__.schema
+      object -> object.__meta__.schema
+    end
+
+    case Keyword.get(socket.assigns.state_opts, :types) do
+      nil -> raise(RuntimeError, "Types not populated in calling subscribed view #{socket.view}")
+      _ -> ""
+    end
+
+    socket =
+      case schema in socket.assigns.state_opts[:types] do
+        true -> UserDocs.Subscription.handle_event(socket, event, payload, socket.assigns.state_opts)
+        false -> socket
+      end
+
+    {:noreply, socket}
+  end
+
+end
