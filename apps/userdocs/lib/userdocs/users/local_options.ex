2868bd338bc39ebdb90715d2564c3cfed0822a93
johns10 <johns10davenport@gmail.com>
2021-08-25 18:31:42 -0400

    add chrome path to schema

--- apps/userdocs/lib/userdocs/users/local_options.ex
+++ apps/userdocs/lib/userdocs/users/local_options.ex
@@ -9,6 +9,7 @@ defmodule UserDocs.Users.LocalOptions do
     field :image_path, :string
     field :max_retries, :integer
     field :user_data_dir_path, :string
+    field :chrome_path, :string
     field :css, :string
     embeds_many :overrides, Override, on_replace: :raise
   end
@@ -16,7 +17,7 @@ defmodule UserDocs.Users.LocalOptions do
   @doc false
   def changeset(local_options, attrs) do
     local_options
-    |> cast(attrs, [:image_path, :max_retries, :user_data_dir_path, :css])
+    |> cast(attrs, [:image_path, :max_retries, :user_data_dir_path, :css, :chrome_path])
     |> cast_embed(:overrides)
   end
 end
