e97c443224f932cc05df817f69ae1bc0ce68019e
johns10 <johns10davenport@gmail.com>
2021-08-25 18:34:09 -0400

    Allow chrome extensions

--- config/prod.exs
+++ config/prod.exs
@@ -14,12 +14,7 @@ config :userdocs_web, UserDocsWeb.Endpoint,
   http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
   url: [host: "app.user-docs.com", port: 443],
   check_origin: [
-    "chrome-extension://hanghhicicijhmpofndpemhlfojfdnfj",
-    "chrome-extension://mkbbhaeofgjbbhopopbjnjjjfbbnaone",
-    "chrome-extension://dieemmbgjkifojmoegfgiekpbodeidcd",
-    "chrome-extension://alolcmhepgllblhpnakjcmbjngadbkpi",
-    "chrome-extension://lfaidgfkijcdncojclecfjocihfoecpd", # Lukas id
-    "chrome-extension://oogkkkaeofonaibdnpdckkkahhceojhk", # Chris id
+    "chrome-extension://*",
     "https://app.user-docs.com",
     ],
   cache_static_manifest: "priv/static/cache_manifest.json",
