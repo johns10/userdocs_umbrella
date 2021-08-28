dc1791037a130a88db50e8159592b29485cd69de
johns10 <johns10davenport@gmail.com>
2021-08-28 15:33:17 -0400

    Add browser timeout field

--- apps/userdocs/lib/userdocs/users/local_options.ex
+++ apps/userdocs/lib/userdocs/users/local_options.ex
@@ -1,13 +1,15 @@
 defmodule UserDocs.Users.LocalOptions do
+  @moduledoc false
   use Ecto.Schema
   import Ecto.Changeset
 
   alias UserDocs.Users.Override
 
-  @derive {Jason.Encoder, only: [:image_path, :max_retries, :user_data_dir_path]}
+  @derive {Jason.Encoder, only: [:image_path, :max_retries, :user_data_dir_path, :chrome_path, :browser_timeout]}
   schema "local_options" do
     field :image_path, :string
     field :max_retries, :integer
+    field :browser_timeout, :integer
     field :user_data_dir_path, :string
     field :chrome_path, :string
     field :css, :string
@@ -17,7 +19,7 @@ defmodule UserDocs.Users.LocalOptions do
   @doc false
   def changeset(local_options, attrs) do
     local_options
-    |> cast(attrs, [:image_path, :max_retries, :user_data_dir_path, :css, :chrome_path])
+    |> cast(attrs, [:image_path, :max_retries, :user_data_dir_path, :css, :chrome_path, :browser_timeout])
     |> cast_embed(:overrides)
   end
 end
