01b363f1710fa9397046914d36128bb4a63060b7
johns10 <johns10davenport@gmail.com>
2021-09-03 17:15:10 -0400

    jobs refactor

--- apps/userdocs_web/lib/userdocs_web/live/automation_manager_live/automation_manager_live.ex
+++ apps/userdocs_web/lib/userdocs_web/live/automation_manager_live/automation_manager_live.ex
@@ -1,15 +1,16 @@
 defmodule UserDocsWeb.AutomationManagerLive do
   use UserDocsWeb, :live_slime_component
 
-  alias UserDocs.Automation
   alias UserDocs.Automation.Step
   alias UserDocs.AutomationManager
+  alias UserDocs.Helpers
   alias UserDocs.Jobs
   alias UserDocs.Jobs.JobProcess
   alias UserDocs.Jobs.JobStep
-  alias UserDocs.StepInstances
-
   alias UserDocs.ProcessInstances
+  alias UserDocs.StepInstances
+  alias UserDocs.Users
+
 
   require Logger
 
@@ -20,11 +21,15 @@ defmodule UserDocsWeb.AutomationManagerLive do
     {
       :ok,
       socket
+      |> assign(:select_lists, %{jobs: []})
    }
   end
 
   @impl true
   def update(assigns, socket) do
+    jobs_select_items =
+      Jobs.list_jobs(%{filters: [team_id: assigns.current_team.id]})
+      |> Helpers.select_list(:name, true)
 
     if assigns.job_id do
       job =
@@ -36,6 +41,7 @@ defmodule UserDocsWeb.AutomationManagerLive do
         :ok,
         socket
         |> assign(assigns)
+        |> assign(:select_lists, %{jobs: jobs_select_items})
         |> assign(:job, job)
      }
     else
@@ -139,8 +145,20 @@ defmodule UserDocsWeb.AutomationManagerLive do
   end
 
   @impl true
+  def handle_event("job-changed", %{"job" => %{"job_id" => ""}}, socket), do: {:noreply, socket}
+  def handle_event("job-changed", %{"job" => %{"job_id" => job_id}}, socket) do
+    job = Jobs.get_job!(job_id, %{preloads: [steps: true, processes: true, last_job_instance: true]})
+    {:ok, user} = Users.update_user_selections(socket.assigns.current_user, %{job_id: job.id})
+    send(self(), {:broadcast, "update", user})
+    {
+      :noreply,
+      socket
+      |> assign(:job, job)
+      |> assign(:job_id, job.id)
+    }
+  end
   def handle_event("reset-job", _payload, socket) do
-    {:ok, job_instance} = UserDocs.JobInstances.create_job_instance(socket.assigns.job)
+    {:ok, _job_instance} = UserDocs.JobInstances.create_job_instance(socket.assigns.job)
 
     {:noreply, assign_job(socket.assigns.job, socket)}
   end
@@ -159,7 +177,7 @@ defmodule UserDocsWeb.AutomationManagerLive do
       |> StepInstances.create_step_instance_from_step(Jobs.max_order(job) + 1)
 
     case Jobs.create_job_step(job, String.to_integer(step_id), step_instance.id) do
-      {:ok, job_step} -> {:noreply, job |> assign_job(socket)}
+      {:ok, _job_step} -> {:noreply, job |> assign_job(socket)}
       {:error, changeset} ->
         formatted_errors = format_changeset_errors(changeset)
         {:noreply, Phoenix.LiveView.put_flash(socket, :error,  formatted_errors)}
@@ -167,8 +185,8 @@ defmodule UserDocsWeb.AutomationManagerLive do
   end
   def handle_event("delete-job-step", %{"id" => job_step_id}, socket) do
     case Jobs.delete_job_step(%JobStep{id: String.to_integer(job_step_id)}) do
-      {:ok, job_step} -> {:noreply, socket.assigns.job |> assign_job(socket)}
-      {:error, changeset} ->
+      {:ok, _job_step} -> {:noreply, socket.assigns.job |> assign_job(socket)}
+      {:error, _changeset} ->
         {:noreply, Phoenix.LiveView.put_flash(socket, :error, "Failed to remove process from job")}
     end
   end
@@ -180,7 +198,7 @@ defmodule UserDocsWeb.AutomationManagerLive do
       |> ProcessInstances.create_process_instance_from_process(Jobs.max_order(job) + 1)
 
     case Jobs.create_job_process(job, String.to_integer(process_id), process_instance.id) do
-      {:ok, job_process} -> {:noreply, job |> assign_job(socket)}
+      {:ok, _job_process} -> {:noreply, job |> assign_job(socket)}
       {:error, changeset} ->
         formatted_errors = format_changeset_errors(changeset)
         {:noreply, Phoenix.LiveView.put_flash(socket, :error,  formatted_errors)}
@@ -193,15 +211,15 @@ defmodule UserDocsWeb.AutomationManagerLive do
         {:noreply, Phoenix.LiveView.put_flash(socket, :error, "Failed to remove process from job")}
     end
   end
-  def handle_event("delete-job", %{"id" => job_id} = payload, socket) do
+  def handle_event("delete-job", %{"id" => job_id}, socket) do
     job = UserDocs.Jobs.get_job!(job_id)
-    {:ok, job} = UserDocs.Jobs.delete_job(job)
+    {:ok, _job} = UserDocs.Jobs.delete_job(job)
     {:noreply, assign(socket, :job, nil) |> assign(:job_id, nil)}
   end
-  def handle_event("create-job", %{"team-id" => team_id} = payload, socket) do
+  def handle_event("create-job", %{"team-id" => team_id}, socket) do
     IO.puts("Create job for #{team_id}")
     {:ok, job} = UserDocs.Jobs.create_job(%{team_id: team_id})
-    {:ok, job_instance} =
+    {:ok, _job_instance} =
       job
       |> Map.put(:job_steps, [])
       |> Map.put(:job_processes, [])
@@ -209,134 +227,20 @@ defmodule UserDocsWeb.AutomationManagerLive do
 
     {:noreply, job |> assign_job(socket) |> assign(:job_id, job.id)}
   end
-  def handle_event("execute-step", %{"id" => step_id} = payload, socket) do
+  def handle_event("execute-job", %{"id" => job_id}, socket) do
+    user_id = socket.assigns.current_user.id |> to_string
+    UserDocsWeb.Endpoint.broadcast("user:" <> user_id, "command:execute_job", %{job_id: job_id})
+    {:noreply, socket}
+  end
+  def handle_event("execute-step", %{"id" => step_id}, socket) do
     user_id = socket.assigns.current_user.id |> to_string
     UserDocsWeb.Endpoint.broadcast("user:" <> user_id, "command:execute_step", %{step_id: step_id})
     {:noreply, socket}
   end
-  def handle_event("execute-process", %{"id" => process_id} = payload, socket) do
+  def handle_event("execute-process", %{"id" => process_id}, socket) do
     user_id = socket.assigns.current_user.id |> to_string
     UserDocsWeb.Endpoint.broadcast("user:" <> user_id, "command:execute_process", %{process_id: process_id})
     {:noreply, socket}
-  end
-  def handle_event("execute-job", _payload, socket) do
-    safe_job =
-      socket.assigns.job
-      |> UserDocsWeb.LiveHelpers.camel_cased_map_keys()
-
-    {
-      :noreply,
-      socket
-      |> Phoenix.LiveView.push_event("executeJob", %{job: safe_job})
-   }
-  end
-  def handle_event("update-step", %{"step" =>
-    %{"id" => id, "lastStepInstance" =>
-      %{"id" => step_instance_id, "processInstanceId" => nil}
-   } = step_attrs}, socket
-  ) do
-    underscored_step_attrs = UserDocsWeb.LiveHelpers.underscored_map_keys(step_attrs)
-    opts = UserDocsWeb.Defaults.opts(socket, types())
-
-    step =
-      Jobs.fetch_step_from_job_step(socket.assigns.job, step_instance_id)
-      || AutomationManager.get_step!(id)
-
-    changeset = Step.runner_changeset(step, underscored_step_attrs)
-    {:ok, updated_step} = UserDocs.Repo.update(changeset)
-    UserDocs.Subscription.broadcast_children(updated_step, changeset, opts)
-    send(self(), {:broadcast, "update", updated_step})
-
-
-    job = UserDocs.Jobs.update_job_step_instance(
-      socket.assigns.job, updated_step.last_step_instance)
-
-    {:noreply, assign(socket, :job, job)}
-  end
-  def handle_event("update-step", %{"step" =>
-    %{"id" => id, "lastStepInstance" =>
-      %{"id" => step_instance_id, "processInstanceId" => process_instance_id}
-   } = step_attrs}, socket
-  ) do
-    step_attrs = UserDocsWeb.LiveHelpers.underscored_map_keys(step_attrs)
-    if step_attrs["last_step_instance"]["step_id"] == nil do
-      raise "Got a nil step id for some reason, not updating"
-    end
-    Logger.info("Handling update step #{id} with process instance #{process_instance_id}.  It's step instance is #{step_attrs["last_step_instance"]["id"]}.  We'll set it's status to #{step_attrs["last_step_instance"]["status"]}")
-    opts = UserDocsWeb.Defaults.opts(socket, types())
-
-    step =
-      Jobs.fetch_step_from_job_processes(socket.assigns.job, process_instance_id, step_instance_id)
-      || AutomationManager.get_step!(id)
-
-    changeset = Step.runner_changeset(step, step_attrs)
-    {:ok, updated_step} = UserDocs.Repo.update(changeset)
-    UserDocs.Subscription.broadcast_children(updated_step, changeset, opts)
-    send(self(), {:broadcast, "update", updated_step})
-
-    job = UserDocs.Jobs.update_job_process_instance_step_instance(
-      socket.assigns.job, updated_step.last_step_instance)
-
-    {:noreply, assign(socket, :job, job)}
-  end
-  def handle_event("update-process", %{"process" => %{"id" => id, "lastProcessInstance" => process_instance_attrs} = process_attrs}, socket) do
-    Logger.info("Received update-process command for process #{id}")
-    opts = UserDocsWeb.Defaults.opts(socket, types())
-
-    updated_job_processes =
-      Enum.map(socket.assigns.job.job_processes,
-        fn(jp) ->
-          if jp.process_instance_id == process_instance_attrs["id"] do
-
-            {:ok, updated_process_instance} =
-              UserDocs.ProcessInstances.update_process_instance(jp.process_instance, process_instance_attrs)
-
-            Logger.info("Updated process instance #{updated_process_instance.id} to status #{updated_process_instance.status}")
-
-            send(self(), {:broadcast, "update", updated_process_instance})
-
-            jp
-            |> Map.put(:process_instance, updated_process_instance)
-          else
-            IO.puts("Not on job process")
-            jp
-          end
-        end
-      )
-
-    process_instance =
-      Enum.reduce(socket.assigns.job.job_processes, nil,
-        fn(jp, acc) ->
-          if jp.process_instance_id == process_instance_attrs["id"] do
-            jp.process_instance
-          else
-            acc
-          end
-        end
-      )
-      || ProcessInstances.get_process_instance!(process_instance_attrs["id"])
-
-    {:ok, updated_process_instance} =
-      UserDocs.ProcessInstances.update_process_instance(process_instance, process_instance_attrs)
-
-    send(self(), {:broadcast, "update", updated_process_instance})
-
-    updated_job_processes =
-      Enum.map(socket.assigns.job.job_processes,
-        fn(jp) ->
-          if jp.process_instance_id == process_instance_attrs["id"] do
-            Map.put(jp, :process_instance, updated_process_instance)
-          else
-            jp
-          end
-        end
-      )
-
-    updated_job =
-      socket.assigns.job
-      |> Map.put(:job_processes, updated_job_processes)
-
-    {:noreply, assign(socket, :job, updated_job)}
   end
 
   def assign_job(%Jobs.Job{id: id}, socket) do
