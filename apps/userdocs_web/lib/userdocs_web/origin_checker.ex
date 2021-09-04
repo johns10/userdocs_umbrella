df3474cdcd2fa89989adcf954f4f33c987f5cf56
johns10 <johns10davenport@gmail.com>
2021-09-04 14:27:43 -0400

    Add http/localhost in dev

--- apps/userdocs_web/lib/userdocs_web/origin_checker.ex
+++ apps/userdocs_web/lib/userdocs_web/origin_checker.ex
@@ -1,6 +1,7 @@
 defmodule UserDocsWeb.OriginChecker do
   @moduledoc false
   def check(%URI{scheme: "https", host: "dev.user-docs.com", port: _}, :dev), do: true
+  def check(%URI{scheme: "http", host: "localhost", port: _}, :dev), do: true
   def check(%URI{scheme: "https", host: "dev.user-docs.com", port: _}, :integration), do: true
   def check(%URI{scheme: "https", host: "app.user-docs.com", port: _}, :prod), do: true
   def check(%URI{scheme: "chrome-extension", host: _, port: _}, :dev), do: true
