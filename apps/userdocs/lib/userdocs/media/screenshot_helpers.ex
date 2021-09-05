17ca8422bd1fef4a53d728e8cba5f1381344808d
johns10 <johns10davenport@gmail.com>
2021-09-05 11:12:06 -0400

    whitespace

--- apps/userdocs/lib/userdocs/media/screenshot_helpers.ex
+++ apps/userdocs/lib/userdocs/media/screenshot_helpers.ex
@@ -10,34 +10,34 @@ defmodule UserDocs.Media.ScreenshotHelpers do
 
   @path "apps/userdocs_web/priv/static/images/"
 
-  def handle_screenshot_upsert_results(state = { :nok, %{ id: nil } }) do
+  def handle_screenshot_upsert_results(state = {:nok, %{id: nil}}) do
     # Logger.debug("The screenshot upsert failed")
     state
   end
-  def handle_screenshot_upsert_results({ :ok, %Screenshot{ id: nil, step_id: step_id } }) do
+  def handle_screenshot_upsert_results({:ok, %Screenshot{id: nil, step_id: step_id}}) do
     # Logger.debug("Screenshot #{step_id} exists, so we retreive it")
     screenshot =
       Media.list_screenshots(%{step_id: step_id})
       |> Enum.at(0)
 
-    { :ok, screenshot }
+    {:ok, screenshot}
   end
-  def handle_screenshot_upsert_results(state = { :ok, %{ id: id, step_id: step_id }}) do
+  def handle_screenshot_upsert_results(state = {:ok, %{id: id, step_id: step_id}}) do
     Logger.debug("Screenshot #{id} for step #{step_id} was created correctly")
     state
   end
 
-  def save_file({ :ok, %Screenshot{} = screenshot }, raw) do
+  def save_file({:ok, %Screenshot{} = screenshot}, raw) do
     Logger.debug("Handling Screenshot File")
     # step = Automation.get_step!(screenshot.step_id)
     file_name = Integer.to_string(screenshot.id) <> ".jpeg"
     case FileHelpers.encode_hash_save_file(raw, file_name) do
-      %{ filename: filename } -> { :ok, screenshot, filename }
+      %{filename: filename} -> {:ok, screenshot, filename}
     end
   end
 
   # This is some bullshit I put in for the inconsistency after we get done.  Same deal as handle_file_disposition
-  def maybe_resize_image(state = { :ok, %Screenshot{}, filename }, "Element Screenshot", %{ "size"=> _ } = element) do
+  def maybe_resize_image(state = {:ok, %Screenshot{}, filename}, "Element Screenshot", element = %{"size"=> _}) do
     geometry = "#{element["size"]["width"]}x#{element["size"]["height"]}+#{element["size"]["x"]}+#{element["size"]["y"]}"
 
     @path <> filename
@@ -51,16 +51,16 @@ defmodule UserDocs.Media.ScreenshotHelpers do
     state
   end
 
-  def update_screenshot({ :ok, screenshot, filename }) do
-    UserDocs.Media.update_screenshot(screenshot, %{ aws_file: (@path <> filename)})
+  def update_screenshot({:ok, screenshot, filename}) do
+    UserDocs.Media.update_screenshot(screenshot, %{aws_file: (@path <> filename)})
   end
 
-  def handle_file_disposition({ :ok, result = %{ screenshot: _, file: _ }}) do
+  def handle_file_disposition({:ok, result = %{screenshot: _, file: _}}) do
     # Logger.debug("Screenshot was created or updated and file was created")
-    { :ok, result }
+    {:ok, result}
   end
-  def handle_file_disposition({ :ok, result = %{ screenshot: _ }}) do
+  def handle_file_disposition({:ok, result = %{screenshot: _}}) do
     # Logger.debug("Screenshot was created or updated.  The file wasn't")
-    { :ok, result }
+    {:ok, result}
   end
 end
