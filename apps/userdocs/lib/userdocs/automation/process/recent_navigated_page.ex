876eb2bba826b39bdc941acc224cda6ef23e1064
johns10 <johns10davenport@gmail.com>
2021-09-05 11:13:17 -0400

    bracket whitespace

--- apps/userdocs/lib/userdocs/automation/process/recent_navigated_page.ex
+++ apps/userdocs/lib/userdocs/automation/process/recent_navigated_page.ex
@@ -12,8 +12,8 @@ defmodule UserDocs.Automation.Process.RecentPage do
 
     RecentPage.get(page, socket.assigns.data.pages)
   """
-  def get_id(process, current_step, [ %Page{} | _ ] = pages) do
-    case get(process, current_step, [ %Page{} | _ ] = pages) do
+  def get_id(process, current_step, [%Page{} | _] = pages) do
+    case get(process, current_step, [%Page{} | _] = pages) do
       nil -> nil
       %Page{} = page ->
         Map.get(page, :id)
@@ -21,13 +21,13 @@ defmodule UserDocs.Automation.Process.RecentPage do
   end
   def get_id(_, _, []), do: nil
 
-  def get(%Process{ steps: [] = steps }, current_step, pages) do
+  def get(%Process{steps: [] = steps}, current_step, pages) do
     get(steps, current_step, pages)
   end
-  def get(%Process{ steps: [ %Step{} | _ ] = steps }, current_step, [ %Page{} | _ ] = pages) do
+  def get(%Process{steps: [%Step{} | _] = steps}, current_step, [%Page{} | _] = pages) do
     get(steps, current_step, pages)
   end
-  def get([ %Step{} | _ ] = steps, current_step, [ %Page{} | _ ] = pages) do
+  def get([%Step{} | _] = steps, current_step, [%Page{} | _] = pages) do
     _step =
       case recent_navigation_step(current_step, steps) do
         None ->
