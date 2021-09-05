dd97d0b4548003bb03f27583a4584d2cc58dcb1a
johns10 <johns10davenport@gmail.com>
2021-09-05 11:36:48 -0400

    Allow nulls in here

--- apps/userdocs/priv/repo/migrations/20210905000001_alter_steps_process_delete_policy.exs
+++ apps/userdocs/priv/repo/migrations/20210905000001_alter_steps_process_delete_policy.exs
@@ -4,14 +4,14 @@ defmodule UserDocs.Repo.Migrations.AlterStepProcessDelete do
   def up do
     drop(constraint(:steps, "steps_process_id_fkey"))
     alter table(:steps) do
-      modify :process_id, references(:processes, on_delete: :delete_all), null: false
+      modify :process_id, references(:processes, on_delete: :delete_all), null: true
     end
   end
 
   def down do
     drop(constraint(:steps, "steps_process_id_fkey"))
     alter table(:steps) do
-      modify :process_id, references(:processes, on_delete: :nothing), null: false
+      modify :process_id, references(:processes, on_delete: :nothing), null: true
     end
   end
 end
