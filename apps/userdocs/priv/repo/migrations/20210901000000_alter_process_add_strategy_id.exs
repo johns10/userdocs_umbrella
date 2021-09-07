15679faa6c0a2e929ec583fd131ddad92f8b00f1
johns10 <johns10davenport@gmail.com>
2021-09-07 00:03:22 -0400

    and again

--- apps/userdocs/priv/repo/migrations/20210901000000_alter_process_add_strategy_id.exs
+++ apps/userdocs/priv/repo/migrations/20210901000000_alter_process_add_strategy_id.exs
@@ -1,18 +1,18 @@
 defmodule UserDocs.Repo.Migrations.AlterProcessAddStrategyId do
   use Ecto.Migration
   require Ecto.Query
+  alias UserDocs.Projects
+  alias UserDocs.Projects.Project
 
   def up do
     alter table(:projects) do
       add :strategy_id, references(:strategies, on_delete: :nothing)
     end
     flush()
-    Ecto.Query.from(version in "versions", select: version.strategy_id)
+    Ecto.Query.from(version in Project)
     |> UserDocs.Repo.all()
-    |> Enum.map(fn(version) ->
-      attrs = %{strategy_id: version}
-      {:ok, process} = UserDocs.Automation.update_process(version.process, attrs)
-      process
+    |> Enum.map(fn(project) ->
+      {:ok, _} = Projects.update_project(project, %{strategy_id: 2})
     end)
   end
 
