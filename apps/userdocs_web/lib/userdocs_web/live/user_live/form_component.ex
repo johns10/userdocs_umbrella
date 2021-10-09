cd62182219a6d96fd4f310af2b5d2e5f62fde642
John Davenport <johns10davenport@gmail.com>
2021-10-08 22:39:07 -0400

    DaisyUI

--- apps/userdocs_web/lib/userdocs_web/live/user_live/form_component.ex
+++ apps/userdocs_web/lib/userdocs_web/live/user_live/form_component.ex
@@ -16,57 +16,68 @@ defmodule UserDocsWeb.UserLive.FormComponent do
         phx_change: "validate",
         phx_submit: "save" %>
 
-        <%= Layout.text_input f, :email, [], "control" %>
-        <%= Layout.text_input f, :image_path, [], "control" %>
-        <%= Layout.text_input f, :user_data_dir_path, [], "control" %>
+        <div class="form-control">
+          <%= label f, :email, class: "label" %>
+          <%= text_input f, :email, type: "text", class: "input input-sm input-bordered" %>
+          <%= error_tag f, :email %>
+        </div>
 
-        <%= label f, :overrides, class: "label" %>
+        <div class="flex justify-between items-center">
+          <%= label f, :overrides, class: "label" %>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+          </svg>
+        </div>
         <%= inputs_for f, :overrides, [append: @append_overrides], fn puof -> %>
-          <div class="field is-grouped">
-            <div class="control">
-              <div class="select">
-                <%= select puof, :project_id, @project_select_options %>
-              </div>
+          <div class="flex items-center mb-2">
+            <div class="flex-1 mr-4">
+              <%= select puof, :project_id, @project_select_options, class: "select select-sm select-bordered" %>
               <%= error_tag(puof, :project_id) %>
             </div>
-            <div class="control is-expanded">
-              <%= text_input puof, :url, class: :input %>
+            <div class="flex-auto mr-2">
+              <%= text_input puof, :url, class: "input input-sm input-bordered" %>
               <%= error_tag(puof, :url) %>
             </div>
             <%= hidden_input puof, :temp_id %>
             <%= if is_nil(Ecto.Changeset.get_field(puof.source, :temp_id)) do %>
-              <%= Layout.checkbox(puof, :delete) %>
+              <%= checkbox puof, :delete, class: "checkbox" %>
             <% else %>
-              <div class="control">
-                <%= link(to: "#", phx_click: "remove-override", phx_value_temp_id: puof.data.temp_id, phx_target: @myself.cid) do %>
-                  <span>x</span>
-                <% end %>
-              </div>
+              <%= link(to: "#", phx_click: "remove-override", phx_value_temp_id: puof.data.temp_id, phx_target: @myself.cid) do %>
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+              </svg>
+              <% end %>
             <% end %>
           </div>
         <% end %>
 
-        <%= link to: "#", phx_click: "add-override", phx_target: @myself.cid, id: "add-override" do %>
+        <%= link to: "#", class: "btn btn-sm btn-success", phx_click: "add-override", phx_target: @myself.cid, id: "add-override" do %>
           <p>New Override</p>
         <% end %>
 
-        <%= inputs_for f, :team_users, fn tuf -> %>
-          <div class="field is-grouped">
+        <div class="grid grid-cols-6 gap-4">
+          <label class="label">Default</label>
+          <label class="label col-span-4">Team</label>
+          <label class="label">Delete</label>
+          <%= inputs_for f, :team_users, fn tuf -> %>
             <%= hidden_input tuf, :user_id, value: f.data.id %>
-            <%= Layout.checkbox tuf, :default %>
-            <%= Layout.select_input tuf, :team_id, @select_lists.teams, [], "control px-2" %>
+            <%= checkbox tuf, :default, class: "checkbox mr-4" %>
+            <%= select tuf, :team_id, @select_lists.teams, class: "select select-sm select-bordered col-span-4" %>
             <%= if is_nil tuf.data.temp_id do %>
-              <%= Layout.checkbox tuf, :delete %>
+              <%= checkbox tuf, :delete, class: "checkbox" %>
             <% else %>
               <%= hidden_input tuf, :temp_id %>
               <%= link(to: "#", phx_click: "remove-team", phx_value_remove: tuf.data.temp_id, phx_target: @myself.cid) do %>
                 <span>x</span>
               <% end %>
             <% end %>
-          </div>
-        <% end %>
+          <% end %>
+        </div>
         <%= error_tag f, :team_users %>
-        <%= submit "Save", phx_disable_with: "Saving...", class: "button is-primary" %>
+
+        <div class="mt-4">
+          <%= submit "Save", phx_disable_with: "Saving...", class: "btn btn-primary mr-2" %>
+        </div>
       </form>
     """
   end
@@ -76,34 +87,32 @@ defmodule UserDocsWeb.UserLive.FormComponent do
       <%= form_for @changeset, registration_path(@socket, action, @user), [
         phx_target: @myself.cid, phx_change: "validate", phx_submit: "save", id: "user-form", as: :user
       ], fn f -> %>
-        <%= Layout.text_input(f, Pow.Ecto.Schema.user_id_field(@changeset), [], "control") %>
+        <div class="form-control">
+          <%= label f, Pow.Ecto.Schema.user_id_field(@changeset), class: "label" %>
+          <%= text_input f, Pow.Ecto.Schema.user_id_field(@changeset), type: "text", class: "input input-bordered" %>
+          <%= error_tag f, Pow.Ecto.Schema.user_id_field(@changeset) %>
+        </div>
 
-        <div class="field">
+        <div class="form-control">
           <%= label f, :current_password, class: "label" %>
-          <div>
-            <%= password_input f, :current_password, class: :input, type: :password %>
-          </div>
+          <%= password_input f, :current_password, class: "input input-bordered" %>
           <%= error_tag f, :current_password %>
         </div>
 
-        <div class="field">
+        <div class="form-control">
           <%= label f, :password, class: "label" %>
-          <div>
-            <%= password_input f, :password, class: :input, type: :password %>
-          </div>
+          <%= password_input f, :password, class: "input input-bordered" %>
           <%= error_tag f, :password %>
         </div>
 
-        <div class="field">
+        <div class="form-control">
           <%= label f, :password_confirmation, class: "label" %>
-          <div>
-            <%= password_input f, :password_confirmation, class: :input, type: :password %>
-          </div>
+          <%= password_input f, :password_confirmation, class: "input input-bordered" %>
           <%= error_tag f, :password_confirmation %>
         </div>
 
-        <div>
-          <%= submit "Update", class: "button is-primary" %>
+        <div class="mt-4">
+          <%= submit "Save", phx_disable_with: "Saving...", class: "btn btn-primary mr-2" %>
         </div>
 
       <% end %>
