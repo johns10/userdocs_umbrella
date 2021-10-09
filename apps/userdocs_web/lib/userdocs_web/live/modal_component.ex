7e7480c3d8b629c95df5522394bca20ee6bb9229
John Davenport <johns10davenport@gmail.com>
2021-10-08 22:31:48 -0400

    Refactor to DaisyUI

--- apps/userdocs_web/lib/userdocs_web/live/modal_component.ex
+++ apps/userdocs_web/lib/userdocs_web/live/modal_component.ex
@@ -7,29 +7,24 @@ defmodule UserDocsWeb.ModalComponent do
   @impl true
   def render(assigns) do
     ~L"""
-    <div id="<%= @id %>" class="modal is-active"
+    <input type="checkbox" id="modal" class="modal-toggle" checked>
+    <div id="live-modal" class="modal"
       phx-capture-click="close"
       phx-window-keydown="close"
       phx-key="escape"
       phx-target="#<%= @id %>"
       phx-page-loading>
 
-      <div
-        class="modal-background"
-        phx-click="close"
-        phx-target="<%= @myself.cid %>"
-      ></div>
-      <div class="modal-content" style="overflow:auto; max-height:calc(100vh - 125px);">
-        <div class="box">
-          <div
-            class="modal-close is-large"
-            phx-click="close"
-            phx-target="<%= @myself.cid %>"
-          ></div>
-          <%= live_component @socket, @component, @opts %>
+      <div class="modal-box">
+        <div class="modal-action float-right m-0" phx-click="close" phx-target="<%= @myself.cid %>">
+          <svg aria-hidden="true" focusable="false"  class="w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            <path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm101.8-262.2L295.6 256l62.2 62.2c4.7 4.7 4.7 12.3 0 17l-22.6 22.6c-4.7 4.7-12.3 4.7-17 0L256 295.6l-62.2 62.2c-4.7 4.7-12.3 4.7-17 0l-22.6-22.6c-4.7-4.7-4.7-12.3 0-17l62.2-62.2-62.2-62.2c-4.7-4.7-4.7-12.3 0-17l22.6-22.6c4.7-4.7 12.3-4.7 17 0l62.2 62.2 62.2-62.2c4.7-4.7 12.3-4.7 17 0l22.6 22.6c4.7 4.7 4.7 12.3 0 17z"></path>
+          </svg>
         </div>
+        <%= live_component @socket, @component, @opts %>
       </div>
     </div>
+
     """
   end
 
