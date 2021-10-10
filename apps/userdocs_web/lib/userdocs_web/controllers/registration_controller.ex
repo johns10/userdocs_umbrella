a22db71f9f6b60628e90f9800cda5a41e4f6da56
John Davenport <johns10davenport@gmail.com>
2021-10-09 20:13:15 -0400

    Set strategy id on registration

--- apps/userdocs_web/lib/userdocs_web/controllers/registration_controller.ex
+++ apps/userdocs_web/lib/userdocs_web/controllers/registration_controller.ex
@@ -1,13 +1,20 @@
 defmodule UserDocsWeb.RegistrationController do
   use UserDocsWeb, :controller
   alias UserDocs.Users
+  alias UserDocs.Web
 
   def create(conn, %{"user" => user_params}) do
     case Users.create_user(user_params) do
       {:ok, user} ->
         {:ok, team} = Users.create_team(%{name: team_name(user.email)})
         {:ok, _team_user} = Users.create_team_user(%{user_id: user.id, team_id: team.id})
-        {:ok, project} = UserDocs.Projects.create_project(%{team_id: team.id, default: true, name: "Default", base_url: "https://www.example.com"})
+        {:ok, project} = UserDocs.Projects.create_project(%{
+          team_id: team.id,
+          default: true,
+          name: "Default",
+          base_url: "https://www.example.com",
+          strategy_id: Web.css_strategy() |> Map.get(:id)
+        })
         {:ok, user} = Users.update_user_options(user, %{selected_team_id: team.id, selected_project_id: project.id})
         PowEmailConfirmation.Phoenix.ControllerCallbacks.send_confirmation_email(user, conn)
         redirect(conn, to: Routes.registration_path(conn, :edit, user))
