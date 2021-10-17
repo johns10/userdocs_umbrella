7d3d2484955c8eaa4dc59c38e244300cb93305c9
John Davenport <johns10davenport@gmail.com>
2021-10-17 10:49:21 -0400

    New live tests

---
+++ apps/userdocs_web/test/userdocs_web/live/page_live_test.exs
@@ -0,0 +1,111 @@
+defmodule UserDocsWeb.PageLiveTest do
+  use UserDocsWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+
+  alias UserDocs.Pages
+
+  @create_attrs %{}
+  @update_attrs %{}
+  @invalid_attrs %{}
+
+  defp fixture(:page) do
+    {:ok, page} = Pages.create_page(@create_attrs)
+    page
+  end
+
+  defp create_page(_) do
+    page = fixture(:page)
+    %{page: page}
+  end
+
+  describe "Index" do
+    setup [:create_page]
+
+    test "lists all pages", %{conn: conn, page: page} do
+      {:ok, _index_live, html} = live(conn, Routes.page_index_path(conn, :index))
+
+      assert html =~ "Listing Pages"
+    end
+
+    test "saves new page", %{conn: conn} do
+      {:ok, index_live, _html} = live(conn, Routes.page_index_path(conn, :index))
+
+      assert index_live |> element("a", "New Page") |> render_click() =~
+               "New Page"
+
+      assert_patch(index_live, Routes.page_index_path(conn, :new))
+
+      assert index_live
+             |> form("#page-form", page: @invalid_attrs)
+             |> render_change() =~ "can&#39;t be blank"
+
+      {:ok, _, html} =
+        index_live
+        |> form("#page-form", page: @create_attrs)
+        |> render_submit()
+        |> follow_redirect(conn, Routes.page_index_path(conn, :index))
+
+      assert html =~ "Page created successfully"
+    end
+
+    test "updates page in listing", %{conn: conn, page: page} do
+      {:ok, index_live, _html} = live(conn, Routes.page_index_path(conn, :index))
+
+      assert index_live |> element("#page-#{page.id} a", "Edit") |> render_click() =~
+               "Edit Page"
+
+      assert_patch(index_live, Routes.page_index_path(conn, :edit, page))
+
+      assert index_live
+             |> form("#page-form", page: @invalid_attrs)
+             |> render_change() =~ "can&#39;t be blank"
+
+      {:ok, _, html} =
+        index_live
+        |> form("#page-form", page: @update_attrs)
+        |> render_submit()
+        |> follow_redirect(conn, Routes.page_index_path(conn, :index))
+
+      assert html =~ "Page updated successfully"
+    end
+
+    test "deletes page in listing", %{conn: conn, page: page} do
+      {:ok, index_live, _html} = live(conn, Routes.page_index_path(conn, :index))
+
+      assert index_live |> element("#page-#{page.id} a", "Delete") |> render_click()
+      refute has_element?(index_live, "#page-#{page.id}")
+    end
+  end
+
+  describe "Show" do
+    setup [:create_page]
+
+    test "displays page", %{conn: conn, page: page} do
+      {:ok, _show_live, html} = live(conn, Routes.page_show_path(conn, :show, page))
+
+      assert html =~ "Show Page"
+    end
+
+    test "updates page within modal", %{conn: conn, page: page} do
+      {:ok, show_live, _html} = live(conn, Routes.page_show_path(conn, :show, page))
+
+      assert show_live |> element("a", "Edit") |> render_click() =~
+               "Edit Page"
+
+      assert_patch(show_live, Routes.page_show_path(conn, :edit, page))
+
+      assert show_live
+             |> form("#page-form", page: @invalid_attrs)
+             |> render_change() =~ "can&#39;t be blank"
+
+      {:ok, _, html} =
+        show_live
+        |> form("#page-form", page: @update_attrs)
+        |> render_submit()
+        |> follow_redirect(conn, Routes.page_show_path(conn, :show, page))
+
+      assert html =~ "Page updated successfully"
+    end
+  end
+end
